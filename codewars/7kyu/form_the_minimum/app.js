6793e6a107e163f99c681599122ae77e3698bd9c
AnsariAbdullah <ansariabdullahar@gmail.com>
2022-08-30 08:57:30 +0530

    format document

--- codewars/7kyu/form_the_minimum/app.js
+++ codewars/7kyu/form_the_minimum/app.js
@@ -1,3 +1,3 @@
-function minValue(values){
-  return +(Array.from(new Set(values))).sort((a,b) => a-b).join('')
+function minValue(values) {
+	return +(Array.from(new Set(values))).sort((a, b) => a - b).join('')
 }
