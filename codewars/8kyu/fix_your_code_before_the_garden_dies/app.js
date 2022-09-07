e329bda5c80014ad5c06dd244a4d3d94d264f44f
AnsariAbdullah <ansariabdullahar@gmail.com>
2022-09-07 14:59:45 +0530

    refactor

--- codewars/8kyu/fix_your_code_before_the_garden_dies/app.js
+++ codewars/8kyu/fix_your_code_before_the_garden_dies/app.js
@@ -1,7 +1,7 @@
-function rainAmount(mm){
+function rainAmount(mm) {
 	if (mm < 40) {
-			 return `You need to give your plant ${40 - mm}mm of water`
-	}else {
-			 return "Your plant has had more than enough water for today!"
+		return `You need to give your plant ${40 - mm}mm of water`
+	} else {
+		return "Your plant has had more than enough water for today!"
 	};
 }
