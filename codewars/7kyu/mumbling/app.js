ec87d64c8c10e2198d8d84dc7cff455be443ac52
AnsariAbdullah <ansariabdullahar@gmail.com>
2022-09-29 15:29:29 +0530

    add mumbling solution

---
+++ codewars/7kyu/mumbling/app.js
@@ -0,0 +1,9 @@
+function accum(s) {
+	let letter = s.split('');
+	let word = []
+
+	for (let i = 0; i < letter.length; i++) {
+		word.push(letter[i].toUpperCase() + Array(i + 1).join(letter[i].toLowerCase()))
+	}
+	return word.join('-')
+}
