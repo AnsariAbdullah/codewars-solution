61c7d20d07729f2dbedd5360616ed2897cf08a60
AnsariAbdullah <ansariabdullahar@gmail.com>
2022-10-04 13:51:01 +0530

    format document

--- codewars/8kyu/bartender_drinks/app.js
+++ codewars/8kyu/bartender_drinks/app.js
@@ -1,25 +1,25 @@
-function getDrinkByProfession(param){
-  const val = param.toLowerCase();
-  switch(val){
-      case 'jabroni':
-        return "Patron Tequila"
-        break;
-      case 'school counselor':
-        return "Anything with Alcohol"
-        break;
-      case 'programmer':
-        return "Hipster Craft Beer"
-        break;
-      case 'bike gang member':
-        return "Moonshine"
-        break;
-      case 'politician':
-        return "Your tax dollars"
-        break;
-      case 'rapper':
-        return "Cristal"
-        break;
-      default:
-        return 'Beer'
-  }
+function getDrinkByProfession(param) {
+	const val = param.toLowerCase();
+	switch (val) {
+		case 'jabroni':
+			return "Patron Tequila"
+			break;
+		case 'school counselor':
+			return "Anything with Alcohol"
+			break;
+		case 'programmer':
+			return "Hipster Craft Beer"
+			break;
+		case 'bike gang member':
+			return "Moonshine"
+			break;
+		case 'politician':
+			return "Your tax dollars"
+			break;
+		case 'rapper':
+			return "Cristal"
+			break;
+		default:
+			return 'Beer'
+	}
 }
