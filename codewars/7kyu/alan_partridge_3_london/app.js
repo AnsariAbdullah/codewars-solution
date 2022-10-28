495f2c8de29b451235fd9071e6858b4b3aa070d9
AnsariAbdullah <ansariabdullahar@gmail.com>
2022-10-28 12:44:02 +0530

    add alan_partridge_3_london solution

---
+++ codewars/7kyu/alan_partridge_3_london/app.js
@@ -0,0 +1,7 @@
+function alan(x){
+  if(x.includes('Rejection') && x.includes('Disappointment') && x.includes('Backstabbing Central') && x.includes('Shattered Dreams Parkway')){
+    return 'Smell my cheese you mother!'
+  }else{
+    return 'No, seriously, run. You will miss it.'
+  }
+}
