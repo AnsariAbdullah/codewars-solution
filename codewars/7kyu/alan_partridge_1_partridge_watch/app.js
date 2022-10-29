d2be4c4024c5739c76cf113654a28eb4b44ad6da
AnsariAbdullah <ansariabdullahar@gmail.com>
2022-10-29 11:57:46 +0530

    format document

--- codewars/7kyu/alan_partridge_1_partridge_watch/app.js
+++ codewars/7kyu/alan_partridge_1_partridge_watch/app.js
@@ -1,9 +1,9 @@
-function part(x){
-  const arr=['Partridge','PearTree','Chat','Dan','Toblerone','Lynn','AlphaPapa','Nomad']
-  let num = x.filter(v=>arr.includes(v)).length
-  if(arr.some(v=>x.includes(v))){
-    return `Mine\'s a Pint${'!'.repeat(num)}`
-  }else{
-    return 'Lynn, I\'ve pierced my foot on a spike!!'
-  }
+function part(x) {
+	const arr = ['Partridge', 'PearTree', 'Chat', 'Dan', 'Toblerone', 'Lynn', 'AlphaPapa', 'Nomad']
+	let num = x.filter(v => arr.includes(v)).length
+	if (arr.some(v => x.includes(v))) {
+		return `Mine\'s a Pint${'!'.repeat(num)}`
+	} else {
+		return 'Lynn, I\'ve pierced my foot on a spike!!'
+	}
 }
