7551ff7246d6c55c4f2974b2b2924203c7963707
AnsariAbdullah <ansariabdullahar@gmail.com>
2022-11-01 09:57:33 +0530

    format document

--- codewars/7kyu/shortest_word/app.js
+++ codewars/7kyu/shortest_word/app.js
@@ -1,7 +1,7 @@
-function findShort(s){
-  let wordLengths = []
-  s.split(' ').map(item => {
-    wordLengths.push(item.length)
-  })
-  return Math.min(...wordLengths)
+function findShort(s) {
+	let wordLengths = []
+	s.split(' ').map(item => {
+		wordLengths.push(item.length)
+	})
+	return Math.min(...wordLengths)
 }
