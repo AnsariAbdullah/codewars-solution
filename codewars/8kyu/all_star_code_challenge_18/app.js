47311c1999563407e99df21ddb1cba9807d09817
AnsariAbdullah <ansariabdullahar@gmail.com>
2022-11-16 13:28:55 +0530

    format document

--- codewars/8kyu/all_star_code_challenge_18/app.js
+++ codewars/8kyu/all_star_code_challenge_18/app.js
@@ -1,9 +1,9 @@
-function strCount(str, letter){  
-  let count = 0;
-  for(let i =0; i<str.length; i++){
-    if(str[i] === letter){
-      count++;
-    }
-  }
-  return count;
+function strCount(str, letter) {
+	let count = 0;
+	for (let i = 0; i < str.length; i++) {
+		if (str[i] === letter) {
+			count++;
+		}
+	}
+	return count;
 }
