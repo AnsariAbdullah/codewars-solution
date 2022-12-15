3a7f70360ff897beb56bdf1e2ccf7fe4e633d5c3
AnsariAbdullah <ansariabdullahar@gmail.com>
2022-12-15 12:35:15 +0530

    format document

--- codewars/8kyu/loop_statement --break,continue/app.js
+++ codewars/8kyu/loop_statement --break,continue/app.js
@@ -1,14 +1,13 @@
-function grabDoll(dolls){
-  let bag=[];
-  for(let i=0; i<dolls.length; i++){
-    if(bag.length >= 3){
-       break
-    }else if(dolls[i] === "Hello Kitty" || dolls[i] === "Barbie doll"){
-      bag.push(dolls[i])
-    }else{
-      continue
-    }
-  }
-  
-  return bag;
+function grabDoll(dolls) {
+	let bag = [];
+	for (let i = 0; i < dolls.length; i++) {
+		if (bag.length >= 3) {
+			break
+		} else if (dolls[i] === "Hello Kitty" || dolls[i] === "Barbie doll") {
+			bag.push(dolls[i])
+		} else {
+			continue
+		}
+	}
+	return bag;
 }
