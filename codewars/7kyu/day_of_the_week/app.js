a388f541931f60d93db0398c5e6d2a57ebae6d53
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-01-12 12:33:04 +0530

    add alternate solution

--- codewars/7kyu/day_of_the_week/app.js
+++ codewars/7kyu/day_of_the_week/app.js
@@ -3,3 +3,8 @@ function dayOfTheWeek(date) {
 	let dt = date.split('/')
 	return dayNames[new Date(dt.reverse().join('-')).getDay()]
 }
+
+// Alternate solution
+function dayOfTheWeek(date) {
+	return new Date(date.split('/').reverse().join('-')).toLocaleString('en-us', {weekday: 'long'})
+}
