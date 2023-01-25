c87b686a6deda3c35f781fd24db5b88589a0b1c3
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-01-25 12:57:53 +0530

    format document

--- codewars/7kyu/factorial/app.js
+++ codewars/7kyu/factorial/app.js
@@ -1,17 +1,17 @@
-function factorial(n){
-  let result = 1
-  for(let i=n; i>0; i--){
-    result *= i
-  }
-  return result;
+function factorial(n) {
+	let result = 1
+	for (let i = n; i > 0; i--) {
+		result *= i
+	}
+	return result;
 }
 
 // alternate solution
-function factorial(n){
-  let result = 1
-  while(n>0){
-    result *= n
-    n--
-  }
-  return result;
+function factorial(n) {
+	let result = 1
+	while (n > 0) {
+		result *= n
+		n--
+	}
+	return result;
 }
