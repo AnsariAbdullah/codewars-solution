f910a57ed294c9bb133552faf6ce8cc3f2100466
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-02-02 15:53:07 +0530

    format document

--- codewars/6kyu/create_phone_number/app.js
+++ codewars/6kyu/create_phone_number/app.js
@@ -1,12 +1,12 @@
-function createPhoneNumber(numbers){
-  return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`
+function createPhoneNumber(numbers) {
+	return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`
 }
 
 // alternate solution
-function createPhoneNumber(numbers){
-  let format = "(xxx) xxx-xxxx"
-  for(let i=0; i<numbers.length; i++){
-    format = format.replace('x', numbers[i])
-  }
-  return format
+function createPhoneNumber(numbers) {
+	let format = "(xxx) xxx-xxxx"
+	for (let i = 0; i < numbers.length; i++) {
+		format = format.replace('x', numbers[i])
+	}
+	return format
 }
