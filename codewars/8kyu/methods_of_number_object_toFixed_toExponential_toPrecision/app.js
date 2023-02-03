665f3d35118d05e4bd78cb184992449772076434
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-02-03 14:05:30 +0530

    format document

--- codewars/8kyu/methods_of_number_object_toFixed_toExponential_toPrecision/app.js
+++ codewars/8kyu/methods_of_number_object_toFixed_toExponential_toPrecision/app.js
@@ -1,9 +1,9 @@
-function howManySmaller(arr,n){
-  let newArr = arr.map(item => item.toFixed(2))
-  return newArr.filter(item => item < n).length
+function howManySmaller(arr, n) {
+	let newArr = arr.map(item => item.toFixed(2))
+	return newArr.filter(item => item < n).length
 }
 
 // alternate solution
-function howManySmaller(arr,n){
-  return arr.filter(x => +x.toFixed(2) < n).length;
+function howManySmaller(arr, n) {
+	return arr.filter(x => +x.toFixed(2) < n).length;
 }
