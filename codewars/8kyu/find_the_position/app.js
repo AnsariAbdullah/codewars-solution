76bcf024b0e93e345f0b2d9823c1fc617c85340b
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-02-13 12:52:29 +0530

    add alternate solution

--- codewars/8kyu/find_the_position/app.js
+++ codewars/8kyu/find_the_position/app.js
@@ -1,3 +1,9 @@
 function position(letter){
   return `Position of alphabet: ${letter.charCodeAt()-96}`
 }
+
+// alternate solution
+function position(letter){
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  return `Position of alphabet: ${alphabet.indexOf(letter)+1}`
+}
