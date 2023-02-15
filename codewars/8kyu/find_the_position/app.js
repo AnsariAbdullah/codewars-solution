5135302982ff3c2d782a07564ffa4da1e6fe5b49
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-02-15 12:23:14 +0530

    format document

--- codewars/8kyu/find_the_position/app.js
+++ codewars/8kyu/find_the_position/app.js
@@ -1,9 +1,9 @@
-function position(letter){
-  return `Position of alphabet: ${letter.charCodeAt()-96}`
+function position(letter) {
+	return `Position of alphabet: ${letter.charCodeAt() - 96}`
 }
 
 // alternate solution
-function position(letter){
-  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-  return `Position of alphabet: ${alphabet.indexOf(letter)+1}`
+function position(letter) {
+	const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+	return `Position of alphabet: ${alphabet.indexOf(letter) + 1}`
 }
