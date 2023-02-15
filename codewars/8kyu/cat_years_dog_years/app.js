eee339120237d24c79007a9e67c4b5ba9a67b23a
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-02-15 12:24:40 +0530

    add cat_years_dog_years solution

---
+++ codewars/8kyu/cat_years_dog_years/app.js
@@ -0,0 +1,22 @@
+var humanYearsCatYearsDogYears = function(humanYears) {
+  let catYears = 0;
+  let dogYears = 0;
+  if(humanYears <= 0 || typeof humanYears !== 'number'){
+    return [0,0,0];
+  }
+  if(humanYears === 1){
+      catYears = 15;
+      dogYears = 15;
+  }
+  if(humanYears === 2){
+      catYears = 15 + 9;
+      dogYears = 15 + 9;
+  }
+  if(humanYears > 2){
+      catYears = 24 + (4 * (humanYears- 2));
+      dogYears = 24 + (5 * (humanYears- 2));;
+  }
+  return [humanYears, catYears, dogYears];
+}
+
+
