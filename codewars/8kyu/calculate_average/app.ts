802695016ea84d8ae73b1590f69bcb93b02de236
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-02-28 07:02:16 +0530

    format document

--- codewars/8kyu/calculate_average/app.ts
+++ codewars/8kyu/calculate_average/app.ts
@@ -1,5 +1,5 @@
 export function findAverage(array: number[]): number {
-  if(array.length === 0) return 0  
-  let total:number = array.reduce((acc, curr) => acc+curr, 0)
-  return total/array.length
+	if (array.length === 0) return 0
+	let total: number = array.reduce((acc, curr) => acc + curr, 0)
+	return total / array.length
 }
