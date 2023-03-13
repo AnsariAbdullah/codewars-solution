759641c2df37c148895d032da6647ca71fe9ee09
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-03-13 07:34:15 +0530

    format document

--- fcc-basic/chunky_monkey/app.js
+++ fcc-basic/chunky_monkey/app.js
@@ -1,9 +1,9 @@
 function chunkArrayInGroups(arr, size) {
-  let newArr = []
-  for(let i=0; i<arr.length; i+=size){
-    newArr.push(arr.slice(i, i+size))
-  }
-  return newArr;
+	let newArr = []
+	for (let i = 0; i < arr.length; i += size) {
+		newArr.push(arr.slice(i, i + size))
+	}
+	return newArr;
 }
 
 chunkArrayInGroups(["a", "b", "c", "d"], 2);
