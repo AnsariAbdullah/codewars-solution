2dc3b642c486f484b32b61e27bc2ac5e63b69f6a
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-03-25 15:01:55 +0530

    add alternate solution

--- codewars/7kyu/number_of_people_in_the_bus/app.js
+++ codewars/7kyu/number_of_people_in_the_bus/app.js
@@ -3,3 +3,8 @@ var number = function (busStops) {
 	busStops.forEach(a => numberOfPeopleLeft = numberOfPeopleLeft + a[0] - a[1])
 	return numberOfPeopleLeft;
 }
+
+// alternate solution
+var number = function (busStops) {
+	return busStops.reduce((rem, [on, off]) => rem + on - off, 0)
+}
