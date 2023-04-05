c9160c47f39f5242d3a099f5482909d4f8dbefa6
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-04-05 12:51:12 +0530

    format document

--- fcc-basic/title_case_a_sentence/app.js
+++ fcc-basic/title_case_a_sentence/app.js
@@ -1,9 +1,9 @@
 function titleCase(str) {
-  return str
-  .toLowerCase()
-  .split(' ')
-  .map(item => item.replace(item.charAt(0), item.charAt(0).toUpperCase()))
-  .join(' ')
+	return str
+		.toLowerCase()
+		.split(' ')
+		.map(item => item.replace(item.charAt(0), item.charAt(0).toUpperCase()))
+		.join(' ')
 }
 
 titleCase("I'm a little tea pot");
