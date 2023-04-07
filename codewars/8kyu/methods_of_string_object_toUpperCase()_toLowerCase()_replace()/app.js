efeca147350a35e0351610fc7b04198056c8df3c
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-04-07 15:23:11 +0530

    add alternate solution

--- codewars/8kyu/methods_of_string_object_toUpperCase()_toLowerCase()_replace()/app.js
+++ codewars/8kyu/methods_of_string_object_toUpperCase()_toLowerCase()_replace()/app.js
@@ -1,3 +1,8 @@
 function alienLanguage(str){
-  return str.split(' ').map(v=>v.slice(0,-1).toUpperCase() +v.slice(-1).toLowerCase()).join(' ');
+  return str.split(" ").map(item => item.slice(0, -1).toUpperCase() + item.slice(-1).toLowerCase()).join(' ')
+}
+
+// alternate solution
+function alienLanguage(str){
+  return str.toUpperCase().replace(/.\b/g, x=> x.toLowerCase())
 }
