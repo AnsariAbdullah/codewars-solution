83d35b970de5b53f12f1ea2eadaff48210a1da3d
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-04-27 11:42:15 +0530

    format document

--- fcc-basic/where_do_i_belong/app.js
+++ fcc-basic/where_do_i_belong/app.js
@@ -1,10 +1,10 @@
 function getIndexToIns(arr, num) {
-  arr.sort((a,b) => a-b)
-  for(let i=0; i<arr.length; i++){
-    if(arr[i]  >= num) return i
-  }
-  
-  return arr.length;
+	arr.sort((a, b) => a - b)
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] >= num) return i
+	}
+
+	return arr.length;
 }
 
 getIndexToIns([40, 60], 50);
@@ -12,10 +12,10 @@ getIndexToIns([40, 60], 50);
 
 // alternate solution
 function getIndexToIns(arr, num) {
-  return arr
-    .concat(num)
-    .sort((a, b) => a -b)
-    .indexOf(num);
+	return arr
+		.concat(num)
+		.sort((a, b) => a - b)
+		.indexOf(num);
 }
 
 getIndexToIns([40, 60], 50);
