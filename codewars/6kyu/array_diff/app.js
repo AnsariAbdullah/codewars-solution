570165652d3732ca585bc6e2f0eb2189ae8ad7cf
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-05-15 12:37:16 +0530

    format document

--- codewars/6kyu/array_diff/app.js
+++ codewars/6kyu/array_diff/app.js
@@ -1,14 +1,14 @@
 function arrayDiff(a, b) {
-  return a.filter(item => !b.includes(item)) 
+	return a.filter(item => !b.includes(item))
 }
 
 // alternate 
 function arrayDiff(a, b) {
-  let newArr = []
-  for(let i=0; i<a.length; i++){
-    if(!b.includes(a[i])){
-      newArr.push(a[i])
-    }
-  }
-  return newArr;
+	let newArr = []
+	for (let i = 0; i < a.length; i++) {
+		if (!b.includes(a[i])) {
+			newArr.push(a[i])
+		}
+	}
+	return newArr;
 }
