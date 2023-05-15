d4c589be4b85cfa4ad6d49ee687053f9e977dbb9
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-05-15 12:34:54 +0530

    add alternate solution

--- codewars/6kyu/array_diff/app.js
+++ codewars/6kyu/array_diff/app.js
@@ -1,3 +1,14 @@
 function arrayDiff(a, b) {
   return a.filter(item => !b.includes(item)) 
 }
+
+// alternate 
+function arrayDiff(a, b) {
+  let newArr = []
+  for(let i=0; i<a.length; i++){
+    if(!b.includes(a[i])){
+      newArr.push(a[i])
+    }
+  }
+  return newArr;
+}
