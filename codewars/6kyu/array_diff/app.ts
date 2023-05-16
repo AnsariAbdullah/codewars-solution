89ce2e146ad3ce0c98585c71be45c559e3e22603
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-05-16 12:58:37 +0530

    add alternate solution

--- codewars/6kyu/array_diff/app.ts
+++ codewars/6kyu/array_diff/app.ts
@@ -1,3 +1,14 @@
 export function arrayDiff(a: number[], b: number[]): number[] {
   return a.filter(item => !b.includes(item));
 }
+
+// alternate solution
+export function altArrayDiff(a: number[], b: number[]): number[] {
+  let newArr:number[] = []
+  for(let i=0; i<a.length; i++){
+    if(!b.includes(a[i])){
+      newArr.push(a[i])
+    }
+  }
+  return newArr;
+}
