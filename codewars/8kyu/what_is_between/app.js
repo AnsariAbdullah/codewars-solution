72f076b8dc965fd834b11b91dd32c1da758cb292
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-05-30 11:17:45 +0530

    add printing_array_with_comma solution

---
+++ codewars/8kyu/what_is_between/app.js
@@ -0,0 +1,17 @@
+function between(a, b) {
+  let list = [];
+  for(let i=a; i<=b; i++){
+    list.push(i)
+  }
+  return list;
+}
+
+// alternate solution
+function between(a, b) {
+  let list = []
+  while(a<=b){
+    list.push(a)
+    a++
+  }
+  return list
+}
