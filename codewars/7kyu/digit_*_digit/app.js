31ed682fac89fcf21de812331946c3574ac205e6
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-06-05 14:15:12 +0530

    format document

--- codewars/7kyu/digit_*_digit/app.js
+++ codewars/7kyu/digit_*_digit/app.js
@@ -1,14 +1,14 @@
-function squareDigits(num){
-  let numb = num.toString()
-    let res = ''
-    for(let i=0; i < numb.length; i++){
-        res += numb[i] * numb[i]
-    }
-    return parseInt(res)
+function squareDigits(num) {
+	let numb = num.toString()
+	let res = ''
+	for (let i = 0; i < numb.length; i++) {
+		res += numb[i] * numb[i]
+	}
+	return parseInt(res)
 }
 
 // alternate solution
-function squareDigits(num){
-  let arr = num.toString().split('').map(int => parseInt(int) * parseInt(int))
-  return parseInt(arr.join(''))
+function squareDigits(num) {
+	let arr = num.toString().split('').map(int => parseInt(int) * parseInt(int))
+	return parseInt(arr.join(''))
 }
--- codewars/7kyu/digit_*_digit/app.ts
+++ codewars/7kyu/digit_*_digit/app.ts
@@ -1,10 +1,10 @@
 export class Kata {
-  static squareDigits(num: number): number {
-    let numb:string = num.toString()
-    let res:string = ''
-    for(let i=0; i < numb.length; i++){
-        res += parseInt(numb[i]) * parseInt(numb[i])
-    }
-    return parseInt(res)
-  }
+	static squareDigits(num: number): number {
+		let numb: string = num.toString()
+		let res: string = ''
+		for (let i = 0; i < numb.length; i++) {
+			res += parseInt(numb[i]) * parseInt(numb[i])
+		}
+		return parseInt(res)
+	}
 }
