566a178235bb47e5998e3a3b6eeb54bdf62b58b5
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-06-19 09:38:51 +0530

    add alternate solution as comment

--- codewars/8kyu/DNA_to_RNA_conversion/app.ts
+++ codewars/8kyu/DNA_to_RNA_conversion/app.ts
@@ -1,4 +1,6 @@
 export function DNAtoRNA(dna: string): string {
+  // return dna.replaceAll('T', 'U');
+	// the above code works as well but throws 'replaceAll' does not exist on type 'string'
 	return dna.replace(/T/g, 'U');
 }
 
