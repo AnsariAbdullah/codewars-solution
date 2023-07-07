651d44e31485a3c6990ae0ae0262f8ed0bb6a746
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-07-07 12:06:02 +0530

    refactor

--- codewars/7kyu/compare_strings_by_sum_of_chars/app.js
+++ codewars/7kyu/compare_strings_by_sum_of_chars/app.js
@@ -1,35 +1,14 @@
 function compare(str1, str2) {
 
-	if (str1 === null || str1.toUpperCase() === "NULL" || str1.toUpperCase() === "NIL" || str1.toUpperCase() === "NONE") {
-		str1 = "";
-	}
-	if (str2 === null || str2.toUpperCase() === "NULL" || str2.toUpperCase() === "NIL" || str2.toUpperCase() === "NONE") {
-		str2 = "";
-	}
-
-
-	if (!str1.match(/^[A-Za-z]+$/)) {
-		str1 = "";
-	}
-	if (!str2.match(/^[A-Za-z]+$/)) {
-		str2 = "";
-	}
-
-
-	str1 = str1.toUpperCase();
-	str2 = str2.toUpperCase();
-
-
 	let sum1 = 0;
-	let sum2 = 0;
-	for (let i = 0; i < str1.length; i++) {
-		sum1 += str1.charCodeAt(i);
-	}
-	for (let i = 0; i < str2.length; i++) {
-		sum2 += str2.charCodeAt(i);
-	}
+  let sum2 = 0;
 
+  if (!s1 || s1.search(/[^a-zA-Z]+/) !== -1) s1 = '';
+  if (!s2 || s2.search(/[^a-zA-Z]+/) !== -1) s2 = '';
 
-	return sum1 === sum2;
+  s1.toUpperCase().split('').map((item) => sum1 += item.charCodeAt(0));
+  s2.toUpperCase().split('').map((item) => sum2 += item.charCodeAt(0));
+
+  return sum1 === sum2;
 }
 
