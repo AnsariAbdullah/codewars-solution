a35ea34a8fe0f63c70d287466300f91f87081db0
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-07-10 12:27:18 +0530

    format document

--- codewars/7kyu/compare_strings_by_sum_of_chars/app.js
+++ codewars/7kyu/compare_strings_by_sum_of_chars/app.js
@@ -1,14 +1,13 @@
 function compare(str1, str2) {
 
 	let sum1 = 0;
-  let sum2 = 0;
+	let sum2 = 0;
 
-  if (!s1 || s1.search(/[^a-zA-Z]+/) !== -1) s1 = '';
-  if (!s2 || s2.search(/[^a-zA-Z]+/) !== -1) s2 = '';
+	if (!s1 || s1.search(/[^a-zA-Z]+/) !== -1) s1 = '';
+	if (!s2 || s2.search(/[^a-zA-Z]+/) !== -1) s2 = '';
 
-  s1.toUpperCase().split('').map((item) => sum1 += item.charCodeAt(0));
-  s2.toUpperCase().split('').map((item) => sum2 += item.charCodeAt(0));
+	s1.toUpperCase().split('').map((item) => sum1 += item.charCodeAt(0));
+	s2.toUpperCase().split('').map((item) => sum2 += item.charCodeAt(0));
 
-  return sum1 === sum2;
+	return sum1 === sum2;
 }
-
