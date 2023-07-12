5eb5906a05550714f3abcff7ef0aec79f1604e43
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-07-12 15:44:15 +0530

    add ts solution

--- codewars/7kyu/disemvowel_trolls/app.ts
+++ codewars/7kyu/disemvowel_trolls/app.ts
@@ -1,15 +1,15 @@
 export class Kata {
-  static disemvowel(str: string): string {
-    return str.replace(/[aeiou]/gi, '');
-  }
+	static disemvowel(str: string): string {
+		return str.replace(/[aeiou]/gi, '');
+	}
 }
 
 export class KataAlternate {
-  static disemvowel(str: string): string {
-    let vowels:string = 'aeiou'
-    return str
-          .split('')
-			  	.filter(letter => !vowels.includes(letter.toLowerCase()))
-				  .join('');;
-  }
+	static disemvowel(str: string): string {
+		let vowels: string = 'aeiou'
+		return str
+			.split('')
+			.filter(letter => !vowels.includes(letter.toLowerCase()))
+			.join('');;
+	}
 }
