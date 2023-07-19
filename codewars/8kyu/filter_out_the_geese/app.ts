6430e80af41bc3581230897f96a8ae1134295d22
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-07-19 10:00:19 +0530

    add ts solution

---
+++ codewars/8kyu/filter_out_the_geese/app.ts
@@ -0,0 +1,4 @@
+export function gooseFilter (birds: string[]): string[] {
+  const geese: string[] = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+  return birds.filter(item => !geese.includes(item))
+}
