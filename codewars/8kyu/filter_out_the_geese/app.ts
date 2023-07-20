2635ee025036ebe99986c2f7fd724a83df91dab2
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-07-20 11:06:56 +0530

    format document

--- codewars/8kyu/filter_out_the_geese/app.ts
+++ codewars/8kyu/filter_out_the_geese/app.ts
@@ -1,4 +1,4 @@
-export function gooseFilter (birds: string[]): string[] {
-  const geese: string[] = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
-  return birds.filter(item => !geese.includes(item))
+export function gooseFilter(birds: string[]): string[] {
+	const geese: string[] = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+	return birds.filter(item => !geese.includes(item))
 }
