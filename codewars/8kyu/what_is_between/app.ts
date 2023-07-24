5d51939f3e81b511c47da522830b7ad6c6eb2631
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-07-24 12:20:07 +0530

    format document

--- codewars/8kyu/what_is_between/app.ts
+++ codewars/8kyu/what_is_between/app.ts
@@ -1,17 +1,17 @@
 export function between(a: number, b: number): number[] {
-  let sol:number[] = []
-  for(let i = a; i<=b; i++){
-    sol.push(i)
-  }
-  return sol;
+	let sol: number[] = []
+	for (let i = a; i <= b; i++) {
+		sol.push(i)
+	}
+	return sol;
 }
 
 // alternate solution
 export function between2(a: number, b: number): number[] {
-  let list:number[] = []
-  while(a<=b){
-    list.push(a);
-    a++
-  }
-  return list;
+	let list: number[] = []
+	while (a <= b) {
+		list.push(a);
+		a++
+	}
+	return list;
 }
