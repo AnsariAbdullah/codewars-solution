285254ba77163d8abf476a65d2f32805b3e258e0
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-09-04 13:17:08 +0530

    add nice_array solution

---
+++ codewars/7kyu/nice_array/app.js
@@ -0,0 +1,12 @@
+export function isNice(arr) {
+  if(arr.length){
+    for(let i = 0; i < arr.length; i++){
+    let n = arr[i]
+      if (arr.indexOf(n-1)==-1 && arr.indexOf(n+1)==-1){return false}
+    }
+    return true
+  }else{
+    return false
+  }
+}
+
