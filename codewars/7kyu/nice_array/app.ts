a4b0eddd1942ee923be1db30ffabce7f9d7a035f
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-09-06 11:38:29 +0530

    format document

--- codewars/7kyu/nice_array/app.ts
+++ codewars/7kyu/nice_array/app.ts
@@ -1,16 +1,16 @@
-export function isNice(arr:number[]): boolean {
-  if(arr.length){
-    for(let i = 0; i < arr.length; i++){
-    let n = arr[i]
-      if (arr.indexOf(n-1)==-1 && arr.indexOf(n+1)==-1){return false}
-    }
-    return true
-  }else{
-    return false
-  }
+export function isNice(arr: number[]): boolean {
+	if (arr.length) {
+		for (let i = 0; i < arr.length; i++) {
+			let n = arr[i]
+			if (arr.indexOf(n - 1) == -1 && arr.indexOf(n + 1) == -1) { return false }
+		}
+		return true
+	} else {
+		return false
+	}
 }
 
 // Alternate solution
-export function isNiceAlt(arr:number[]): boolean {
-  return arr.length > 0 ? arr.every(item => arr.includes(item-1) || arr.includes(item+1)) : false
+export function isNiceAlt(arr: number[]): boolean {
+	return arr.length > 0 ? arr.every(item => arr.includes(item - 1) || arr.includes(item + 1)) : false
 }
