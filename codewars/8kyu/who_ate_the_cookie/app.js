a3806f41c82dcedf38b780e75df5df877b21c72b
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-09-13 11:24:06 +0530

    optimize

--- codewars/8kyu/who_ate_the_cookie/app.js
+++ codewars/8kyu/who_ate_the_cookie/app.js
@@ -1,5 +1,3 @@
-function cookie(x){
-  return typeof x==="string" ? "Who ate the last cookie? It was Zach!" :
-  typeof x==="number" ? "Who ate the last cookie? It was Monica!" :
-  "Who ate the last cookie? It was the dog!"
+function cookie(x) {
+	return `Who ate the last cookie? It was ${typeof x === "string" ? "Zach" : typeof x === "number" ? "Monica" : "the dog"}!`
 }
