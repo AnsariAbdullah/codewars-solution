3631b2f673ee38c0297987f706edd40befcd98ab
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-09-23 13:12:45 +0530

    optimize code

--- codewars/8kyu/who_ate_the_cookie/app.ts
+++ codewars/8kyu/who_ate_the_cookie/app.ts
@@ -1,3 +1,4 @@
 function cookie(x: string | number): string {
-	return `Who ate the last cookie? It was ${typeof x === "string" ? "Zach" : typeof x === "number" ? "Monica" : "the dog"}!`;
+	let who: string = typeof x === "string" ? "Zach" : typeof x === "number" ? "Monica" : "the dog"
+  return `Who ate the last cookie? It was ${who}!`
 }
