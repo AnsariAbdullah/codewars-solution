97cf82544745e27e938d848a5486dd624c43f00c
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-09-28 11:43:12 +0530

    format document

--- codewars/8kyu/getNumberFromString/app.js
+++ codewars/8kyu/getNumberFromString/app.js
@@ -1,17 +1,17 @@
 function getNumberFromString(s) {
-  const digitString = s.replace(/\D/g, '');
-  
-  return parseInt(digitString)
+	const digitString = s.replace(/\D/g, '');
+
+	return parseInt(digitString)
 }
 
 // alternate solution
 function getNumberFromString(s) {
-  let digitString = "";
+	let digitString = "";
 
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] >= "0" && s[i] <= "9") {
-      digitString += s[i];
-    }
-  }
-  return parseInt(digitString);
+	for (let i = 0; i < s.length; i++) {
+		if (s[i] >= "0" && s[i] <= "9") {
+			digitString += s[i];
+		}
+	}
+	return parseInt(digitString);
 }
