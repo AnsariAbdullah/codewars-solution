9b6372cfc7db9e4cd191cc989a639972eccb3967
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-10-02 11:48:45 +0530

    add number_pairs solution

---
+++ codewars/7kyu/number_pairs/app.js
@@ -0,0 +1,8 @@
+function getLargerNumbers(a, b) {
+  if (a.length !== b.length) {
+    throw new Error("Arrays must have the same length");
+  }
+  const result = a.map((num1, index) => Math.max(num1, b[index]));
+
+  return result;
+}
