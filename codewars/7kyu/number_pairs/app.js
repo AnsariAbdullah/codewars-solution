aab60e82cab241c4cc9e75bec6a4c02c99c66aa0
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-10-03 11:41:10 +0530

    add alternate solution

--- codewars/7kyu/number_pairs/app.js
+++ codewars/7kyu/number_pairs/app.js
@@ -6,3 +6,13 @@ function getLargerNumbers(a, b) {
 
   return result;
 }
+
+
+// alternate solution
+function getLargerNumbers(a, b) {
+  let newArray= [];
+  for(let i=0; i<a.length; i++){
+    newArray.push(Math.max(a[i], b[i]))
+  }
+  return newArray;
+}
