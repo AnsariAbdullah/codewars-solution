8d352c655433189c67aa06acf39723499ea4f685
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-10-03 11:42:33 +0530

    format document

--- codewars/7kyu/number_pairs/app.js
+++ codewars/7kyu/number_pairs/app.js
@@ -1,18 +1,18 @@
 function getLargerNumbers(a, b) {
-  if (a.length !== b.length) {
-    throw new Error("Arrays must have the same length");
-  }
-  const result = a.map((num1, index) => Math.max(num1, b[index]));
+	if (a.length !== b.length) {
+		throw new Error("Arrays must have the same length");
+	}
+	const result = a.map((num1, index) => Math.max(num1, b[index]));
 
-  return result;
+	return result;
 }
 
 
 // alternate solution
 function getLargerNumbers(a, b) {
-  let newArray= [];
-  for(let i=0; i<a.length; i++){
-    newArray.push(Math.max(a[i], b[i]))
-  }
-  return newArray;
+	let newArray = [];
+	for (let i = 0; i < a.length; i++) {
+		newArray.push(Math.max(a[i], b[i]))
+	}
+	return newArray;
 }
