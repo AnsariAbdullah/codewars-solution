bf7f2cd6a64d6e58446bd9088b7c6526610258ce
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-10-27 14:37:32 +0530

    format document

--- codewars/7kyu/help_suzuki_rake_his_garden/app.js
+++ codewars/7kyu/help_suzuki_rake_his_garden/app.js
@@ -1,14 +1,14 @@
 function rakeGarden(garden) {
-  let word = garden.split(' ');
-  for(let i=0; i<word.length; i++){
-    if(word[i] !== 'rock' && word[i] !== 'gravel'){
-      word[i] = 'gravel'
-    }
-  }
-  return word.join(' ');
+	let word = garden.split(' ');
+	for (let i = 0; i < word.length; i++) {
+		if (word[i] !== 'rock' && word[i] !== 'gravel') {
+			word[i] = 'gravel'
+		}
+	}
+	return word.join(' ');
 }
 
 // alternate solution
 function rakeGarden(garden) {
-  return garden.split(' ').map(item => item === 'rock' ? 'rock' : 'gravel').join(' ');
+	return garden.split(' ').map(item => item === 'rock' ? 'rock' : 'gravel').join(' ');
 }
--- codewars/7kyu/help_suzuki_rake_his_garden/app.ts
+++ codewars/7kyu/help_suzuki_rake_his_garden/app.ts
@@ -1,14 +1,14 @@
-export function rakeGarden(garden:string):string {
-  let word = garden.split(' ')
-  for(let i=0; i<word.length; i++){
-    if(word[i] !== 'rock' && word[i] !=='gravel'){
-      word[i] = 'gravel'
-    }
-  }
-  return word.join(' ')
+export function rakeGarden(garden: string): string {
+	let word = garden.split(' ')
+	for (let i = 0; i < word.length; i++) {
+		if (word[i] !== 'rock' && word[i] !== 'gravel') {
+			word[i] = 'gravel'
+		}
+	}
+	return word.join(' ')
 }
 
 // alternate solution
-export function rakeGarden2(garden:string):string {
-  return garden.split(' ').map(item => item === 'rock' ? 'rock' : 'gravel').join(' ')
+export function rakeGarden2(garden: string): string {
+	return garden.split(' ').map(item => item === 'rock' ? 'rock' : 'gravel').join(' ')
 }
