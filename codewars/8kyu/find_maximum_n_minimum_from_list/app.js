491c6d58a1c8db03d44d63dfa268651c4a45431e
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-11-02 11:26:26 +0530

    format document

--- codewars/8kyu/find_maximum_n_minimum_from_list/app.js
+++ codewars/8kyu/find_maximum_n_minimum_from_list/app.js
@@ -1,20 +1,18 @@
-var min = function(list){
-    
+var min = function (list) {
 	return Math.min(...list);
 }
 
-var max = function(list){
-	
+var max = function (list) {
 	return Math.max(...list);
 }
 
 // alternate solution
-var min = function(list){
-	let arr = list.sort((a, b) => a-b)
+var min = function (list) {
+	let arr = list.sort((a, b) => a - b)
 	return arr[0];
 }
 
-var max = function(list){
-	let arr = list.sort((a, b) => b-a)
+var max = function (list) {
+	let arr = list.sort((a, b) => b - a)
 	return arr[0];
 }
