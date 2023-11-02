cc527bb5d66e5b79d11a22d3ae2ad6ce5440bd85
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-11-02 11:27:42 +0530

    add alternate solution

--- codewars/8kyu/find_maximum_n_minimum_from_list/app.ts
+++ codewars/8kyu/find_maximum_n_minimum_from_list/app.ts
@@ -5,3 +5,14 @@ const min = (list: number[]): number => {
 const max = (list: number[]): number => {
 	return Math.max(...list);
 }
+
+// alternate solution
+const min = (list: number[]): number => {
+	let arr = list.sort((a, b) => a - b);
+	return arr[0];
+}
+
+const max = (list: number[]): number => {
+	let arr = list.sort((a, b) => b - a);
+	return arr[0];
+}
