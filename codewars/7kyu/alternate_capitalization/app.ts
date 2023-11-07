305de0667461168571e57d8013fc3db7c6014978
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-11-07 12:05:20 +0530

    format document

--- codewars/7kyu/alternate_capitalization/app.ts
+++ codewars/7kyu/alternate_capitalization/app.ts
@@ -1,22 +1,22 @@
 export function capitalize(s: string): [string, string] {
-  const word: string = s.toLowerCase();
-  let oddCaps: string = '';
-  let evenCaps: string = '';
-  for (let i: number = 0; i < word.length; i++) {
-    if (i % 2 == 0) {
-      oddCaps += word[i].toUpperCase();
-      evenCaps += word[i];
-    } else {
-      oddCaps += word[i];
-      evenCaps += word[i].toUpperCase();
-    }
-  }
-  return [oddCaps, evenCaps];
+	const word: string = s.toLowerCase();
+	let oddCaps: string = '';
+	let evenCaps: string = '';
+	for (let i: number = 0; i < word.length; i++) {
+		if (i % 2 == 0) {
+			oddCaps += word[i].toUpperCase();
+			evenCaps += word[i];
+		} else {
+			oddCaps += word[i];
+			evenCaps += word[i].toUpperCase();
+		}
+	}
+	return [oddCaps, evenCaps];
 }
 
 // alternate solution
 function capitalize(s: string): [string, string] {
-  let odd: string = s.split('').map((l, i) => i % 2 == 0 ? l.toUpperCase() : l).join('');
-  let even: string = s.split('').map((l, i) => i % 2 == 0 ? l : l.toUpperCase()).join('');
-  return [odd, even];
+	let odd: string = s.split('').map((l, i) => i % 2 == 0 ? l.toUpperCase() : l).join('');
+	let even: string = s.split('').map((l, i) => i % 2 == 0 ? l : l.toUpperCase()).join('');
+	return [odd, even];
 }
