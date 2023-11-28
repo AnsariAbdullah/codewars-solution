e05a5af356156988d85773d2762a838d8db60505
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-11-28 13:50:10 +0530

    format document

--- codewars/8kyu/the_if_function/app.js
+++ codewars/8kyu/the_if_function/app.js
@@ -1,12 +1,12 @@
 function _if(bool, func1, func2) {
-  if(bool){
-    func1()
-  }else{
-    func2()
-  }
+	if (bool) {
+		func1()
+	} else {
+		func2()
+	}
 }
 
 // alternate solution
 function _if(bool, func1, func2) {
-  bool ? func1() : func2()
+	bool ? func1() : func2()
 }
