7bca4d629668e25498762a4f3af8dd90f5a0c946
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-12-06 11:45:43 +0530

    format document

--- codewars/8kyu/flickSwitch/app.js
+++ codewars/8kyu/flickSwitch/app.js
@@ -1,19 +1,19 @@
-function flickSwitch(arr){
-  let flick = true;
-  return arr.map(val => val==='flick' ? flick = !flick : flick)
+function flickSwitch(arr) {
+	let flick = true;
+	return arr.map(val => val === 'flick' ? flick = !flick : flick)
 }
 
 // alternate solution
-function flickSwitch(arr){
-  let resultArr = []
-  let boolVal = true;
-  arr.forEach(item => {
-    if(item !== 'flick'){
-      resultArr.push(boolVal)
-    }else{
-      boolVal = !boolVal;
-      resultArr.push(boolVal)
-    }
-  })
-  return resultArr
+function flickSwitch(arr) {
+	let resultArr = []
+	let boolVal = true;
+	arr.forEach(item => {
+		if (item !== 'flick') {
+			resultArr.push(boolVal)
+		} else {
+			boolVal = !boolVal;
+			resultArr.push(boolVal)
+		}
+	})
+	return resultArr
 }
