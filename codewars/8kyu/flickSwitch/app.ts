512d5f2928eab7dbecf8d08ab27640e555256a4b
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-12-07 11:34:52 +0530

    add alternate solution

--- codewars/8kyu/flickSwitch/app.ts
+++ codewars/8kyu/flickSwitch/app.ts
@@ -2,3 +2,18 @@ export function flickSwitch(arr: string[]): boolean[] {
   let flick: boolean = true;
   return arr.map((val: string) => val === 'flick' ? flick = !flick : flick);
 }
+
+// alternate solution
+function flickSwitch2(arr: string[]): boolean[] {
+	let resultArr: boolean[] = []
+	let boolVal: boolean = true;
+	arr.forEach(item => {
+		if (item !== 'flick') {
+			resultArr.push(boolVal)
+		} else {
+			boolVal = !boolVal;
+			resultArr.push(boolVal)
+		}
+	})
+	return resultArr
+}
