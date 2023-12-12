15748abcdc4e3d44db717da1f4555f358d08358f
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-12-12 11:34:11 +0530

    format document

--- codewars/8kyu/validation_of_a_username_with_regex/app.ts
+++ codewars/8kyu/validation_of_a_username_with_regex/app.ts
@@ -1,6 +1,5 @@
 export function validateUsr(username: string): boolean {
-  const usernameRegex: RegExp = /^[a-z0-9_]{4,16}$/;
-
-  const res: boolean = usernameRegex.test(username);
-  return res;
+	const usernameRegex: RegExp = /^[a-z0-9_]{4,16}$/;
+	const res: boolean = usernameRegex.test(username);
+	return res;
 }
