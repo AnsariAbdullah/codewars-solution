04c8dd3f716b2381d473d158980133aee9c331c6
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-01-03 13:28:28 +0530

    format document

--- codewars/7kyu/simple_remove_duplicates/app.js
+++ codewars/7kyu/simple_remove_duplicates/app.js
@@ -1,16 +1,16 @@
 function solve(arr) {
-  const lastOccurrence = {};
-  const result = [];
-  for (let i = arr.length - 1; i >= 0; i--) {
-      if (!(arr[i] in lastOccurrence)) {
-          result.push(arr[i]);
-          lastOccurrence[arr[i]] = i;
-      }
-  }
-  return result.reverse();
+	const lastOccurrence = {};
+	const result = [];
+	for (let i = arr.length - 1; i >= 0; i--) {
+		if (!(arr[i] in lastOccurrence)) {
+			result.push(arr[i]);
+			lastOccurrence[arr[i]] = i;
+		}
+	}
+	return result.reverse();
 }
 
 // alternate solution
 function solve(arr) {
-  return [...new Set(arr.reverse())].reverse()
+	return [...new Set(arr.reverse())].reverse()
 }
