585cbf9c89dfa9f5099c800180c7099e30c4e43b
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-01-08 13:25:29 +0530

    format document

--- codewars/8kyu/object_oriented_piracy/app.js
+++ codewars/8kyu/object_oriented_piracy/app.js
@@ -1,11 +1,11 @@
 class Ship {
-  constructor(draft, crew) {
-    this.draft = draft
-    this.crew = crew
-    this.crewWeight = crew * 1.5
-  }
-  
-  isWorthIt = function (){
-    return this.draft - this.crewWeight > 20
-  }
+	constructor(draft, crew) {
+		this.draft = draft
+		this.crew = crew
+		this.crewWeight = crew * 1.5
+	}
+
+	isWorthIt = function () {
+		return this.draft - this.crewWeight > 20
+	}
 }
