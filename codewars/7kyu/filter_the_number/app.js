a48e5f72eb3e680594eccfdb50151aa8758a81f7
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-01-16 15:39:55 +0530

    format document

--- codewars/7kyu/filter_the_number/app.js
+++ codewars/7kyu/filter_the_number/app.js
@@ -1,9 +1,9 @@
-var filterString = function(value) {
-  let num = ''
-  for(let i = 0; i<value.length; i++){
-    if(/\d+/g.test(value[i])){
-      num += value[i]
-    }
-  }
-  return Number(num)
+var filterString = function (value) {
+	let num = ''
+	for (let i = 0; i < value.length; i++) {
+		if (/\d+/g.test(value[i])) {
+			num += value[i]
+		}
+	}
+	return Number(num)
 }
