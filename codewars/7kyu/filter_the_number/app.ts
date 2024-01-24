17df58dd70e52666d33a53450aa78ff3481bdb62
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-01-24 12:11:13 +0530

    format document

--- codewars/7kyu/filter_the_number/app.ts
+++ codewars/7kyu/filter_the_number/app.ts
@@ -1,14 +1,14 @@
 export const filterString = (value: string): number => {
-  return +value.replace(/\D/g, '');
+	return +value.replace(/\D/g, '');
 }
 
 // alternate solution
 export const altfilterString = (value: string): number => {
-  let num = '';
-  for (let i = 0; i < value.length; i++) {
-    if (/\d+/g.test(value[i])) {
-      num += value[i];
-    }
-  }
-  return Number(num);
+	let num = '';
+	for (let i = 0; i < value.length; i++) {
+		if (/\d+/g.test(value[i])) {
+			num += value[i];
+		}
+	}
+	return Number(num);
 }
