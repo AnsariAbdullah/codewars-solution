304e2a417286f8be6886142667559431b446be0d
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-02-01 12:13:54 +0530

    format document

--- codewars/8kyu/a_letters_best_friend/app.ts
+++ codewars/8kyu/a_letters_best_friend/app.ts
@@ -1,6 +1,6 @@
 export function bestFriend(txt: string, a: string, b: string): boolean {
-  for(let i=0; i<txt.length; i++){
-    if(txt[i] == a && txt[i+1] !== b) return false
-  }
-  return true;
+	for (let i = 0; i < txt.length; i++) {
+		if (txt[i] == a && txt[i + 1] !== b) return false
+	}
+	return true;
 }
