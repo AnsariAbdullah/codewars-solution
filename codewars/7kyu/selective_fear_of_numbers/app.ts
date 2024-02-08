99b510c2d043093f4e4b68fb7c4fee8176436678
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-02-08 16:21:51 +0530

    add ts solution

---
+++ codewars/7kyu/selective_fear_of_numbers/app.ts
@@ -0,0 +1,23 @@
+export function amIAfraid(day: string, num: number): boolean {
+  if(day == "Monday" && num == 12){
+    return true
+  }else if(day == "Tuesday" && num > 95){
+    return true
+  }else if(day == "Wednesday" && num == 34){
+    return true
+  }else if(day == "Thursday" && num == 0){
+    return true
+  }else if(day == "Friday"){
+    if(num%2==0){
+      return true
+    }else{
+      return false
+    }
+  }else if(day == "Saturday" && num === 56){
+    return true
+  }else if(day == "Sunday" && (num === 666 || num === -666 )){
+    return true
+  }else{
+    return false
+  }
+}
