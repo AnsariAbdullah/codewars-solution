a78cf385772dacd1fee7737b1715f710bc2969b8
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-02-24 13:35:44 +0530

    formatc document

--- codewars/7kyu/consecutive_items/app.js
+++ codewars/7kyu/consecutive_items/app.js
@@ -1,9 +1,8 @@
 function consecutive(arr, a, b) {
-  let indexofFirst = arr.indexOf(a);
-  let indexofSecond = arr.indexOf(b);
-  if(indexofSecond === indexofFirst + 1 || indexofFirst === indexofSecond + 1){
-    return true
-  }
-  return false
+	let indexofFirst = arr.indexOf(a);
+	let indexofSecond = arr.indexOf(b);
+	if (indexofSecond === indexofFirst + 1 || indexofFirst === indexofSecond + 1) {
+		return true
+	}
+	return false
 }
-
