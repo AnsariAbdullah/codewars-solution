f4b869416eafb0e26d3b23688b029fc242636945
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-02-27 12:51:42 +0530

    add ts solution

---
+++ codewars/7kyu/consecutive_items/app.ts
@@ -0,0 +1,8 @@
+export function consecutive(arr: number[], a: number, b: number): boolean {
+  let indexofFirst: number = arr.indexOf(a);
+  let indexofSecond: number = arr.indexOf(b);
+  if (indexofSecond === indexofFirst + 1 || indexofFirst === indexofSecond + 1) {
+    return true;
+  }
+  return false;
+}
