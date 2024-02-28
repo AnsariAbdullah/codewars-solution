2b78ebaa08945f82f42d46316b247970430e5611
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-02-28 15:30:45 +0530

    optimize

--- codewars/7kyu/consecutive_items/app.ts
+++ codewars/7kyu/consecutive_items/app.ts
@@ -1,8 +1,3 @@
-export function consecutive(arr: number[], a: number, b: number): boolean {
-	let indexofFirst: number = arr.indexOf(a);
-	let indexofSecond: number = arr.indexOf(b);
-	if (indexofSecond === indexofFirst + 1 || indexofFirst === indexofSecond + 1) {
-		return true;
-	}
-	return false;
+export function consecutive(arr, a, b) {
+	return arr.indexOf(a) === arr.indexOf(b) + 1 || arr.indexOf(b) === arr.indexOf(a) + 1
 }
