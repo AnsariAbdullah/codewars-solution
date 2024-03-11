00f62acdabfce0aa65d8f78ddd48e7f974e45903
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-03-11 18:28:56 +0530

    format document

--- codewars/8kyu/enumerable_magic_1_true_for_all/app.js
+++ codewars/8kyu/enumerable_magic_1_true_for_all/app.js
@@ -1,8 +1,8 @@
-function all( arr, fun ){
-  for(let i=0; i<arr.length; i++){
-    if(!fun(arr[i])){
-      return false
-    }
-  }
-  return true
+function all(arr, fun) {
+	for (let i = 0; i < arr.length; i++) {
+		if (!fun(arr[i])) {
+			return false
+		}
+	}
+	return true
 }
