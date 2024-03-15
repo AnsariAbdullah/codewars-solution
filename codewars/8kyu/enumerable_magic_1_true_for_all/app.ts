7c40effd2c76a904e93ce46e6999764b02d1ddd3
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-03-15 22:38:47 +0530

    add ts solution

---
+++ codewars/8kyu/enumerable_magic_1_true_for_all/app.ts
@@ -0,0 +1,8 @@
+export function all(arr: any[], fun: (item: any) => boolean): boolean {
+  for(let i=0; i<arr.length; i++){
+    if(!fun(arr[i])){
+      return false;
+    }
+  }
+  return true;
+}
