d7cd30844354cb399601bad00cbe3441cbe023b2
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-03-16 11:52:22 +0530

    optmize solution

--- codewars/8kyu/enumerable_magic_1_true_for_all/app.ts
+++ codewars/8kyu/enumerable_magic_1_true_for_all/app.ts
@@ -1,8 +1,3 @@
 export function all(arr: any[], fun: (item: any) => boolean): boolean {
-	for (let i = 0; i < arr.length; i++) {
-		if (!fun(arr[i])) {
-			return false;
-		}
-	}
-	return true;
+  return arr.every(item => fun(item))
 }
