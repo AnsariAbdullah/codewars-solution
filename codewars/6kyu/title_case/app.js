1280a40222ee43cce13af562838bb48141747801
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-03-28 22:19:01 +0530

    add title_case solution

---
+++ codewars/6kyu/title_case/app.js
@@ -0,0 +1,11 @@
+function titleCase(title, minorWords) {
+  let minorWord = typeof minorWords !== "undefined" ? minorWords.toLowerCase().split(' ') : [];
+  return title.toLowerCase().split(' ').map(function(v, i) {
+    if(v != "" && ( (minorWord.indexOf(v) === -1) || i == 0)) {
+      v = v.split('');
+      v[0] = v[0].toUpperCase();
+      v = v.join('');
+    }
+    return v;
+  }).join(' ');
+}
