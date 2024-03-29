94217a48b97ba9cc366cecd01f79a73fd9253528
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-03-29 16:01:13 +0530

    format document

--- codewars/6kyu/replace_with_alphabet_position/app.ts
+++ codewars/6kyu/replace_with_alphabet_position/app.ts
@@ -1,12 +1,12 @@
 export function alphabetPosition(text: string): string {
-  const alphabet: string = 'abcdefghijklmnopqrstuvwxyz';
-  let result: string = '';
-  for (let i: number = 0; i < text.length; i++) {
-    const char: string = text[i].toLowerCase();
-    if (alphabet.includes(char)) {
-      result += (alphabet.indexOf(char) + 1) + ' ';
-    }
-  }
-  result = result.trim();
-  return result;
+	const alphabet: string = 'abcdefghijklmnopqrstuvwxyz';
+	let result: string = '';
+	for (let i: number = 0; i < text.length; i++) {
+		const char: string = text[i].toLowerCase();
+		if (alphabet.includes(char)) {
+			result += (alphabet.indexOf(char) + 1) + ' ';
+		}
+	}
+	result = result.trim();
+	return result;
 }
