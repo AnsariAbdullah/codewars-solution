e07e951fe5085beef399fe70763ba7d1c82c6272
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-04-03 01:43:42 +0530

    minor changes

--- codewars/8kyu/classy_classes/app.ts
+++ codewars/8kyu/classy_classes/app.ts
@@ -1,12 +1,10 @@
 class Person {
   name: string;
   age: number;
-
   constructor(name: string, age: number) {
     this.name = name;
     this.age = age;
   }
-
   get info(): string {
     return `${this.name}s age is ${this.age}`;
   }
