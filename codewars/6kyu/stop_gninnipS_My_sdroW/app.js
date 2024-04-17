95cd488fc9d4c649fb0dbd21cafcc2997db141e8
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-04-18 02:08:43 +0530

    add alternate solution

--- codewars/6kyu/stop_gninnipS_My_sdroW/app.js
+++ codewars/6kyu/stop_gninnipS_My_sdroW/app.js
@@ -3,3 +3,8 @@ function spinWords(string){
     return item.length >= 5 ? item.split('').reverse().join('') : item
   }).join(' ')
 }
+
+// alternate solution
+function spinWords(string){
+  return string.replace(/\w{5,}/g, (w) => w.split('').reverse().join(''))
+}
