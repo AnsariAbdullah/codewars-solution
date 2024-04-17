096683b7afaad617990835e4f57cd711f24a0647
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-04-18 02:09:00 +0530

    format document

--- codewars/6kyu/stop_gninnipS_My_sdroW/app.js
+++ codewars/6kyu/stop_gninnipS_My_sdroW/app.js
@@ -1,10 +1,10 @@
-function spinWords(string){
-  return string.split(' ').map(item => {
-    return item.length >= 5 ? item.split('').reverse().join('') : item
-  }).join(' ')
+function spinWords(string) {
+	return string.split(' ').map(item => {
+		return item.length >= 5 ? item.split('').reverse().join('') : item
+	}).join(' ')
 }
 
 // alternate solution
-function spinWords(string){
-  return string.replace(/\w{5,}/g, (w) => w.split('').reverse().join(''))
+function spinWords(string) {
+	return string.replace(/\w{5,}/g, (w) => w.split('').reverse().join(''))
 }
