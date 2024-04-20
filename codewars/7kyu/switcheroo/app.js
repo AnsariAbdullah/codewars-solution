644fdc13a973a7105c6e6f2a33acfa0f76d2cc85
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-04-20 13:00:34 +0530

    format document

--- codewars/7kyu/switcheroo/app.js
+++ codewars/7kyu/switcheroo/app.js
@@ -1,32 +1,32 @@
-function switcheroo(x){
-  let str = ''
-  for(let i=0; i<x.length; i++){
-    if(x[i] === 'a' || x[i] === 'b'){
-      str += x[i] === 'a' ? 'b' : 'a'
-    }else{
-      str += x[i]
-    }
-  }
-  return str;
+function switcheroo(x) {
+	let str = ''
+	for (let i = 0; i < x.length; i++) {
+		if (x[i] === 'a' || x[i] === 'b') {
+			str += x[i] === 'a' ? 'b' : 'a'
+		} else {
+			str += x[i]
+		}
+	}
+	return str;
 }
 
 // alternate solution
-function switcheroo(x){
-  return x.split('').map(item => {
-    if(item === 'a'){
-      return 'b'
-    }
-    if(item === 'b'){
-      return 'a'
-    }
-    if(item === 'c'){
-      return 'c'
-    }
-  }).join('')
+function switcheroo(x) {
+	return x.split('').map(item => {
+		if (item === 'a') {
+			return 'b'
+		}
+		if (item === 'b') {
+			return 'a'
+		}
+		if (item === 'c') {
+			return 'c'
+		}
+	}).join('')
 }
 
 
 // alternate soltuion
-function switcheroo(x){
-  return x.replace(/[ab]/g, x => x=='a'?'b':'a')
+function switcheroo(x) {
+	return x.replace(/[ab]/g, x => x == 'a' ? 'b' : 'a')
 }
