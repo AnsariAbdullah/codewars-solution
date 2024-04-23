282d4f7feb4f744ab055a434f32fcb12b049c17f
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-04-24 00:46:50 +0530

    add alternate solution

--- codewars/6kyu/stop_gninnipS_My_sdroW/app.ts
+++ codewars/6kyu/stop_gninnipS_My_sdroW/app.ts
@@ -3,3 +3,9 @@ export function spinWords(words: string): string {
     return item.length >= 5 ? item.split('').reverse().join('') : item
   }).join(' ')
 }
+
+
+// alternate solution
+export function spinWordsAlt(words: string): string {
+  return words.replace(/\w{5,}/g, (w: string)=> w.length >=5 ? w.split('').reverse().join('') : w)
+}
