61a1f68b5c80dad2983d98dc71b0d78413c483cc
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-04-27 16:23:13 +0530

    format document

--- codewars/7kyu/find_the_vowels/app.js
+++ codewars/7kyu/find_the_vowels/app.js
@@ -1,20 +1,20 @@
 function vowelIndices(word) {
-  const vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
-  let result = []
-  const words = word.toLowerCase();
-  for(let i=0; i<words.length; i++){
-    if(vowels.includes(words[i])){
-      result.push(i+1)
-    }
-  }
-  return result
+	const vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
+	let result = []
+	const words = word.toLowerCase();
+	for (let i = 0; i < words.length; i++) {
+		if (vowels.includes(words[i])) {
+			result.push(i + 1)
+		}
+	}
+	return result
 }
 
 // alternate solution
-function vowelIndices(word){
-  let result = [];
-  for(let i=0; i<word.length; i++){
-    if(/[aeiouy]/i.test(word[i])) result.push(i+1)
-  }
-  return result;
+function vowelIndices(word) {
+	let result = [];
+	for (let i = 0; i < word.length; i++) {
+		if (/[aeiouy]/i.test(word[i])) result.push(i + 1)
+	}
+	return result;
 }
