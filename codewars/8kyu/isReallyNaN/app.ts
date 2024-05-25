1046447247ffbc0fa3cc3bc9d8b5bdb7232b86b2
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-05-25 18:53:55 +0530

    add alternate solution

--- codewars/8kyu/isReallyNaN/app.ts
+++ codewars/8kyu/isReallyNaN/app.ts
@@ -1,3 +1,8 @@
 export function isReallyNaN(val: any): boolean {
   return Number.isNaN(val);
 };
+
+// alternate solution
+export function isReallyNaNAlt(val: any): boolean {
+  return typeof val === 'number' && isNaN(val)
+};
