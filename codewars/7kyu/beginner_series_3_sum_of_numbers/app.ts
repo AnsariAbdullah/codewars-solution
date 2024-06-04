77672de01d4abfeb8a9f881715eff0df7b976782
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-06-04 09:09:23 +0530

    format document

--- codewars/7kyu/beginner_series_3_sum_of_numbers/app.ts
+++ codewars/7kyu/beginner_series_3_sum_of_numbers/app.ts
@@ -1,24 +1,24 @@
 export function getSum(a: number, b: number): number {
-  const small = a < b ? a : b;
-  const bigger = a > b ? a : b;
-  let result = 0;
-  for(let i=small; i<= bigger; i++){
-    result += i
-  }
-  return result;
+	const small = a < b ? a : b;
+	const bigger = a > b ? a : b;
+	let result = 0;
+	for (let i = small; i <= bigger; i++) {
+		result += i
+	}
+	return result;
 }
 
 // alternate solution
 function getSumAlt(a: number, b: number): number {
-  let temp = 0;
-  if (a < b) {
-    while (a <= b) {
-      temp += a++;
-    }
-  } else {
-    while (a >= b) {
-      temp += a--;
-    }
-  }
-  return temp;
+	let temp = 0;
+	if (a < b) {
+		while (a <= b) {
+			temp += a++;
+		}
+	} else {
+		while (a >= b) {
+			temp += a--;
+		}
+	}
+	return temp;
 }
