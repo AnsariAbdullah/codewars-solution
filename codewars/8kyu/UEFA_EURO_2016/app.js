6ea71170e43e0ad13c92d9705e688a8320a95415
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-06-05 21:15:19 +0530

    add alternate solution

--- codewars/8kyu/UEFA_EURO_2016/app.js
+++ codewars/8kyu/UEFA_EURO_2016/app.js
@@ -9,3 +9,13 @@ function uefaEuro2016(teams, scores){
   }
   return `At match ${teams[0]} - ${teams[1]}, ${result}`
 }
+
+// alternate solution
+function uefaEuro2016(teams, scores){
+  const [team1, team2] = teams;
+  const [score1, score2] = scores;
+  
+  const winner = score1 > score2 ? team1 : team2
+  return score1 === score2 ? `At match ${team1} - ${team2}, teams played draw.`
+    : `At match ${team1} - ${team2}, ${winner} won!`
+}
