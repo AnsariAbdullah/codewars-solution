a342949cf3521aeb3b2fa8b47ee97516a7af5ae1
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-06-10 15:03:31 +0530

    format document

--- codewars/8kyu/UEFA_EURO_2016/app.js
+++ codewars/8kyu/UEFA_EURO_2016/app.js
@@ -1,21 +1,21 @@
-function uefaEuro2016(teams, scores){
-  let result = ''
-  if(scores[0]>scores[1]){
-    result = teams[0]+ " won!"
-  }else if(scores[0]<scores[1]){
-    result = teams[1]+ " won!"
-  }else{
-    result = "teams played draw."
-  }
-  return `At match ${teams[0]} - ${teams[1]}, ${result}`
+function uefaEuro2016(teams, scores) {
+	let result = ''
+	if (scores[0] > scores[1]) {
+		result = teams[0] + " won!"
+	} else if (scores[0] < scores[1]) {
+		result = teams[1] + " won!"
+	} else {
+		result = "teams played draw."
+	}
+	return `At match ${teams[0]} - ${teams[1]}, ${result}`
 }
 
 // alternate solution
-function uefaEuro2016(teams, scores){
-  const [team1, team2] = teams;
-  const [score1, score2] = scores;
-  
-  const winner = score1 > score2 ? team1 : team2
-  return score1 === score2 ? `At match ${team1} - ${team2}, teams played draw.`
-    : `At match ${team1} - ${team2}, ${winner} won!`
+function uefaEuro2016(teams, scores) {
+	const [team1, team2] = teams;
+	const [score1, score2] = scores;
+
+	const winner = score1 > score2 ? team1 : team2
+	return score1 === score2 ? `At match ${team1} - ${team2}, teams played draw.`
+		: `At match ${team1} - ${team2}, ${winner} won!`
 }
