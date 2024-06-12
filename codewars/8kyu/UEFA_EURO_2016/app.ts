ad4e7b5f35e10b80dbfa16fc34ac6d4f70a01a33
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-06-12 13:30:40 +0530

    add alterrnate solution

--- codewars/8kyu/UEFA_EURO_2016/app.ts
+++ codewars/8kyu/UEFA_EURO_2016/app.ts
@@ -9,3 +9,14 @@ export function uefaEuro2016(teams: [string, string], scores: [number, number]):
   }
   return `At match ${teams[0]} - ${teams[1]}, ${result}`;
 }
+
+// alternate solution
+export function uefaEuro20162(teams: [string, string], scores: [number, number]): string {
+  const [team1, team2] = teams;
+  const [score1, score2] = scores;
+  
+  const winner = score1 > score2 ? team1 : team2;
+  return score1 === score2
+    ? `At match ${team1} - ${team2}, teams played draw.`
+    : `At match ${team1} - ${team2}, ${winner} won!`;
+}
