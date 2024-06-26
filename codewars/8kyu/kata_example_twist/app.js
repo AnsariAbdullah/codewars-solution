5b8518a431280c781399e69e7042e3d7cbd95d31
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-06-26 17:02:27 +0530

    add alternate solution

--- codewars/8kyu/kata_example_twist/app.js
+++ codewars/8kyu/kata_example_twist/app.js
@@ -1,3 +1,6 @@
 // add the value "codewars" to the websites array 1,000 times
 var websites = new Array(1000).fill("codewars")
 
+// alternate solution
+var websites = []
+while(websites.length < 1000) websites.push("codewars")
