04a30c04e2630c6e7f948b03fa9c51ee9e047574
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-07-01 15:20:46 +0530

    add alternate solution

--- codewars/8kyu/kata_example_twist/app.ts
+++ codewars/8kyu/kata_example_twist/app.ts
@@ -1,2 +1,12 @@
 // add the value "codewars" to the websites array 1,000 times
 const websites: string[] = new Array(1000).fill("codewars")
+
+// alternate solution
+const websites: string[] = []
+while (websites.length < 1000) websites.push("codewars")
+
+// alternate solution
+const websites: string[] = []
+for (let i = 0; i < 1000; i++) {
+  websites.push("codewars")
+}
