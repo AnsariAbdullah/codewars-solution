5291c72d6f1ae399ade2229871ca3271c30943c0
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-07-04 15:11:40 +0530

    format document

--- codewars/8kyu/tip_calculator/app.ts
+++ codewars/8kyu/tip_calculator/app.ts
@@ -1,19 +1,19 @@
 export const percentageCalc = (base: number, percentage: number): number => Math.ceil((base * percentage) / 100);
 
 export function calculateTip(amount: number, rating: string): number | string {
-  const ratings = rating.toLowerCase();
-  switch (ratings) {
-    case 'terrible':
-      return percentageCalc(amount, 0);
-    case 'poor':
-      return percentageCalc(amount, 5);
-    case 'good':
-      return percentageCalc(amount, 10);
-    case 'great':
-      return percentageCalc(amount, 15);
-    case 'excellent':
-      return percentageCalc(amount, 20);
-    default:
-      return "Rating not recognised";
-  }
+	const ratings = rating.toLowerCase();
+	switch (ratings) {
+		case 'terrible':
+			return percentageCalc(amount, 0);
+		case 'poor':
+			return percentageCalc(amount, 5);
+		case 'good':
+			return percentageCalc(amount, 10);
+		case 'great':
+			return percentageCalc(amount, 15);
+		case 'excellent':
+			return percentageCalc(amount, 20);
+		default:
+			return "Rating not recognised";
+	}
 }
