8c4c231a2b65d98a935556068dbfb59bc033298a
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-07-05 17:23:27 +0530

    format document

--- codewars/8kyu/simple_calculator/app.js
+++ codewars/8kyu/simple_calculator/app.js
@@ -1,18 +1,18 @@
-function calculator(a,b,sign){
-  if (Number.isInteger(a+b)) {
-    if(sign === "+"){
-      return a+b
-    }else if(sign === "-"){
-      return a-b
-    }else if(sign === "/"){
-      return a/b
-    }else if(sign === "*"){
-      return a*b
-    }else{
-      return "unknown value"
-    }
-  }else{
-    return "unknown value"
-  }
+function calculator(a, b, sign) {
+	if (Number.isInteger(a + b)) {
+		if (sign === "+") {
+			return a + b
+		} else if (sign === "-") {
+			return a - b
+		} else if (sign === "/") {
+			return a / b
+		} else if (sign === "*") {
+			return a * b
+		} else {
+			return "unknown value"
+		}
+	} else {
+		return "unknown value"
+	}
 
 }
