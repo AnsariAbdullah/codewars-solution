785077449de08d7d1f56a8ff9bae817bd29bef91
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-07-13 01:16:44 +0530

    format document

--- codewars/8kyu/power/app.js
+++ codewars/8kyu/power/app.js
@@ -1,8 +1,8 @@
-function numberToPower(number, power){
-  if(power === 0) return 1
-  let result = 1;
-  for(let i=0; i<power; i++){
-    result *= number
-  }
-  return result
+function numberToPower(number, power) {
+	if (power === 0) return 1
+	let result = 1;
+	for (let i = 0; i < power; i++) {
+		result *= number
+	}
+	return result
 }
