8dfe2b549974a3d78cb6b6c3b51a56a3f6fda8cb
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-07-16 22:14:23 +0530

    format document

--- codewars/8kyu/power/app.ts
+++ codewars/8kyu/power/app.ts
@@ -1,8 +1,8 @@
-export function numberToPower(number: number, power:number): number{
-  if(power === 0) return 1
-  let result = 1;
-  for(let i=0; i<power; i++){
-    result *= number
-  }
-  return result
+export function numberToPower(number: number, power: number): number {
+	if (power === 0) return 1
+	let result = 1;
+	for (let i = 0; i < power; i++) {
+		result *= number
+	}
+	return result
 }
