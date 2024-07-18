66844f7a90afec30f578492ad8cf38b9b52adc1c
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-07-18 13:57:14 +0530

    format document

--- codewars/8kyu/short_long_short/app.js
+++ codewars/8kyu/short_long_short/app.js
@@ -1,6 +1,6 @@
-function solution(a, b){
-  let aLenght = a.length;
-  let bLenght = b.length;
-  
-  return aLenght < bLenght ? `${a}${b}${a}` : `${b}${a}${b}`
+function solution(a, b) {
+	let aLenght = a.length;
+	let bLenght = b.length;
+
+	return aLenght < bLenght ? `${a}${b}${a}` : `${b}${a}${b}`
 }
