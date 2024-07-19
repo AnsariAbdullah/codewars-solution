15604506760d0dadc7ef32850a4657117acb5714
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-07-19 17:30:59 +0530

    add alternate solution

--- codewars/8kyu/short_long_short/app.js
+++ codewars/8kyu/short_long_short/app.js
@@ -1,3 +1,9 @@
 function solution(a, b){
   return a.length < b.length ? a + b + a : b + a + b
 }
+
+// alternate solution
+function solutionAlt(a, b){
+  let [short, long] = [a,b].sort((a, b) => a.length - b.length);
+  return `${short}${long}${short}`;
+}
