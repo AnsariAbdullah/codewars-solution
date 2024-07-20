12e5b775341febeb459b95e6c4abfcb0ce3460c5
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-07-20 14:52:26 +0530

    add alternate solution

--- codewars/8kyu/short_long_short/app.ts
+++ codewars/8kyu/short_long_short/app.ts
@@ -1,3 +1,9 @@
 export function shortLongShort(a:string, b:string) {
   return a.length < b.length ? a + b + a : b + a + b
 }
+
+// alternate solution
+export function shortLongShortAlt(a:string, b:string) {
+  let [short, long] = [a,b].sort((a, b) => a.length - b.length)
+  return `${short}${long}${short}`;
+}
