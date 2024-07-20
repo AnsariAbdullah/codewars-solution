fbf66a83be20fc669e8cba2b5edb63766618a8ab
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-07-20 14:50:34 +0530

    optimize solution

--- codewars/8kyu/short_long_short/app.ts
+++ codewars/8kyu/short_long_short/app.ts
@@ -1,6 +1,3 @@
-export function shortLongShort(a: string, b: string) {
-	let aLenght = a.length;
-	let bLenght = b.length;
-
-	return aLenght < bLenght ? `${a}${b}${a}` : `${b}${a}${b}`
+export function shortLongShort(a:string, b:string) {
+  return a.length < b.length ? a + b + a : b + a + b
 }
