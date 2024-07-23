323e63a84949816f8c69c55c9ddc1cf926bdc2aa
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-07-23 18:25:49 +0530

    optimize solution

--- codewars/7kyu/categorize_new_member/app.js
+++ codewars/7kyu/categorize_new_member/app.js
@@ -1,12 +1,3 @@
-function openOrSenior(data) {
-	let result = []
-	for (let i = 0; i < data.length; i++) {
-		const [age, handStatus] = data[i]
-		if (age > 54 && handStatus > 7) {
-			result.push("Senior")
-		} else {
-			result.push("Open")
-		}
-	}
-	return result;
+function openOrSenior(data){
+  return data.map(([age, handicapStatus]) => age>54 && handicapStatus > 7 ? "Senior" : "Open")
 }
