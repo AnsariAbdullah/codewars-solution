85b9c953a937a0311f88fb7a59f5fa62d90f56c0
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-07-23 18:24:51 +0530

    format document

--- codewars/7kyu/categorize_new_member/app.js
+++ codewars/7kyu/categorize_new_member/app.js
@@ -1,12 +1,12 @@
-function openOrSenior(data){
-  let result = []
-  for(let i=0; i<data.length; i++){
-    const [age, handStatus] = data[i]
-    if(age > 54 && handStatus > 7){
-      result.push("Senior")
-    }else{
-      result.push("Open")
-    }
-  }
-  return result;
+function openOrSenior(data) {
+	let result = []
+	for (let i = 0; i < data.length; i++) {
+		const [age, handStatus] = data[i]
+		if (age > 54 && handStatus > 7) {
+			result.push("Senior")
+		} else {
+			result.push("Open")
+		}
+	}
+	return result;
 }
