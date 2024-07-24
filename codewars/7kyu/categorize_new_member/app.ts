e1eb31042a3e77a7aaf169208357c926c5462090
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-07-24 16:37:53 +0530

    optimise solution

--- codewars/7kyu/categorize_new_member/app.ts
+++ codewars/7kyu/categorize_new_member/app.ts
@@ -1,12 +1,3 @@
 export function openOrSenior(data: [number, number][]): string[] {
-	let result: string[] = [];
-	for (let i = 0; i < data.length; i++) {
-		const [age, handStatus] = data[i];
-		if (age > 54 && handStatus > 7) {
-			result.push("Senior");
-		} else {
-			result.push("Open");
-		}
-	}
-	return result;
+  return data.map(([age, handicapStatus]) => (age > 54 && handicapStatus > 7 ? "Senior" : "Open"));
 }
