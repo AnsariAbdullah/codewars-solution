a341d1d187a008e32a839570af22548b2b3d66d6
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-07-26 15:57:17 +0530

    add highest_rank_number_in_an_array solution

---
+++ codewars/6kyu/highest_rank_number_in_an_array/app.js
@@ -0,0 +1,22 @@
+function highestRank(arr) {
+	const frequencyMap = {};
+
+	for (const num of arr) {
+			frequencyMap[num] = (frequencyMap[num] || 0) + 1;
+	}
+
+	let maxFrequency = 0;
+	let mostFrequent = -Infinity;
+
+	for (const num in frequencyMap) {
+			const frequency = frequencyMap[num];
+			const number = parseInt(num);
+			
+			if (frequency > maxFrequency || (frequency === maxFrequency && number > mostFrequent)) {
+					maxFrequency = frequency;
+					mostFrequent = number;
+			}
+	}
+
+	return mostFrequent;
+}
