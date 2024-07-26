163c188ffb7ba2648929045798a241e33a57e9f1
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-07-27 00:51:31 +0530

    add TS solution

---
+++ codewars/6kyu/highest_rank_number_in_an_array/app.ts
@@ -0,0 +1,22 @@
+export function highestRank(arr: number[]): number {
+	const frequencyMap: { [key: number]: number } = {};
+
+	for (const num of arr) {
+			frequencyMap[num] = (frequencyMap[num] || 0) + 1;
+	}
+
+	let maxFrequency = 0;
+	let mostFrequent = -Infinity;
+
+	for (const num in frequencyMap) {
+			const frequency = frequencyMap[num];
+			const number = parseInt(num);
+			
+			if (frequency > maxFrequency || (frequency === maxFrequency && number > mostFrequent)) {
+					maxFrequency = frequency;
+					mostFrequent = number;
+			}
+	}
+
+	return mostFrequent;
+}
