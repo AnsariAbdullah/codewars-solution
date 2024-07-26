1ec809241d3fd8a0120dbccd9761d65f2b9f63fe
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-07-26 15:58:25 +0530

    format document

--- codewars/6kyu/highest_rank_number_in_an_array/app.js
+++ codewars/6kyu/highest_rank_number_in_an_array/app.js
@@ -2,20 +2,20 @@ function highestRank(arr) {
 	const frequencyMap = {};
 
 	for (const num of arr) {
-			frequencyMap[num] = (frequencyMap[num] || 0) + 1;
+		frequencyMap[num] = (frequencyMap[num] || 0) + 1;
 	}
 
 	let maxFrequency = 0;
 	let mostFrequent = -Infinity;
 
 	for (const num in frequencyMap) {
-			const frequency = frequencyMap[num];
-			const number = parseInt(num);
-			
-			if (frequency > maxFrequency || (frequency === maxFrequency && number > mostFrequent)) {
-					maxFrequency = frequency;
-					mostFrequent = number;
-			}
+		const frequency = frequencyMap[num];
+		const number = parseInt(num);
+
+		if (frequency > maxFrequency || (frequency === maxFrequency && number > mostFrequent)) {
+			maxFrequency = frequency;
+			mostFrequent = number;
+		}
 	}
 
 	return mostFrequent;
