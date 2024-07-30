b3e60c703ad6c039958520aa19ecc01eaa3ff080
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-07-31 01:17:13 +0530

    format document

--- codewars/6kyu/data_reverse/app.js
+++ codewars/6kyu/data_reverse/app.js
@@ -1,10 +1,10 @@
 function dataReverse(data) {
-  const byteLength = 8;
-  const result = [];
-  for(let i=data.length - byteLength; i>=0; i -=byteLength){
-    for(let j=0; j<byteLength; j++){
-      result.push(data[i+j])
-    }
-  }
-  return result;
+	const byteLength = 8;
+	const result = [];
+	for (let i = data.length - byteLength; i >= 0; i -= byteLength) {
+		for (let j = 0; j < byteLength; j++) {
+			result.push(data[i + j])
+		}
+	}
+	return result;
 }
