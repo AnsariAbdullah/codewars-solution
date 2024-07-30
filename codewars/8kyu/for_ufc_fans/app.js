44fec91c84a55f2f02e4b7c6e02097c4086bdb7b
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-07-30 21:13:17 +0530

    minor changes

--- codewars/8kyu/for_ufc_fans/app.js
+++ codewars/8kyu/for_ufc_fans/app.js
@@ -2,5 +2,4 @@ var quote = function (fighter) {
 	return fighter.toLowerCase() == "george saint pierre" ?
 		"I am not impressed by your performance."
 		: "I'd like to take this chance to apologize.. To absolutely NOBODY!"
-
 };
