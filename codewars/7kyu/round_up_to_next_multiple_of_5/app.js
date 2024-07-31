3ed4bd0f883738bfed5864c3928e49eb4a44d2b0
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-08-01 01:33:21 +0530

    format document

--- codewars/7kyu/round_up_to_next_multiple_of_5/app.js
+++ codewars/7kyu/round_up_to_next_multiple_of_5/app.js
@@ -1,10 +1,10 @@
-function roundToNext5(n){
-  if (n % 5 === 0) {
-			return n;
+function roundToNext5(n) {
+	if (n % 5 === 0) {
+		return n;
 	}
 	if (n > 0) {
-			return n + (5 - (n % 5));
+		return n + (5 - (n % 5));
 	} else {
-			return n - (n % 5);
+		return n - (n % 5);
 	}
 }
