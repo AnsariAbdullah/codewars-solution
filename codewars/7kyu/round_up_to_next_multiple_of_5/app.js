e7c7165bae83ed4d83d04f2df5498803fa370036
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-08-02 00:46:12 +0530

    add alternate solution

--- codewars/7kyu/round_up_to_next_multiple_of_5/app.js
+++ codewars/7kyu/round_up_to_next_multiple_of_5/app.js
@@ -13,3 +13,9 @@ function roundToNext5(n) {
 function roundToNext5(n) {
 	return Math.ceil(n / 5) * 5
 }
+
+// alternate solution
+function roundToNext5(n){
+  while(n % 5 !== 0) n++;
+  return n
+}
