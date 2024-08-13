0a10904eb40b5fb0a431da9edcf22e5714a76da3
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-08-14 00:59:33 +0530

    add missing_letters solution

---
+++ fcc-basic/missing_letters/app.js
@@ -0,0 +1,14 @@
+function fearNotLetter(str) {
+  const alphab = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  
+  const fIndex = alphab.indexOf(str.charAt(0));
+  
+  for (let i = 0; i < str.length; i++) {
+    if (str.charAt(i) !== alphab[fIndex + i]) {
+      return alphab[fIndex + i];
+    }
+  }
+  return undefined;
+}
+
+fearNotLetter("stvwx");
