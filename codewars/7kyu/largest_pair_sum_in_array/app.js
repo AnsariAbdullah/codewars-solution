466ed8de2a93031f48e6dba354bddc544d40ab7b
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-08-16 15:16:02 +0530

    add optimized solution

--- codewars/7kyu/largest_pair_sum_in_array/app.js
+++ codewars/7kyu/largest_pair_sum_in_array/app.js
@@ -2,3 +2,9 @@ function largestPairSum(numbers) {
 	let num = numbers.sort((a, b) => b - a)
 	return num[0] + num[1]
 }
+
+// optimized
+function largestPairSum (numbers) {
+	numbers.sort((a, b) => b - a);
+	return numbers[0] + numbers[1];
+}
