91d6b24b6e891d333748e3ee0d162a22b2c15736
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-08-16 15:17:21 +0530

    remove duplicate code

--- codewars/7kyu/largest_pair_sum_in_array/app.js
+++ codewars/7kyu/largest_pair_sum_in_array/app.js
@@ -1,10 +1,4 @@
 function largestPairSum(numbers) {
-	let num = numbers.sort((a, b) => b - a)
-	return num[0] + num[1]
-}
-
-// optimized
-function largestPairSum(numbers) {
 	numbers.sort((a, b) => b - a);
 	return numbers[0] + numbers[1];
 }
