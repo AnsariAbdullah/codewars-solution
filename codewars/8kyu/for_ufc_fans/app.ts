81edefc5885a217cd43bb76f4cfe4bff22987933
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-08-19 16:52:05 +0530

    format document

--- codewars/8kyu/for_ufc_fans/app.ts
+++ codewars/8kyu/for_ufc_fans/app.ts
@@ -1,5 +1,5 @@
 export const quote = (fighter: string): string => {
-  return fighter.toLowerCase() === "george saint pierre"
-    ? "I am not impressed by your performance."
-    : "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+	return fighter.toLowerCase() === "george saint pierre"
+		? "I am not impressed by your performance."
+		: "I'd like to take this chance to apologize.. To absolutely NOBODY!";
 };
