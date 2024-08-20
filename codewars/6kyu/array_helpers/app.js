b180b481a95226ee591a025fa48684525fdb50bf
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-08-21 00:39:28 +0530

    format document

--- codewars/6kyu/array_helpers/app.js
+++ codewars/6kyu/array_helpers/app.js
@@ -24,46 +24,46 @@ Array.prototype.odd = function () {
 
 
 // alternate solution w/o map
-Array.prototype.square = function() {
-  const square = []
-  for(let i=0; i<this.length; i++){
-    square[i] = Math.pow(this[i], 2)
-  }
-  return square;
+Array.prototype.square = function () {
+	const square = []
+	for (let i = 0; i < this.length; i++) {
+		square[i] = Math.pow(this[i], 2)
+	}
+	return square;
 };
 
-Array.prototype.cube = function() {
-  const cube = []
-  for(let i=0; i<this.length; i++){
-    cube[i] = Math.pow(this[i], 3)
-  }
-  return cube;
+Array.prototype.cube = function () {
+	const cube = []
+	for (let i = 0; i < this.length; i++) {
+		cube[i] = Math.pow(this[i], 3)
+	}
+	return cube;
 };
 
-Array.prototype.average = function() {
-  return this.reduce((acc, x) => acc+x, 0) / this.length;
+Array.prototype.average = function () {
+	return this.reduce((acc, x) => acc + x, 0) / this.length;
 };
 
-Array.prototype.sum = function() {
-  let sum = 0
-  for(let i=0; i<this.length; i++){
-    sum += this[i]
-  }
-  return sum;
+Array.prototype.sum = function () {
+	let sum = 0
+	for (let i = 0; i < this.length; i++) {
+		sum += this[i]
+	}
+	return sum;
 };
 
-Array.prototype.even = function() {
-  const even = []
-  for(let i=0; i<this.length; i++){
-    if(this[i]%2===0) even.push(this[i])
-  }
-  return even;
+Array.prototype.even = function () {
+	const even = []
+	for (let i = 0; i < this.length; i++) {
+		if (this[i] % 2 === 0) even.push(this[i])
+	}
+	return even;
 };
 
-Array.prototype.odd = function() {
-  const odd = []
-  for(let i=0; i<this.length; i++){
-    if(this[i]%2 !== 0) odd.push(this[i])
-  }
-  return odd;
+Array.prototype.odd = function () {
+	const odd = []
+	for (let i = 0; i < this.length; i++) {
+		if (this[i] % 2 !== 0) odd.push(this[i])
+	}
+	return odd;
 };
