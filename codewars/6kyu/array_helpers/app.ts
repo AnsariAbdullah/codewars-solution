9afd25bb3487e9ac431b0e10226a80fc1a25fcab
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-08-24 14:35:10 +0530

    add ts solution

---
+++ codewars/6kyu/array_helpers/app.ts
@@ -0,0 +1,33 @@
+interface Array<T> {
+	square(): number[];
+	cube(): number[];
+	average(): number;
+	sum(): number;
+	even(): number[];
+	odd(): number[];
+}
+
+Array.prototype.square = function(): number[] {
+	return this.map(x => x * x);
+};
+
+Array.prototype.cube = function(): number[] {
+	return this.map(x => x * x * x);
+};
+
+Array.prototype.average = function(): number {
+	return this.length === 0 ? NaN : this.sum() / this.length;
+};
+
+Array.prototype.sum = function(): number {
+	return this.reduce((acc, x) => acc + x, 0);
+};
+
+Array.prototype.even = function(): number[] {
+	return this.filter(x => x % 2 === 0);
+};
+
+Array.prototype.odd = function(): number[] {
+	return this.filter(x => x % 2 !== 0);
+};
+
