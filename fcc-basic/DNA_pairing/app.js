e0189336da47b2dcc19fdb3922389d1801af5e69
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-08-26 16:35:56 +0530

    format document

--- fcc-basic/DNA_pairing/app.js
+++ fcc-basic/DNA_pairing/app.js
@@ -1,21 +1,21 @@
 function pairElement(str) {
-  let arr = []
-  for(let i=0; i<str.length; i++){
-    if(str[i] == "G" || str[i] == "C"){
-      if(str[i] == "G"){
-        arr.push(["G", "C"])
-      }else{
-        arr.push(["C", "G"])
-      }
-    }else{
-      if(str[i] == "A"){
-        arr.push(["A", "T"])
-      }else{
-        arr.push(["T", "A"])
-      }
-    }
-  }
-  return arr;
+	let arr = []
+	for (let i = 0; i < str.length; i++) {
+		if (str[i] == "G" || str[i] == "C") {
+			if (str[i] == "G") {
+				arr.push(["G", "C"])
+			} else {
+				arr.push(["C", "G"])
+			}
+		} else {
+			if (str[i] == "A") {
+				arr.push(["A", "T"])
+			} else {
+				arr.push(["T", "A"])
+			}
+		}
+	}
+	return arr;
 }
 
 pairElement("GCG");
