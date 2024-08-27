b6ddb322c2bcf06a48c1234fc303154ffa24237c
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-08-28 01:28:29 +0530

    format document

--- codewars/6kyu/data_reverse/app.ts
+++ codewars/6kyu/data_reverse/app.ts
@@ -1,15 +1,15 @@
 function reverseBytes(data: number[]): number[] {
 	if (data.length % 8 !== 0) {
-			throw new Error("Data length must be a multiple of 8.");
+		throw new Error("Data length must be a multiple of 8.");
 	}
 
 	const byteLength = 8;
 	const result: number[] = [];
 
 	for (let i = data.length - byteLength; i >= 0; i -= byteLength) {
-			for (let j = 0; j < byteLength; j++) {
-					result.push(data[i + j]);
-			}
+		for (let j = 0; j < byteLength; j++) {
+			result.push(data[i + j]);
+		}
 	}
 
 	return result;
