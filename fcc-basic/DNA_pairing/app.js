2ea8032db20bc47906d97db6cc16e12245397508
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-08-27 16:14:17 +0530

    optimize solution

--- fcc-basic/DNA_pairing/app.js
+++ fcc-basic/DNA_pairing/app.js
@@ -1,21 +1,19 @@
 function pairElement(str) {
-	let arr = []
-	for (let i = 0; i < str.length; i++) {
-		if (str[i] == "G" || str[i] == "C") {
-			if (str[i] == "G") {
-				arr.push(["G", "C"])
-			} else {
-				arr.push(["C", "G"])
-			}
-		} else {
-			if (str[i] == "A") {
-				arr.push(["A", "T"])
-			} else {
-				arr.push(["T", "A"])
-			}
-		}
-	}
-	return arr;
+  const getPair = (base) => {
+    switch(base){
+      case 'A':
+        return ['A', 'T']
+      case 'T':
+        return ['T', 'A']
+      case 'C':
+        return ['C', 'G']
+      case 'G':
+        return ['G', 'C']
+      default:
+        return []
+    }
+  }
+  return str.split("").map(getPair) 
 }
 
 pairElement("GCG");
