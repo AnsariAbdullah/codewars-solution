0b1b2e5a6b0983dd1972fbb3d613400b7e29ddd8
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-08-27 16:15:23 +0530

    format document

--- fcc-basic/DNA_pairing/app.js
+++ fcc-basic/DNA_pairing/app.js
@@ -1,19 +1,19 @@
 function pairElement(str) {
-  const getPair = (base) => {
-    switch(base){
-      case 'A':
-        return ['A', 'T']
-      case 'T':
-        return ['T', 'A']
-      case 'C':
-        return ['C', 'G']
-      case 'G':
-        return ['G', 'C']
-      default:
-        return []
-    }
-  }
-  return str.split("").map(getPair) 
+	const getPair = (base) => {
+		switch (base) {
+			case 'A':
+				return ['A', 'T']
+			case 'T':
+				return ['T', 'A']
+			case 'C':
+				return ['C', 'G']
+			case 'G':
+				return ['G', 'C']
+			default:
+				return []
+		}
+	}
+	return str.split("").map(getPair)
 }
 
 pairElement("GCG");
