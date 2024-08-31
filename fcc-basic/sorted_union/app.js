c71754f42ba4fb3323b4bf71cd8a6fb17776ad8d
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-08-31 16:09:54 +0530

    format document

--- fcc-basic/sorted_union/app.js
+++ fcc-basic/sorted_union/app.js
@@ -1,10 +1,10 @@
 function uniteUnique(...arr) {
-  const combArray = [].concat(...arr)
+	const combArray = [].concat(...arr)
 
-  const uniqueArr = combArray.filter((item, index) => {
-    return combArray.indexOf(item) === index
-  })
-  return uniqueArr;
+	const uniqueArr = combArray.filter((item, index) => {
+		return combArray.indexOf(item) === index
+	})
+	return uniqueArr;
 }
 
 uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
