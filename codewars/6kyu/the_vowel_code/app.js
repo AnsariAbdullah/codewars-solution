aa3f5387b29ce3f2d77afa79b4cad526403d8bc8
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-09-05 01:39:16 +0530

    format document

--- codewars/6kyu/the_vowel_code/app.js
+++ codewars/6kyu/the_vowel_code/app.js
@@ -1,9 +1,9 @@
-const  vowels = 'aeiou';
+const vowels = 'aeiou';
 
 function encode(string) {
-  return string.replace(/[aeiou]/g, (x) => (vowels.indexOf(x)+1).toString());
+	return string.replace(/[aeiou]/g, (x) => (vowels.indexOf(x) + 1).toString());
 }
 
 function decode(string) {
-  return string.replace(/[1-5]/g, (x) => vowels.charAt(parseInt(x)-1));
+	return string.replace(/[1-5]/g, (x) => vowels.charAt(parseInt(x) - 1));
 }
