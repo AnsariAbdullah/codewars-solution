80414f6afabca1aeb4b30eb595abaac6b6b4157f
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-09-11 23:50:52 +0530

    optimize solution

--- codewars/6kyu/the_vowel_code/app.ts
+++ codewars/6kyu/the_vowel_code/app.ts
@@ -1,48 +1,11 @@
+// optimized solution
+const  vowels = 'aeiou';
 // turn vowels into numbers
 export function encode(string: string): string {
-	const obj = {
-		a: 1,
-		e: 2,
-		i: 3,
-		o: 4,
-		u: 5
-	}
-	let newStr: string = ''
-	for (let i = 0; i < string.length; i++) {
-		if (string[i] == 'a') {
-			newStr += obj.a
-		} else if (string[i] == 'e') {
-			newStr += obj.e
-		} else if (string[i] == 'i') {
-			newStr += obj.i
-		} else if (string[i] == 'o') {
-			newStr += obj.o
-		} else if (string[i] == 'u') {
-			newStr += obj.u
-		} else {
-			newStr += string[i]
-		}
-	}
-	return newStr
+  return string.replace(/[aeiou]/g, (x) => (vowels.indexOf(x)+1).toString());
 }
 
 // turn numbers back into vowels
 export function decode(string: string): string {
-	let newStr: string = ''
-	for (let i = 0; i < string.length; i++) {
-		if (string[i] == "1") {
-			newStr += 'a'
-		} else if (string[i] == "2") {
-			newStr += 'e'
-		} else if (string[i] == "3") {
-			newStr += 'i'
-		} else if (string[i] == "4") {
-			newStr += 'o'
-		} else if (string[i] == "5") {
-			newStr += 'u'
-		} else {
-			newStr += string[i]
-		}
-	}
-	return newStr
+  return string.replace(/[1-5]/g, (x) => vowels.charAt(parseInt(x)-1));
 }
