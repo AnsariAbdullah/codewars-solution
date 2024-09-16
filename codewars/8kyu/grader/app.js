75e39b84366450417585507c70daf386948296a7
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-09-16 15:41:20 +0530

    refactor solution

--- codewars/8kyu/grader/app.js
+++ codewars/8kyu/grader/app.js
@@ -1,13 +1,7 @@
 function grader(score) {
-	if (score > 1 || score < 0.6) {
-		return "F"
-	} else if (score >= 0.9) {
-		return "A"
-	} else if (score >= 0.8) {
-		return "B"
-	} else if (score >= 0.7) {
-		return "C"
-	} else if (score >= 0.6) {
-		return "D"
-	}
+  if (score > 1 || score < 0.6) return "F";
+  if (score >= 0.9) return "A"; 
+  if (score >= 0.8) return "B";
+  if (score >= 0.7) return "C";
+  if (score >= 0.6) return "D";
 }
