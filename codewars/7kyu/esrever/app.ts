a43409d6571bfda4c65183b149c9d28263c1fe39
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-09-23 13:16:50 +0530

    format document

--- codewars/7kyu/esrever/app.ts
+++ codewars/7kyu/esrever/app.ts
@@ -1,7 +1,7 @@
 const reverse = (array: any[]): any[] => {
-  let arr: any[] = [];
-  for (let i = array.length - 1; i >= 0; i--) {
-    arr.push(array[i]);
-  }
-  return arr;
+	let arr: any[] = [];
+	for (let i = array.length - 1; i >= 0; i--) {
+		arr.push(array[i]);
+	}
+	return arr;
 };
