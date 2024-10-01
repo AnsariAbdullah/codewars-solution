9aa9e0ea841985b16d57519d0b95320b2eba1e8a
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-10-01 13:52:33 +0530

    format document

--- codewars/7kyu/latin_squares/app.js
+++ codewars/7kyu/latin_squares/app.js
@@ -1,12 +1,12 @@
 function makeLatinSquare(n) {
-  let result = [];
-  for(let i = 0; i<n; i++){
-    let row = [];
-    for(let j=i+1; j<=i+n; j++){
-      nextNumber = j>n ? j-n : j;
-      row.push(nextNumber);
-    }
-    result.push(row)
-  }
-  return result;
+	let result = [];
+	for (let i = 0; i < n; i++) {
+		let row = [];
+		for (let j = i + 1; j <= i + n; j++) {
+			nextNumber = j > n ? j - n : j;
+			row.push(nextNumber);
+		}
+		result.push(row)
+	}
+	return result;
 }
