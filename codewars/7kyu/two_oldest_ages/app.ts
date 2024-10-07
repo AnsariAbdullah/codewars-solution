5b3b031aab79f49730dbb0978b73d848df2b8dd9
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-10-07 13:46:06 +0530

    format document

--- codewars/7kyu/two_oldest_ages/app.ts
+++ codewars/7kyu/two_oldest_ages/app.ts
@@ -1,6 +1,6 @@
 // return the two oldest/oldest ages within the array of ages passed in.
 // it should return the two ages as a sorted array, youngest age first
 export function twoOldestAges(ages: number[]): number[] {
-  ages.sort((a, b) => b - a)  
-  return [ages[1], ages[0]];
+	ages.sort((a, b) => b - a)
+	return [ages[1], ages[0]];
 }
