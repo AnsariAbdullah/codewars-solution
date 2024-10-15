9c0f145d77f4b18f77bcef03026fe045585f9720
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-10-15 17:37:30 +0530

    add ts solution

---
+++ codewars/7kyu/number_to_digit_tiers/app.ts
@@ -0,0 +1,10 @@
+export function createArrayOfTiers(num: number): string[] {
+  let result: string[] = [];
+  const numStr: string = num.toString();
+  
+  for (let i = 1; i <= numStr.length; i++) {
+    result.push(numStr.slice(0, i));
+  }
+  
+  return result;
+}
