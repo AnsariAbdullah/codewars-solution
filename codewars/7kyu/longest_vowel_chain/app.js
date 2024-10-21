7b3ad344ee4474d8bebc9e4c2363554ce7f0d5a6
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-10-21 15:14:30 +0530

    add alternate solution

--- codewars/7kyu/longest_vowel_chain/app.js
+++ codewars/7kyu/longest_vowel_chain/app.js
@@ -1,3 +1,20 @@
 function solve(s) {
 	return Math.max(...s.split(/[^aeiou]/i).map(i => i.length))
 }
+
+// alternate solution
+function solve(s){
+  let curr = 0;
+  let max = 0;
+  for(let i=0; i<s.length; i++){
+    if('aeiou'.includes(s[i])){
+      curr++
+      if(curr > max){
+        max = curr
+      }
+    }else{
+      curr=0
+    }
+  }
+  return max;
+}
