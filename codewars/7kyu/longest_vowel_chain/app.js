8c6324b80da4f48e464445b80726aa4084087805
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-10-22 17:31:42 +0530

    format document

--- codewars/7kyu/longest_vowel_chain/app.js
+++ codewars/7kyu/longest_vowel_chain/app.js
@@ -3,18 +3,18 @@ function solve(s) {
 }
 
 // alternate solution
-function solve(s){
-  let curr = 0;
-  let max = 0;
-  for(let i=0; i<s.length; i++){
-    if('aeiou'.includes(s[i])){
-      curr++
-      if(curr > max){
-        max = curr
-      }
-    }else{
-      curr=0
-    }
-  }
-  return max;
+function solve(s) {
+	let curr = 0;
+	let max = 0;
+	for (let i = 0; i < s.length; i++) {
+		if ('aeiou'.includes(s[i])) {
+			curr++
+			if (curr > max) {
+				max = curr
+			}
+		} else {
+			curr = 0
+		}
+	}
+	return max;
 }
