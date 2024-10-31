423c4548a99e1172bd3cdcbce00adbb03ccb087d
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-10-31 12:40:18 +0530

    format document

--- codewars/7kyu/where_are_my_glasses/app.js
+++ codewars/7kyu/where_are_my_glasses/app.js
@@ -1,12 +1,12 @@
-function findGlasses(arr){
-  const glassesRegex = /O-+O/
-  for(let i=0; i<arr.length; i++){
-    if(glassesRegex.test(arr[i])) return i
-  }
-  return -1
+function findGlasses(arr) {
+	const glassesRegex = /O-+O/
+	for (let i = 0; i < arr.length; i++) {
+		if (glassesRegex.test(arr[i])) return i
+	}
+	return -1
 }
 
 // alternate solution 
-function findGlasses(arr){
-  return arr.findIndex(i => /O-+O/.test(i))
+function findGlasses(arr) {
+	return arr.findIndex(i => /O-+O/.test(i))
 }
