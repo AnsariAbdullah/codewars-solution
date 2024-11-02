3b69524a90c30efb46fcd23ee8545426e8f0b4a0
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-11-02 15:43:08 +0530

    format document

--- codewars/7kyu/where_are_my_glasses/app.ts
+++ codewars/7kyu/where_are_my_glasses/app.ts
@@ -1,12 +1,12 @@
-export function findGlasses(arr: string[]): number{
-  const glassesRegex = /O-+O/
-  for(let i=0; i<arr.length; i++){
-    if(glassesRegex.test(arr[i])) return i
-  }
-  return -1
+export function findGlasses(arr: string[]): number {
+	const glassesRegex = /O-+O/
+	for (let i = 0; i < arr.length; i++) {
+		if (glassesRegex.test(arr[i])) return i
+	}
+	return -1
 }
 
 // optimized 
-export function findGlassesAlt(arr: string[]): number{
-  return arr.findIndex(i => /O-+O/.test(i))
+export function findGlassesAlt(arr: string[]): number {
+	return arr.findIndex(i => /O-+O/.test(i))
 }
