7fc931c74da830703eca6c99ecb9e4063347b8fb
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-11-02 15:41:36 +0530

    add TS solution

---
+++ codewars/7kyu/where_are_my_glasses/app.ts
@@ -0,0 +1,7 @@
+export function findGlasses(arr: string[]): number{
+  const glassesRegex = /O-+O/
+  for(let i=0; i<arr.length; i++){
+    if(glassesRegex.test(arr[i])) return i
+  }
+  return -1
+}
