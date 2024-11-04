c519e0df8f807e23d9df572f0aec67ea7feaf40e
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-11-04 16:24:16 +0530

    add simple_string_characters solution

---
+++ codewars/7kyu/simple_string_characters/app.js
@@ -0,0 +1,15 @@
+function solve(s){
+  let uppercase = 0, lowercase = 0, numbers = 0, special = 0;
+  for(let i=0; i<s.length; i++){
+    if(/[A-Z]/.test(s[i])){
+      uppercase ++
+    }else if(/[a-z]/.test(s[i])){
+      lowercase++
+    }else if(/[0-9]/.test(s[i])){
+      numbers++
+    }else{
+      special++
+    }
+  }
+  return [uppercase, lowercase, numbers, special]
+}
