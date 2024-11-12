b1ecbc9908e4de3e4caacb4547f363e7305f3a95
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-11-12 17:20:16 +0530

    format document

--- codewars/7kyu/smallest_value_of_an_array/app.js
+++ codewars/7kyu/smallest_value_of_an_array/app.js
@@ -1,17 +1,17 @@
-function min(arr, toReturn) { 
-  let smallestValue = arr[0];
-  let smallestIndex = 0;
-  for(let i=0; i<arr.length; i++){
-    if(arr[i]<smallestValue){    
-      smallestValue = arr[i];
-      smallestIndex = i;
-    }
-  }
-  return toReturn == "value" ? smallestValue : smallestIndex;
+function min(arr, toReturn) {
+	let smallestValue = arr[0];
+	let smallestIndex = 0;
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] < smallestValue) {
+			smallestValue = arr[i];
+			smallestIndex = i;
+		}
+	}
+	return toReturn == "value" ? smallestValue : smallestIndex;
 }
 
 // alternate solution
-function min(arr, toReturn) { 
-  let smallestValue = Math.min(...arr);
-  return toReturn === "value" ? smallestValue : arr.indexOf(smallestValue);
+function min(arr, toReturn) {
+	let smallestValue = Math.min(...arr);
+	return toReturn === "value" ? smallestValue : arr.indexOf(smallestValue);
 }
