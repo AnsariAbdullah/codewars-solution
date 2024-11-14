7dfbe79a31ff62c68523b1e8c778953e1b25f351
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-11-14 12:47:43 +0530

    format document

--- codewars/8kyu/ascii_total/app.ts
+++ codewars/8kyu/ascii_total/app.ts
@@ -1,7 +1,7 @@
-export function uniTotal (string: string): number {
-  let result = 0;
-  for(let i=0; i<string.length; i++){
-    result += string.charCodeAt(i)
-  }
-  return result;
+export function uniTotal(string: string): number {
+	let result = 0;
+	for (let i = 0; i < string.length; i++) {
+		result += string.charCodeAt(i)
+	}
+	return result;
 }
