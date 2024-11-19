5e7ea39dad7fb8edeb6c6cfe13fb7b30f52b6b05
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-11-19 16:21:37 +0530

    format document

--- codewars/7kyu/longest_vowel_chain/app.ts
+++ codewars/7kyu/longest_vowel_chain/app.ts
@@ -1,20 +1,20 @@
-export function solve(s:string): number{
-  return Math.max(...s.split(/[^aeiou]/i).map(i => i.length))
+export function solve(s: string): number {
+	return Math.max(...s.split(/[^aeiou]/i).map(i => i.length))
 }
 
 // alternate solution
-export function solveAlt(s:string): number{
-  let curr = 0;
-  let max = 0;
-  for(let i=0; i<s.length; i++){
-    if('aeiou'.includes(s[i])){
-      curr++
-      if(curr > max){
-        max = curr
-      }
-    }else{
-      curr=0
-    }
-  }
-  return max;
+export function solveAlt(s: string): number {
+	let curr = 0;
+	let max = 0;
+	for (let i = 0; i < s.length; i++) {
+		if ('aeiou'.includes(s[i])) {
+			curr++
+			if (curr > max) {
+				max = curr
+			}
+		} else {
+			curr = 0
+		}
+	}
+	return max;
 }
