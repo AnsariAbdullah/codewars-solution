55cd8220b55da20b2ef1a032b7a7cc184a9a866d
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-11-25 17:37:34 +0530

    add alternate solution

--- codewars/8kyu/Replace_All/app.ts
+++ codewars/8kyu/Replace_All/app.ts
@@ -1,3 +1,8 @@
 export function replaceDots(str: string): string {
   return str.replaceAll('.', '-');
 }
+
+// alternate solution
+export function replaceDotsAlt(str: string): string {
+  return str.replace(/\./g, '-');
+}
