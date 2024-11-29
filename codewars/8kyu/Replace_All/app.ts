4866839c3d86366314e2909061a651ef5764ec1d
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-11-29 15:48:52 +0530

    remove unused files

--- codewars/8kyu/Replace_All/app.ts
+++
@@ -1,8 +0,0 @@
-export function replaceDots(str: string): string {
-	return str.replaceAll('.', '-');
-}
-
-// alternate solution
-export function replaceDotsAlt(str: string): string {
-	return str.replace(/\./g, '-');
-}
