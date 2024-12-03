1701d5afd5613caf3fb452633a59ad15f1bd0603
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-12-03 16:09:04 +0530

    add ts solution

---
+++ codewars/7kyu/simple_string_characters/app.ts
@@ -0,0 +1,15 @@
+export function solve(s: string) : [number, number, number, number]{
+  let uppercase = 0, lowercase = 0, numbers = 0, special = 0;
+  for(let i=0; i<s.length; i++){
+    if(/[A-Z]/.test(s[i])){
+      uppercase ++
+    }else if(/[a-z]/.test(s[i])){
+      lowercase++
+    }else if(/[0-9]/.test(s[i])){
+      numbers++
+    }else{
+      special++
+    }
+  }
+  return [uppercase, lowercase, numbers, special];
+}
