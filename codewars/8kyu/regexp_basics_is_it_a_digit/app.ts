71f89e89e9bec9f062ac5ed29c075848cd47fac6
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-12-10 17:34:13 +0530

    add alternate solution

--- codewars/8kyu/regexp_basics_is_it_a_digit/app.ts
+++ codewars/8kyu/regexp_basics_is_it_a_digit/app.ts
@@ -5,3 +5,8 @@ interface String {
 String.prototype.digit = function(): boolean {
   return /^\d$/.test(this as string);
 };
+
+// alternate solution
+String.prototype.digit = function(): boolean {
+  return /^[0-9]$/.test(this as string);
+};
