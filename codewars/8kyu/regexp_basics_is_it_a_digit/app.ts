1d27c8dd6b2df65f1d17fddbc3f9ecc78d0365be
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-12-11 17:10:08 +0530

    format document

--- codewars/8kyu/regexp_basics_is_it_a_digit/app.ts
+++ codewars/8kyu/regexp_basics_is_it_a_digit/app.ts
@@ -1,12 +1,12 @@
 interface String {
-  digit(): boolean;
+	digit(): boolean;
 }
 
-String.prototype.digit = function(): boolean {
-  return /^\d$/.test(this as string);
+String.prototype.digit = function (): boolean {
+	return /^\d$/.test(this as string);
 };
 
 // alternate solution
-String.prototype.digit = function(): boolean {
-  return /^[0-9]$/.test(this as string);
+String.prototype.digit = function (): boolean {
+	return /^[0-9]$/.test(this as string);
 };
