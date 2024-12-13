e645f44b4ea779583e79514f6a7758243c13d5ab
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-12-13 16:06:11 +0530

    format document

--- codewars/7kyu/smallest_value_of_an_array/app.ts
+++ codewars/7kyu/smallest_value_of_an_array/app.ts
@@ -1,20 +1,20 @@
-function min(arr: number[], toReturn: "value" | "index"): number { 
-  let smallestValue = arr[0];
-  let smallestIndex = 0;
-  
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] < smallestValue) {    
-      smallestValue = arr[i];
-      smallestIndex = i;
-    }
-  }
+function min(arr: number[], toReturn: "value" | "index"): number {
+	let smallestValue = arr[0];
+	let smallestIndex = 0;
 
-  return toReturn === "value" ? smallestValue : smallestIndex;
+	for (let i = 1; i < arr.length; i++) {
+		if (arr[i] < smallestValue) {
+			smallestValue = arr[i];
+			smallestIndex = i;
+		}
+	}
+
+	return toReturn === "value" ? smallestValue : smallestIndex;
 }
 
 // alternate solution
-function minAlternative(arr: number[], toReturn: "value" | "index"): number { 
-  let smallestValue = Math.min(...arr);
+function minAlternative(arr: number[], toReturn: "value" | "index"): number {
+	let smallestValue = Math.min(...arr);
 
-  return toReturn === "value" ? smallestValue : arr.indexOf(smallestValue);
+	return toReturn === "value" ? smallestValue : arr.indexOf(smallestValue);
 }
