085325e45b702958a88a6f9852890b88516ade2d
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-12-16 17:24:40 +0530

    format document

--- codewars/7kyu/move_10/app.js
+++ codewars/7kyu/move_10/app.js
@@ -1,5 +1,5 @@
-function moveTen(s){
-  const str='abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz';
-  const arr = s.split("").map(a => a=str.indexOf(a)+10);
-  return arr.map(v => v=str[v]).join("")
+function moveTen(s) {
+	const str = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz';
+	const arr = s.split("").map(a => a = str.indexOf(a) + 10);
+	return arr.map(v => v = str[v]).join("")
 }
