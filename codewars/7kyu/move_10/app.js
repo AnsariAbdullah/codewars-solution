15d72d811004af9d40d7e3e8b7b0ab86fcf151ee
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-12-16 17:25:19 +0530

    add alternate solution

--- codewars/7kyu/move_10/app.js
+++ codewars/7kyu/move_10/app.js
@@ -3,3 +3,10 @@ function moveTen(s) {
 	const arr = s.split("").map(a => a = str.indexOf(a) + 10);
 	return arr.map(v => v = str[v]).join("")
 }
+
+// alternate solution
+function moveTen(s){
+  let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  let move10 = 'klmnopqrstuvwxyzabcdefghij';
+  return s.replace(/[a-z]/g, x => move10[alphabet.indexOf(x)])
+}
