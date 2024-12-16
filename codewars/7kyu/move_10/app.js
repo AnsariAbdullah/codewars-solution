bbf6aa565e0f36027a6711a07bf356dc148d3b49
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-12-16 17:24:12 +0530

    add move_10 solution

---
+++ codewars/7kyu/move_10/app.js
@@ -0,0 +1,5 @@
+function moveTen(s){
+  const str='abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz';
+  const arr = s.split("").map(a => a=str.indexOf(a)+10);
+  return arr.map(v => v=str[v]).join("")
+}
