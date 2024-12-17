6e781f9f5c3d5f34481418a60e34f571447ad12b
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-12-17 14:25:32 +0530

    format document

--- codewars/7kyu/move_10/app.js
+++ codewars/7kyu/move_10/app.js
@@ -5,8 +5,8 @@ function moveTen(s) {
 }
 
 // alternate solution
-function moveTen(s){
-  let alphabet = 'abcdefghijklmnopqrstuvwxyz';
-  let move10 = 'klmnopqrstuvwxyzabcdefghij';
-  return s.replace(/[a-z]/g, x => move10[alphabet.indexOf(x)])
+function moveTen(s) {
+	let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+	let move10 = 'klmnopqrstuvwxyzabcdefghij';
+	return s.replace(/[a-z]/g, x => move10[alphabet.indexOf(x)])
 }
