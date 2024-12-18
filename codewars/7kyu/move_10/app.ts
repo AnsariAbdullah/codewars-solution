78272b8237deed63fd30318d2b23b05d1051445b
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-12-18 17:14:23 +0530

    format document

--- codewars/7kyu/move_10/app.ts
+++ codewars/7kyu/move_10/app.ts
@@ -1,5 +1,5 @@
 export function moveTen(s: string): string {
-  const str: string = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz';
-  const arr: number[] = s.split("").map((a: string) => str.indexOf(a) + 10);
-  return arr.map((v: number) => str[v]).join("");
+	const str: string = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz';
+	const arr: number[] = s.split("").map((a: string) => str.indexOf(a) + 10);
+	return arr.map((v: number) => str[v]).join("");
 }
