ae56a7b03a68f8efbbb43e2d345e4d9287d6fbe2
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-12-20 15:10:43 +0530

    add ts solution

--- codewars/7kyu/move_10/app.ts
+++ codewars/7kyu/move_10/app.ts
@@ -3,3 +3,10 @@ export function moveTen(s: string): string {
 	const arr: number[] = s.split("").map((a: string) => str.indexOf(a) + 10);
 	return arr.map((v: number) => str[v]).join("");
 }
+
+// alternate solution
+export function moveTen1(s: string): string {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  const move10 = 'klmnopqrstuvwxyzabcdefghij';
+  return s.replace(/[a-z]/g, x => move10[alphabet.indexOf(x)]);
+}
