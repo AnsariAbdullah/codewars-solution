dbef66de13d039ca99402b0b89fa9d4e8882e233
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-12-24 17:28:33 +0530

    format document

--- codewars/7kyu/fix_string_case/app.ts
+++ codewars/7kyu/fix_string_case/app.ts
@@ -1,19 +1,19 @@
 export function solve(s: string): string {
-  
-	let lowerCaseChars:number = 0;
-	let upperCaseChars:number = 0;
-  
-  for(let char of s){
-    if(char === char.toUpperCase()){
-      upperCaseChars++
-    }else{
-      lowerCaseChars++
-    }
-  }
-  
-  if(lowerCaseChars >= upperCaseChars){
-    return s.toLowerCase();
-  }else{
-   return s.toUpperCase();
-  }
+
+	let lowerCaseChars: number = 0;
+	let upperCaseChars: number = 0;
+
+	for (let char of s) {
+		if (char === char.toUpperCase()) {
+			upperCaseChars++
+		} else {
+			lowerCaseChars++
+		}
+	}
+
+	if (lowerCaseChars >= upperCaseChars) {
+		return s.toLowerCase();
+	} else {
+		return s.toUpperCase();
+	}
 }
