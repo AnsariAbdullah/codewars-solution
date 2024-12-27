1744f7fadf5723de3ddf97956e2c64185b16962e
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-12-27 16:20:38 +0530

    format document

--- codewars/8kyu/methods_of_arrayObject-concat_n_join/app.js
+++ codewars/8kyu/methods_of_arrayObject-concat_n_join/app.js
@@ -1,7 +1,7 @@
-function bigToSmall(arr){
-  let result = [];
-  for(let i=0; i<arr.length ; i++){
-    result = result.concat(arr[i])
-  }
-  return result.sort((a, b) => b-a).join('>')
+function bigToSmall(arr) {
+	let result = [];
+	for (let i = 0; i < arr.length; i++) {
+		result = result.concat(arr[i])
+	}
+	return result.sort((a, b) => b - a).join('>')
 }
