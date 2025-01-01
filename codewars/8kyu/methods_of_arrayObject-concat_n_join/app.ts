2e75b7278c72788c6ef55f03907563ba1c9cd4d7
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-01-01 15:34:29 +0530

    add methods_of_arrayObject-concat_n_join solution

---
+++ codewars/8kyu/methods_of_arrayObject-concat_n_join/app.ts
@@ -0,0 +1,8 @@
+export function bigToSmall(arr: number[][]): string {  
+  let result:number[] = [];
+  for(let i=0; i<arr.length; i++){
+    result = result.concat(arr[i])
+  }
+  
+  return result.sort((a, b) => b-a).join('>');
+}
