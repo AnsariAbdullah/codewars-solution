6ca260a52ac878c95940e0c6a597feda717f76b2
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-01-02 17:43:45 +0530

    add plus_minus_plus_plus_count solution

---
+++ codewars/7kyu/plus_minus_plus_plus_count/app.js
@@ -0,0 +1,9 @@
+function catchSignChange(arr) {
+  let count = 0;
+  for(let i = 0; i<arr.length-1; i++){
+    if((arr[i] >= 0 && arr[i+1] < 0) || (arr[i] < 0 && arr[i+1]  >=0)){
+      count++
+    }
+  }
+  return count;
+}
