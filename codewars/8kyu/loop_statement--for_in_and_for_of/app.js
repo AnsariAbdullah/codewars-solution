5d28db6326bc91779e59047edee8a7554a1a0763
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-01-03 15:16:48 +0530

    refactor code

--- codewars/8kyu/loop_statement--for_in_and_for_of/app.js
+++ codewars/8kyu/loop_statement--for_in_and_for_of/app.js
@@ -1,12 +1,8 @@
-function giveMeFive(obj) {
-	let arr = []
-	for (let key in obj) {
-		if (key.length == 5) {
-			arr.push(key)
-		}
-		if (obj[key].length == 5) {
-			arr.push(obj[key])
-		}
-	}
-	return arr;
+function giveMeFive(obj){
+  let result = [];
+  for(let key in obj){
+    if(key.length == 5) result.push(key);
+    if(obj[key].length == 5) result.push(obj[key])
+  }
+  return result;
 }
