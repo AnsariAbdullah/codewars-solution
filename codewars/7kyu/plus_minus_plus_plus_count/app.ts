1aa4b5e23701e0818cdfef049ee63bcbe64e804e
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-01-06 15:46:45 +0530

    add TS solution

---
+++ codewars/7kyu/plus_minus_plus_plus_count/app.ts
@@ -0,0 +1,9 @@
+export function catchSignChange(arr: number[]): number {
+  let count = 0;
+  for(let i = 0; i<arr.length-1; i++){
+    if((arr[i] >= 0 && arr[i+1] < 0) || (arr[i] < 0 && arr[i+1]  >=0)){
+      count++
+    }
+  }
+  return count;
+}
