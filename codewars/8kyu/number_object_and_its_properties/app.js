9e44558bce2fec3900e7bf036a2303b7f328d652
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-01-06 18:06:43 +0530

    refactor code

--- codewars/8kyu/number_object_and_its_properties/app.js
+++ codewars/8kyu/number_object_and_its_properties/app.js
@@ -1,19 +1,12 @@
-function whatNumberIsIt(n) {
-	let text = ''
-	if (n == Number.MAX_VALUE) {
-		text = 'Input number is Number.MAX_VALUE'
-	} else if (n == Number.MIN_VALUE) {
-		text = 'Input number is Number.MIN_VALUE'
-	} else if (isNaN(n)) {
-		text = "Input number is Number.NaN"
-	} else if (n == Number.NEGATIVE_INFINITY) {
-		text = "Input number is Number.NEGATIVE_INFINITY"
-	} else if (n == Number.POSITIVE_INFINITY) {
-		text = "Input number is Number.POSITIVE_INFINITY"
-	} else {
-		text = "Input number is " + n
-	}
-	return text
+function whatNumberIsIt(n){
+  let type = '';
+  if(n == Infinity) type = "Number.POSITIVE_INFINITY";
+  else if(n == -Infinity) type = "Number.NEGATIVE_INFINITY";
+  else if(n == 5e-324) type = "Number.MIN_VALUE";
+  else if(n == 1.7976931348623157e+308) type = "Number.MAX_VALUE";
+  else if(isNaN(n)) type = "Number.NaN";
+  else type = n;
+  return `Input number is ${type}`;  
 }
 
 // optimise (use switch) also try to avoid writing repetative text Input number is...
