0465ae0ad66a0ef6260c05fd2ca60493974fb1d0
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-01-07 17:02:49 +0530

    add alternate solution

--- codewars/8kyu/number_object_and_its_properties/app.js
+++ codewars/8kyu/number_object_and_its_properties/app.js
@@ -9,4 +9,27 @@ function whatNumberIsIt(n){
   return `Input number is ${type}`;  
 }
 
-// optimise (use switch) also try to avoid writing repetative text Input number is...
+// alternate solution
+function whatNumberIsIt(n){
+  let type = '';
+  switch(n){
+    case Number.MAX_VALUE: 
+      type = "Number.MAX_VALUE";
+      break;
+    case Number.MIN_VALUE: 
+      type = "Number.MIN_VALUE";
+      break;
+    case Number.POSITIVE_INFINITY:
+      type = "Number.POSITIVE_INFINITY";
+      break;
+    case Number.NEGATIVE_INFINITY:
+      type = "Number.NEGATIVE_INFINITY";
+      break;
+    case Number(n): 
+      type = n;
+      break;  
+    default:
+      type = "Number.NaN";
+  }
+  return `Input number is ${type}`;
+}
