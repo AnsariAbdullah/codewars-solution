762f9fad8fc9e739174d64db1f0c1ebbc225589e
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-01-08 16:06:29 +0530

    format document

--- codewars/8kyu/number_object_and_its_properties/app.js
+++ codewars/8kyu/number_object_and_its_properties/app.js
@@ -1,35 +1,35 @@
-function whatNumberIsIt(n){
-  let type = '';
-  if(n == Infinity) type = "Number.POSITIVE_INFINITY";
-  else if(n == -Infinity) type = "Number.NEGATIVE_INFINITY";
-  else if(n == 5e-324) type = "Number.MIN_VALUE";
-  else if(n == 1.7976931348623157e+308) type = "Number.MAX_VALUE";
-  else if(isNaN(n)) type = "Number.NaN";
-  else type = n;
-  return `Input number is ${type}`;  
+function whatNumberIsIt(n) {
+	let type = '';
+	if (n == Infinity) type = "Number.POSITIVE_INFINITY";
+	else if (n == -Infinity) type = "Number.NEGATIVE_INFINITY";
+	else if (n == 5e-324) type = "Number.MIN_VALUE";
+	else if (n == 1.7976931348623157e+308) type = "Number.MAX_VALUE";
+	else if (isNaN(n)) type = "Number.NaN";
+	else type = n;
+	return `Input number is ${type}`;
 }
 
 // alternate solution
-function whatNumberIsIt(n){
-  let type = '';
-  switch(n){
-    case Number.MAX_VALUE: 
-      type = "Number.MAX_VALUE";
-      break;
-    case Number.MIN_VALUE: 
-      type = "Number.MIN_VALUE";
-      break;
-    case Number.POSITIVE_INFINITY:
-      type = "Number.POSITIVE_INFINITY";
-      break;
-    case Number.NEGATIVE_INFINITY:
-      type = "Number.NEGATIVE_INFINITY";
-      break;
-    case Number(n): 
-      type = n;
-      break;  
-    default:
-      type = "Number.NaN";
-  }
-  return `Input number is ${type}`;
+function whatNumberIsIt(n) {
+	let type = '';
+	switch (n) {
+		case Number.MAX_VALUE:
+			type = "Number.MAX_VALUE";
+			break;
+		case Number.MIN_VALUE:
+			type = "Number.MIN_VALUE";
+			break;
+		case Number.POSITIVE_INFINITY:
+			type = "Number.POSITIVE_INFINITY";
+			break;
+		case Number.NEGATIVE_INFINITY:
+			type = "Number.NEGATIVE_INFINITY";
+			break;
+		case Number(n):
+			type = n;
+			break;
+		default:
+			type = "Number.NaN";
+	}
+	return `Input number is ${type}`;
 }
