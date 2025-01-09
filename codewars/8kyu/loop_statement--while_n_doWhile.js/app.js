57e2657468ee9fd0b6ecf314c3133052acda5f01
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-01-09 11:53:39 +0530

    format document

--- codewars/8kyu/loop_statement--while_n_doWhile.js/app.js
+++ codewars/8kyu/loop_statement--while_n_doWhile.js/app.js
@@ -1,13 +1,13 @@
-function padIt(str,n){
-  let res = str;
-  let i = 0;
-  while(i<n){
-    if(i%2 ==0){
-      res = `*${res}`;
-    }else{
-      res = `${res}*`;
-    }
-    i++
-  }
-  return res;
+function padIt(str, n) {
+	let res = str;
+	let i = 0;
+	while (i < n) {
+		if (i % 2 == 0) {
+			res = `*${res}`;
+		} else {
+			res = `${res}*`;
+		}
+		i++
+	}
+	return res;
 }
