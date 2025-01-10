08da3839ecea388eb9a17e82971b743fc8568893
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-01-10 16:00:56 +0530

    add alternate solution

--- codewars/8kyu/loop_statement--while_n_doWhile.js/app.js
+++ codewars/8kyu/loop_statement--while_n_doWhile.js/app.js
@@ -11,3 +11,21 @@ function padIt(str, n) {
 	}
 	return res;
 }
+
+// alternate solution
+function padIt(str,n){
+  let res = str;
+  let i = 0;
+  let side = true;
+  while(i<n){
+    if(side){
+      res = "*"+res;
+      side = false;
+    }else{
+      res = res+"*";
+      side = true;
+    }
+    i++
+  }
+  return res;
+}
