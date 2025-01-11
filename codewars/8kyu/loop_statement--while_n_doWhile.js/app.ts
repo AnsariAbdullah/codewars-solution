3d161a777a3c31f4611db171079c7fde10d59978
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-01-11 16:07:50 +0530

    add TS solution

---
+++ codewars/8kyu/loop_statement--while_n_doWhile.js/app.ts
@@ -0,0 +1,13 @@
+export function padIt(str: string, n: number): string {
+  let res: string = str;
+  let i: number = 0;
+  while (i < n) {
+    if (i % 2 === 0) {
+      res = `*${res}`;
+    } else {
+      res = `${res}*`;
+    }
+    i++;
+  }
+  return res;
+}
