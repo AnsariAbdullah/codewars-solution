21631554ef14a559853c77478332cac56e6e12c4
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-01-16 14:37:02 +0530

    add alternate solution

--- codewars/8kyu/loop_statement--while_n_doWhile.js/app.ts
+++ codewars/8kyu/loop_statement--while_n_doWhile.js/app.ts
@@ -11,3 +11,22 @@ export function padIt(str: string, n: number): string {
 	}
 	return res;
 }
+
+
+// alternate solution
+export function padItAlt(str: string, n: number): string {
+  let res: string = str;
+  let i: number = 0;
+  let side: boolean = true;
+  while (i < n) {
+    if (side) {
+      res = `*${res}`;
+      side = false;
+    } else {
+      res = `${res}*`;
+      side = true;
+    }
+    i++;
+  }
+  return res;
+}
