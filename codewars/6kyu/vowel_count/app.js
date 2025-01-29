b73f446d98f00f7df378b167b9e772410a3e1c23
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-01-29 16:45:30 +0530

    format document

--- codewars/6kyu/vowel_count/app.js
+++ codewars/6kyu/vowel_count/app.js
@@ -1,7 +1,7 @@
 function getCount(str) {
-  let count = 0;
-  for (let i=0; i<str.length; i++){
-    if(str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u') count++
-  }
-  return count;
+	let count = 0;
+	for (let i = 0; i < str.length; i++) {
+		if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u') count++
+	}
+	return count;
 }
