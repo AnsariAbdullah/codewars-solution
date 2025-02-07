ff2ff4204a71a38e296c12c68cc829d3adbf5796
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-02-07 15:40:11 +0530

    format document

--- codewars/8kyu/methods_of_array_object_splice()_and_slice()/app.js
+++ codewars/8kyu/methods_of_array_object_splice()_and_slice()/app.js
@@ -1,10 +1,10 @@
-function threeInOne(arr){
-  let result = [];
-  let newArr = arr.slice();
-  while(newArr.length > 0){
-    let chunk = newArr.splice(0,3);
-    let sum = chunk.reduce((acc, num) => acc+num, 0)
-    result.push(sum)
-  }
-  return result;  
+function threeInOne(arr) {
+	let result = [];
+	let newArr = arr.slice();
+	while (newArr.length > 0) {
+		let chunk = newArr.splice(0, 3);
+		let sum = chunk.reduce((acc, num) => acc + num, 0)
+		result.push(sum)
+	}
+	return result;
 }
