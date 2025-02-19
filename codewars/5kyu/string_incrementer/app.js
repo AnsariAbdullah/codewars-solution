c52d8f2c2a73b1ddbc93a8c7cd60b71a9465b788
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-02-19 13:05:46 +0530

    add string_incrementer solution

---
+++ codewars/5kyu/string_incrementer/app.js
@@ -0,0 +1,11 @@
+function incrementString(str) {
+	const match = str.match(/(\d*)$/);
+	if (match) {
+			const numPart = match[0];
+			const textPart = str.slice(0, -numPart.length);
+			if (numPart === "") return str + "1";
+			const newNum = (parseInt(numPart, 10) + 1).toString();
+			return textPart + numPart.slice(0, -newNum.length) + newNum;
+	}
+	return str + "1";
+}
