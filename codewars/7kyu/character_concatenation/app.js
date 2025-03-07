c0e760b49ea434d067a5385f6cf8e84f2379cd6b
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-03-07 16:58:15 +0530

    add character_concatenation solution

---
+++ codewars/7kyu/character_concatenation/app.js
@@ -0,0 +1,11 @@
+function charConcat(string){
+  let result = "";
+  let length = string.length;
+  let half = Math.floor(length / 2);
+  
+  for(let i = 0; i<half; i++){
+    result += string[i] + string[length - 1 - i] + (i+1);
+  }
+
+  return result;
+}
