a0d62d85ad3e0ab70e03ee3a2038e5c0fb62d2fb
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-03-11 16:38:34 +0530

    format document

--- codewars/8kyu/methods_of_string_object_slice()_substring()_substr()/app.js
+++ codewars/8kyu/methods_of_string_object_slice()_substring()_substr()/app.js
@@ -1,11 +1,11 @@
-function cutIt(arr){
-  let shortestString = arr[0].length;
-  let result = [];
-  for(let i=0; i< arr.length; i++){
-    if(arr[i].length < shortestString) shortestString = arr[i].length;
-  }
-  for(let i=0; i< arr.length; i++){
-    result.push(arr[i].substr(0, shortestString))
-  }
-  return result;
+function cutIt(arr) {
+	let shortestString = arr[0].length;
+	let result = [];
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i].length < shortestString) shortestString = arr[i].length;
+	}
+	for (let i = 0; i < arr.length; i++) {
+		result.push(arr[i].substr(0, shortestString))
+	}
+	return result;
 }
