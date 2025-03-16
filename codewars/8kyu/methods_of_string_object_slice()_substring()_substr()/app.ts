841334ae342c3bda2534b4cbe916bd8ee30710e2
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-03-17 01:40:34 +0530

    add TS solution

---
+++ codewars/8kyu/methods_of_string_object_slice()_substring()_substr()/app.ts
@@ -0,0 +1,11 @@
+export function cutIt(arr: string[]): string[]{
+  let shortestString = arr[0].length;
+  let result: string[] = [];
+  for(let i=0; i< arr.length; i++){
+    if(arr[i].length < shortestString) shortestString = arr[i].length;
+  }
+  for(let i=0; i< arr.length; i++){
+    result.push(arr[i].substr(0, shortestString))
+  }
+  return result;
+}
