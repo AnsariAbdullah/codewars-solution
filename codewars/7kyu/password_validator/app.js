4d94a6b09005a0fe4f86c2b80888f829e73bb8a9
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-03-29 23:00:36 +0530

    add alternate solution

--- codewars/7kyu/password_validator/app.js
+++ codewars/7kyu/password_validator/app.js
@@ -2,3 +2,7 @@ function password(str) {
 	const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$/;
 	return passwordRegex.test(str);
 }
+
+function password(str) {
+  return str.length >= 8 && /[a-z]/.test(str) && /[A-Z]/.test(str) && /\d/.test(str);
+}
