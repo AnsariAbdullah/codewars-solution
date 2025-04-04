66ee197e8b3775816ed06f83b8ad01d0b5e74417
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-04-04 15:27:52 +0530

    format document

--- codewars/7kyu/password_validator/app.ts
+++ codewars/7kyu/password_validator/app.ts
@@ -1,4 +1,4 @@
 export function password(str: string): boolean {
-  const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$/;
-  return passwordRegex.test(str);
+	const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$/;
+	return passwordRegex.test(str);
 }
