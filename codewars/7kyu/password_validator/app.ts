92fed4f10d99886d60014e095a67561b4fe254b4
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-04-05 15:38:01 +0530

    add alternate solution

--- codewars/7kyu/password_validator/app.ts
+++ codewars/7kyu/password_validator/app.ts
@@ -2,3 +2,8 @@ export function password(str: string): boolean {
 	const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$/;
 	return passwordRegex.test(str);
 }
+
+// alternate solution
+export function passwordAlt(str:string): boolean {
+  return str.length >= 8 && /[a-z]/.test(str) && /[A-Z]/.test(str) && /\d/.test(str);
+}
