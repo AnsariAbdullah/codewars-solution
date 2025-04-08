7ccfdb706524500082c5562c863cc0327d049c06
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-04-08 18:12:58 +0530

    format document

--- codewars/7kyu/list_filtering/app.ts
+++ codewars/7kyu/list_filtering/app.ts
@@ -1,3 +1,3 @@
 function filter_list(l: (number | string)[]): number[] {
-  return l.filter((item): item is number => typeof item === 'number');
+	return l.filter((item): item is number => typeof item === 'number');
 }
