47105e8bf4a88cae5a26ee3db1345d48d1ce9bda
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-04-17 16:09:21 +0530

    add deodorant_evaporator solution

---
+++ codewars/7kyu/deodorant_evaporator/app.js
@@ -0,0 +1,11 @@
+function evaporator(content, evap_per_day, threshold) {
+  let days = 0;
+  let current = 100;
+
+  while (current > threshold) {
+    current -= current * (evap_per_day / 100);
+    days++;
+  }
+
+  return days;
+}
