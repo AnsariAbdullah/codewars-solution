17bd6bc25222835f802da897b335ccfeac4e634a
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-04-19 18:02:57 +0530

    format document

--- codewars/7kyu/deodorant_evaporator/app.ts
+++ codewars/7kyu/deodorant_evaporator/app.ts
@@ -1,11 +1,11 @@
 export function evaporator(content: number, evap_per_day: number, threshold: number): number {
-  let days = 0;
-  let current = 100;
+	let days = 0;
+	let current = 100;
 
-  while (current > threshold) {
-    current -= current * (evap_per_day / 100);
-    days++;
-  }
+	while (current > threshold) {
+		current -= current * (evap_per_day / 100);
+		days++;
+	}
 
-  return days;
+	return days;
 }
