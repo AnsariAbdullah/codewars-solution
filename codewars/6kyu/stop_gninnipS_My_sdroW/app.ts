b7f5b3fe72f2496076467e4178a663cc01294759
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-04-22 16:07:12 +0530

    format document

--- codewars/6kyu/stop_gninnipS_My_sdroW/app.ts
+++ codewars/6kyu/stop_gninnipS_My_sdroW/app.ts
@@ -1,11 +1,11 @@
 export function spinWords(words: string): string {
-  return words.split(' ').map((item: string) => {
-    return item.length >= 5 ? item.split('').reverse().join('') : item
-  }).join(' ')
+	return words.split(' ').map((item: string) => {
+		return item.length >= 5 ? item.split('').reverse().join('') : item
+	}).join(' ')
 }
 
 
 // alternate solution
 export function spinWordsAlt(words: string): string {
-  return words.replace(/\w{5,}/g, (w: string)=> w.length >=5 ? w.split('').reverse().join('') : w)
+	return words.replace(/\w{5,}/g, (w: string) => w.length >= 5 ? w.split('').reverse().join('') : w)
 }
