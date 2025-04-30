af70179b79bc438e2f8722ca62edc5fcea8f4007
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-04-30 13:24:33 +0530

    format document

--- codewars/8kyu/age_range_compatibility_equation/app.js
+++ codewars/8kyu/age_range_compatibility_equation/app.js
@@ -1,13 +1,13 @@
-function datingRange(age){
+function datingRange(age) {
   let min;
   let max;
-  if(age <= 14){
-    min = age - 0.10 * age;
-    max = age + 0.10 * age; 
-  }else{
-    min = (age/2) + 7;
-    max = (age-7) * 2; 
+  if (age <= 14) {
+    min = age - 0.1 * age;
+    max = age + 0.1 * age;
+  } else {
+    min = age / 2 + 7;
+    max = (age - 7) * 2;
   }
-  
-  return `${Math.floor(min)}-${Math.floor(max)}`
+
+  return `${Math.floor(min)}-${Math.floor(max)}`;
 }
