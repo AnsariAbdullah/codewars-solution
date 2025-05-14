f860e5647a9c13a522b30d1fa386be801de9576c
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-05-14 18:22:28 +0530

    add alternate solution

--- codewars/8kyu/student_final_grade/app.js
+++ codewars/8kyu/student_final_grade/app.js
@@ -10,3 +10,17 @@ function finalGrade(exam, projects) {
   }
   return 0;
 }
+
+// alternate solution
+function finalGrade (exam, projects) {
+  switch(true){
+    case (exam > 90 || projects > 10):
+      return 100; 
+    case (exam > 75 && projects >= 5):
+      return 90; 
+    case (exam > 50 && projects >= 2):
+      return 75; 
+    default:
+      return 0;
+  }
+}
