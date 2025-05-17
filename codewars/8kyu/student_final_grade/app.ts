b247bef0543beb75b401f0b2c089f793a691992e
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-05-17 19:48:04 +0530

    add alternate solution

--- codewars/8kyu/student_final_grade/app.ts
+++ codewars/8kyu/student_final_grade/app.ts
@@ -10,3 +10,18 @@ export function finalGrade(exam: number, projects: number): number {
   }
   return 0;
 }
+
+// alternate solution
+export function finalGradeAlt (exam: number, projects: number): number {
+  switch(true){
+    case (exam > 90 || projects > 10):
+      return 100; 
+    case (exam > 75 && projects >= 5):
+      return 90; 
+    case (exam > 50 && projects >= 2):
+      return 75; 
+    default:
+      return 0;
+  }
+}
+
