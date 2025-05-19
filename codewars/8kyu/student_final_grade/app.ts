0fedf683d9661b2eaeaa80ea91827f0dc74925c5
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-05-19 17:03:27 +0530

    format document

--- codewars/8kyu/student_final_grade/app.ts
+++ codewars/8kyu/student_final_grade/app.ts
@@ -12,16 +12,15 @@ export function finalGrade(exam: number, projects: number): number {
 }
 
 // alternate solution
-export function finalGradeAlt (exam: number, projects: number): number {
-  switch(true){
-    case (exam > 90 || projects > 10):
-      return 100; 
-    case (exam > 75 && projects >= 5):
-      return 90; 
-    case (exam > 50 && projects >= 2):
-      return 75; 
+export function finalGradeAlt(exam: number, projects: number): number {
+  switch (true) {
+    case exam > 90 || projects > 10:
+      return 100;
+    case exam > 75 && projects >= 5:
+      return 90;
+    case exam > 50 && projects >= 2:
+      return 75;
     default:
       return 0;
   }
 }
-
