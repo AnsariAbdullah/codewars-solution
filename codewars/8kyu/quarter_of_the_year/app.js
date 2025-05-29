16f55371b88dde66fe1d69ecc9a25eab39fb575c
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-05-29 17:13:10 +0530

    add alternate solution

--- codewars/8kyu/quarter_of_the_year/app.js
+++ codewars/8kyu/quarter_of_the_year/app.js
@@ -1,3 +1,8 @@
 const quarterOf = (month) => {
   return month <= 3 ? 1 : month <= 6 ? 2 : month <= 9 ? 3 : 4;
 };
+
+// alternate solution
+const quarterOf = (month) => {
+  return Math.ceil(month/3)
+}
