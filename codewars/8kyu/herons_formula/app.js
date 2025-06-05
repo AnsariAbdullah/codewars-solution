ddd1df1d9e7aaaa0b9b09f6425de8c3e10d0a76a
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-06-05 16:13:39 +0530

    format document

--- codewars/8kyu/herons_formula/app.js
+++ codewars/8kyu/herons_formula/app.js
@@ -1,5 +1,5 @@
 function heron(a, b, c) {
-  const s = (a+b+c) / 2;
-  const sqRoot = s * (s-a) * (s-b) * (s-c)
+  const s = (a + b + c) / 2;
+  const sqRoot = s * (s - a) * (s - b) * (s - c);
   return Math.sqrt(sqRoot);
 }
