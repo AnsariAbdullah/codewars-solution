53729746e9f7ebe56db0e4be2f35b0791775673c
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-06-12 17:02:35 +0530

    add sum_of_differences_in_array solution

---
+++ codewars/8kyu/sum_of_differences_in_array/app.js
@@ -0,0 +1,9 @@
+function sumOfDifferences(arr) {
+  if(arr.length <=1) return 0;
+  arr.sort((a, b) => b - a);
+  let result = 0;
+  for(let i = 0; i<arr.length - 1; i++){
+    result += arr[i] - arr[i+1];
+  }
+  return result;
+}
