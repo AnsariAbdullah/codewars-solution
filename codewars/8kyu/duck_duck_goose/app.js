4ccc8793fcd9f6aad288a8f26a5056adb99cd4c4
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-06-19 20:29:17 +0530

    add alternate solution

--- codewars/8kyu/duck_duck_goose/app.js
+++ codewars/8kyu/duck_duck_goose/app.js
@@ -2,3 +2,12 @@ function duckDuckGoose(players, goose) {
   const adjustedIndex = (goose - 1) % players.length;
   return players[adjustedIndex].name;
 }
+
+// alternate solution
+function duckDuckGoose(players, goose) {
+  let circle = goose;
+  while(players.length < circle) {
+      circle -= players.length
+    }
+    return players[circle - 1 ].name
+}
