88c8f07ba4db8afaa132b2a39875669aa8736a71
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-06-24 18:38:33 +0530

    add alternate solution

--- codewars/8kyu/duck_duck_goose/app.ts
+++ codewars/8kyu/duck_duck_goose/app.ts
@@ -6,3 +6,12 @@ export function duckDuckGoose<T>(players: Player[], goose: number): string {
   const adjustedIndex = (goose - 1) % players.length;
   return players[adjustedIndex].name;
 }
+
+// alternate solution
+export function duckDuckGooseAlt(players: Player[], goose: number): string {
+  let circle = goose;
+  while(players.length < circle) {
+      circle -= players.length
+    }
+    return players[circle - 1 ].name
+}
