f58d721092aad4ccf9a9d9c9828c39f5e3ea084b
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-06-27 16:26:16 +0530

    add alternate solution

--- codewars/8kyu/unscrambled_eggs/app.ts
+++ codewars/8kyu/unscrambled_eggs/app.ts
@@ -2,3 +2,8 @@ export function unscrambleEggs(word: string): string {
   const output = word.replace(/egg/g, "");
   return output;
 }
+
+// optimised solution
+export function unscrambleEggsAlt(word: string): string {
+return word.replace(/egg/g, "");
+}
