3ff60905057dd3e8167b60416e02fd7e670d2e3c
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-07-01 13:33:27 +0530

    format document

--- codewars/8kyu/sum_of_differences_in_array/app.ts
+++ codewars/8kyu/sum_of_differences_in_array/app.ts
@@ -1,9 +1,9 @@
 export function sumOfDifferencesAlt(arr: number[]): number {
-  if(arr.length <=1) return 0;
+  if (arr.length <= 1) return 0;
   arr.sort((a, b) => b - a);
-  let result:number = 0;
-  for(let i = 0; i<arr.length - 1; i++){
-    result += arr[i] - arr[i+1];
+  let result: number = 0;
+  for (let i = 0; i < arr.length - 1; i++) {
+    result += arr[i] - arr[i + 1];
   }
   return result;
 }
