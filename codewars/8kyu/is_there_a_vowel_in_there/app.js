71e2d5a44f271ba2cbf179dfe3148a8ca0f7cabd
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-07-02 13:06:32 +0530

    add is_there_a_vowel_in_there solution

---
+++ codewars/8kyu/is_there_a_vowel_in_there/app.js
@@ -0,0 +1,11 @@
+function isVow(a){
+  let newArr = [];
+  for(let i=0; i<a.length; i++){
+  if(a[i] == 97 || a[i] ==  101 || a[i] ==  105 || a[i] ==  111 || a[i] ==  117){
+      newArr.push(String.fromCharCode(a[i]))
+    }else{
+      newArr.push(a[i])
+    }
+  }
+  return newArr;
+}
