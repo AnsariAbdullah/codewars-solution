cf7017e63840f9fb990c00a84dd13a6433c43833
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-07-02 13:07:18 +0530

    format document

--- codewars/8kyu/is_there_a_vowel_in_there/app.js
+++ codewars/8kyu/is_there_a_vowel_in_there/app.js
@@ -1,10 +1,16 @@
-function isVow(a){
+function isVow(a) {
   let newArr = [];
-  for(let i=0; i<a.length; i++){
-  if(a[i] == 97 || a[i] ==  101 || a[i] ==  105 || a[i] ==  111 || a[i] ==  117){
-      newArr.push(String.fromCharCode(a[i]))
-    }else{
-      newArr.push(a[i])
+  for (let i = 0; i < a.length; i++) {
+    if (
+      a[i] == 97 ||
+      a[i] == 101 ||
+      a[i] == 105 ||
+      a[i] == 111 ||
+      a[i] == 117
+    ) {
+      newArr.push(String.fromCharCode(a[i]));
+    } else {
+      newArr.push(a[i]);
     }
   }
   return newArr;
