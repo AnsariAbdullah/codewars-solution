b74c7fd5960e446b9b9ba0d8ecd063b1954c88bf
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-07-03 16:36:01 +0530

    add alternate solution

--- codewars/8kyu/is_there_a_vowel_in_there/app.js
+++ codewars/8kyu/is_there_a_vowel_in_there/app.js
@@ -15,3 +15,13 @@ function isVow(a) {
   }
   return newArr;
 }
+
+// alternate solution
+function isVow(a){
+  for(let i=0, l = a.length; i<l; i++){
+  let char = String.fromCharCode(a[i]);
+  if('aeiou'.indexOf(char) !== -1)
+    a[i] = char;
+  }
+  return a;
+}
