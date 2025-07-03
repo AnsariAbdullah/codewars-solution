2a28a4763c42988c4a9cd0af871660856b7c7572
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-07-03 16:37:24 +0530

    format document

--- codewars/8kyu/is_there_a_vowel_in_there/app.js
+++ codewars/8kyu/is_there_a_vowel_in_there/app.js
@@ -17,11 +17,10 @@ function isVow(a) {
 }
 
 // alternate solution
-function isVow(a){
-  for(let i=0, l = a.length; i<l; i++){
-  let char = String.fromCharCode(a[i]);
-  if('aeiou'.indexOf(char) !== -1)
-    a[i] = char;
+function isVow(a) {
+  for (let i = 0, l = a.length; i < l; i++) {
+    let char = String.fromCharCode(a[i]);
+    if ("aeiou".indexOf(char) !== -1) a[i] = char;
   }
   return a;
 }
