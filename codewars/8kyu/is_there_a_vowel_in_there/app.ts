5e02428c1f19dfe474f5182c0fde9123b4faaa91
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-07-07 16:24:20 +0530

    add alternate solution

--- codewars/8kyu/is_there_a_vowel_in_there/app.ts
+++ codewars/8kyu/is_there_a_vowel_in_there/app.ts
@@ -1,11 +1,29 @@
-export function isVow(a: number[]): (number | string)[]{
-let newArr: (number | string)[] = [];
-for(let i=0; i<a.length; i++){
-  if(a[i] == 97 || a[i] ==  101 || a[i] ==  105 || a[i] ==  111 || a[i] ==  117){
-    newArr.push(String.fromCharCode(a[i]))
-  }else{
-    newArr.push(a[i])
+export function isVow(a: number[]): (number | string)[] {
+  let newArr: (number | string)[] = [];
+  for (let i = 0; i < a.length; i++) {
+    if (
+      a[i] == 97 ||
+      a[i] == 101 ||
+      a[i] == 105 ||
+      a[i] == 111 ||
+      a[i] == 117
+    ) {
+      newArr.push(String.fromCharCode(a[i]));
+    } else {
+      newArr.push(a[i]);
+    }
   }
+  return newArr;
 }
-return newArr;
+
+
+
+// alternate solution
+export function isVowAlt(a: (number | string)[]): (number | string)[] {
+for(let i=0, l = a.length; i<l; i++){
+  let char = String.fromCharCode(Number(a[i]));
+  if('aeiou'.indexOf(char) !== -1)
+    a[i] = char;
+}
+return a
 }
