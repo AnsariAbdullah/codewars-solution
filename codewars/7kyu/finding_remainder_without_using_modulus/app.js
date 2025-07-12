8e05163d1b0545ecd4b5ec47a44cf20c4762705d
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-07-12 13:11:03 +0530

    add alternate solution

--- codewars/7kyu/finding_remainder_without_using_modulus/app.js
+++ codewars/7kyu/finding_remainder_without_using_modulus/app.js
@@ -1,2 +1,10 @@
 const remainder = (dividend, divisor) =>
   dividend - Math.floor(dividend / divisor) * divisor;
+
+// aleternate solution
+const remainderAlt = (D, d) => {
+while(D>=d){
+  D-=d
+}
+  return D
+};
