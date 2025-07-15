5d576f2019625551e9baf302599e724be8a191ff
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-07-15 17:25:22 +0530

    add esreverEsreveR solution

---
+++ codewars/7kyu/esreverEsreveR/app.js
@@ -0,0 +1,9 @@
+function esrever(str) {
+    const punctuation = str.slice(-1);
+    const sentence = str.slice(0, -1);
+    return sentence
+          .split(' ')
+          .reverse()
+          .map(word => word.split("").reverse().join(''))
+          .join(' ') + punctuation;
+}
