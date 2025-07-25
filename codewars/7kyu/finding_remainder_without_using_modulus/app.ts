078462bbc8ed1f46536935bd54c3845a26e50e1a
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-07-25 15:54:20 +0530

    format document

--- codewars/7kyu/finding_remainder_without_using_modulus/app.ts
+++ codewars/7kyu/finding_remainder_without_using_modulus/app.ts
@@ -2,9 +2,9 @@ export const remainder = (dividend: number, divisor: number): number =>
   dividend - Math.floor(dividend / divisor) * divisor;
 
 // aleternate solution
-export const remainderAlt = (D:number, d:number):number => {
-while(D>=d){
-  D-=d
-}
-  return D
+export const remainderAlt = (D: number, d: number): number => {
+  while (D >= d) {
+    D -= d;
+  }
+  return D;
 };
