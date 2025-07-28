87f9e6e17e8c4885481d68458b744bef98ec9147
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-07-28 11:28:15 +0530

    format document

--- codewars/7kyu/esreverEsreveR/app.ts
+++ codewars/7kyu/esreverEsreveR/app.ts
@@ -1,9 +1,11 @@
-export function esrever(str:string): string {
-    const punctuation = str.slice(-1);
-    const sentence = str.slice(0, -1);
-    return sentence
-    .split(' ')
-    .reverse()
-    .map(word => word.split("").reverse().join(''))
-    .join(' ') + punctuation;
+export function esrever(str: string): string {
+  const punctuation = str.slice(-1);
+  const sentence = str.slice(0, -1);
+  return (
+    sentence
+      .split(" ")
+      .reverse()
+      .map((word) => word.split("").reverse().join(""))
+      .join(" ") + punctuation
+  );
 }
