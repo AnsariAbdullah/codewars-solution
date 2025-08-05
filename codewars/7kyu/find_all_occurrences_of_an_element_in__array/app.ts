691bdeb2753461ff1879b2338ac0e1e1b54e8e03
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-08-05 17:51:29 +0530

    clean up

--- codewars/7kyu/find_all_occurrences_of_an_element_in__array/app.ts
+++ codewars/7kyu/find_all_occurrences_of_an_element_in__array/app.ts
@@ -3,6 +3,5 @@ export function findAll(array: number[], n: number): number[] {
   for (let i = 0; i < array.length; i++) {
     if (array[i] === n) res.push(i);
   }
-  console.log(res);
   return res;
 }
