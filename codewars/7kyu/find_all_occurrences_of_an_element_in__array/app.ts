babaac8bebb86c49b72d0807b0803f21fd47917b
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-08-05 17:50:47 +0530

    format document

--- codewars/7kyu/find_all_occurrences_of_an_element_in__array/app.ts
+++ codewars/7kyu/find_all_occurrences_of_an_element_in__array/app.ts
@@ -1,9 +1,8 @@
-export function findAll(array: number[], n: number): number[]{
-  let res:number[] = [];
-  for(let i=0; i<array.length; i++){
-    if(array[i]===n) res.push(i)
+export function findAll(array: number[], n: number): number[] {
+  let res: number[] = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === n) res.push(i);
   }
   console.log(res);
   return res;
-  
 }
