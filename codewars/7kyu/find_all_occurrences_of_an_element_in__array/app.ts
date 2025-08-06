8a97b8493c7ae787452d6ed2d63a2aa74fd7a854
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-08-06 15:50:59 +0530

    add laternate solution

--- codewars/7kyu/find_all_occurrences_of_an_element_in__array/app.ts
+++ codewars/7kyu/find_all_occurrences_of_an_element_in__array/app.ts
@@ -5,3 +5,9 @@ export function findAll(array: number[], n: number): number[] {
   }
   return res;
 }
+
+
+// alternate solution
+export function findAllAlt(array: number[], n: number): number[] {
+return array.reduce<number[]>((acc: number[], curr: number, ind: number) => curr === n ? [...acc, ind] : acc, [])
+}
