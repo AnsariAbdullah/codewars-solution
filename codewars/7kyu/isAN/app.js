af85dab4679fdf7f0c2a515c953e9a1073d2cef8
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-08-23 13:01:32 +0530

    refactor code

--- codewars/7kyu/isAN/app.js
+++ codewars/7kyu/isAN/app.js
@@ -1,6 +1,3 @@
 function isAN(value) {
-  if (typeof value === "number") return !Number.isNaN(value);
-  if (value instanceof Number) return !Number.isNaN(value.valueOf());
-
-  return false;
+  return (value instanceof Number || typeof value === 'number') && !Number.isNaN(value)
 }
