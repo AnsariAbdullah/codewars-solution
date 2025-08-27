c5a3f6396173427de05aab0cee2d13a742404cca
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-08-28 01:28:30 +0530

    add speed_control solution

---
+++ codewars/7kyu/speed_control/app.js
@@ -0,0 +1,13 @@
+function gps(s, x) {
+  if (x.length <= 1) return 0;
+  
+  let maxSpeed = 0;
+  
+  for(let i=0; i<x.length; i++){
+    const delta = x[i] - x[i-1];
+    const speed = (3600 * delta) / s;
+    if(speed > maxSpeed) maxSpeed = speed;
+  }
+  
+  return maxSpeed;
+}
