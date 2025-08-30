89084e64a637caee389b548406802b4a960fa7ba
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-08-30 16:50:14 +0530

    add TS solution

---
+++ codewars/7kyu/speed_control/app.ts
@@ -0,0 +1,11 @@
+export const gps = (s:number, xs:number[]):number => {
+  if(xs.length <= 1) return 0
+  
+  let maxSpeed = 0;
+  for(let i=0; i<xs.length; i++){
+    const delta = xs[i] - xs[i-1];
+    const speed = (3600 * delta) / s;
+    if (speed > maxSpeed) maxSpeed = speed;
+  }
+  return maxSpeed
+}
