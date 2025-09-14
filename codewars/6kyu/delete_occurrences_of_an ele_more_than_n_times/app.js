b69a495755ef95a5545385b34d2256dab29d24d6
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-09-15 00:44:28 +0530

    format document

--- codewars/6kyu/delete_occurrences_of_an ele_more_than_n_times/app.js
+++ codewars/6kyu/delete_occurrences_of_an ele_more_than_n_times/app.js
@@ -1,12 +1,12 @@
-function deleteNth(arr,n){
-  let count = {}
-  let result = []
-  for(const num of arr){
-  count[num] = (count[num] || 0) + 1;
-  if(count[num] <= n){
-    result.push(num)
+function deleteNth(arr, n) {
+  let count = {};
+  let result = [];
+  for (const num of arr) {
+    count[num] = (count[num] || 0) + 1;
+    if (count[num] <= n) {
+      result.push(num);
+    }
   }
-  }
-  
+
   return result;
 }
