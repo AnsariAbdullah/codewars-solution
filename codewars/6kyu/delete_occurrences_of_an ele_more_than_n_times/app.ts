5298a93c418f6fca1e7a54523681d129826a4c58
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-09-17 02:03:21 +0530

    format document

--- codewars/6kyu/delete_occurrences_of_an ele_more_than_n_times/app.ts
+++ codewars/6kyu/delete_occurrences_of_an ele_more_than_n_times/app.ts
@@ -1,12 +1,12 @@
-export function deleteNth(arr: number[],n: number): number[]{
-  let count: {[key:number] : number} = {}
-  let result: number[] = []
-  for(const num of arr){
+export function deleteNth(arr: number[], n: number): number[] {
+  let count: { [key: number]: number } = {};
+  let result: number[] = [];
+  for (const num of arr) {
     count[num] = (count[num] || 0) + 1;
-    if(count[num] <= n){
-      result.push(num)
+    if (count[num] <= n) {
+      result.push(num);
     }
   }
-  
+
   return result;
 }
