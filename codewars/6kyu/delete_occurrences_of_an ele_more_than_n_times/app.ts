4dbfdd82a8797ad41313313d9724590c20f8c553
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-09-16 22:23:08 +0530

    add TS solution

---
+++ codewars/6kyu/delete_occurrences_of_an ele_more_than_n_times/app.ts
@@ -0,0 +1,12 @@
+export function deleteNth(arr: number[],n: number): number[]{
+  let count = {}
+  let result: number[] = []
+  for(const num of arr){
+    count[num] = (count[num] || 0) + 1;
+    if(count[num] <= n){
+      result.push(num)
+    }
+  }
+  
+  return result;
+}
