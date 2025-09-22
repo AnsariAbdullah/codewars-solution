638cfe2cbe8ff431505191ad1430fc329d4c89c8
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-09-23 00:38:50 +0530

    add building_blocks solution

---
+++ codewars/7kyu/building_blocks/app.js
@@ -0,0 +1,29 @@
+class Block{
+  constructor(data){
+    [this.width, this.length, this.height] = data;
+  }
+	
+  getWidth(){
+    return this.width;
+  }
+
+  getLength(){
+    return this.length;
+  }
+
+  getHeight(){
+    return this.height;
+  }
+
+  getVolume(){
+    return this.width * this.length * this.height;
+  }
+
+  getSurfaceArea(){
+    return 2 * (
+      this.height * this.length +
+      this.height * this.width +
+      this.length * this.width
+    )
+  }
+}
