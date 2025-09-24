5340d678159791fdfcaeee9d7fc06998ce9b1687
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-09-25 00:58:53 +0530

    add TS solution

---
+++ codewars/7kyu/building_blocks/app.ts
@@ -0,0 +1,34 @@
+export class Block {
+  private width: number;
+  private length: number;
+  private height: number;
+
+  constructor(data: [number, number, number]) {
+    [this.width, this.length, this.height] = data;
+  }
+
+  getWidth(): number {
+   return this.width;
+  }
+
+  getLength(): number {
+  return this.length;
+  }
+
+  getHeight(): number {
+return this.height;
+  }
+
+  getVolume(): number {
+    return this.width * this.length * this.height;
+  }
+
+  getSurfaceArea(): number {
+    return (
+      2 *
+      (this.height * this.length +
+        this.height * this.width +
+        this.length * this.width)
+    );
+  }
+}
