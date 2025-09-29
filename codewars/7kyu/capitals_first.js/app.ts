ec4392b3a5bbdd3ce7faafc6e4cc7b2452fe6b14
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-09-30 02:50:40 +0530

    add TS solution

---
+++ codewars/7kyu/capitals_first.js/app.ts
@@ -0,0 +1,15 @@
+export function capitalsFirst(input: string): string {
+  const words = input.split(' ');
+  const uppers: string[] = [];
+  const lowers: string[] = [];
+
+  for (const word of words) {
+    if (/^[A-Z]/.test(word)) {
+      uppers.push(word);
+    } else if (/^[a-z]/.test(word)) {
+      lowers.push(word);
+    }
+  }
+
+  return [...uppers, ...lowers].join(" ");
+}
