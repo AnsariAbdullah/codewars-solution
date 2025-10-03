32f809bb967c46c050568f045a81ad3cdf13ee6d
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-10-03 23:43:15 +0530

    format document

--- codewars/7kyu/most_digits/app.js
+++ codewars/7kyu/most_digits/app.js
@@ -1,8 +1,8 @@
-function findLongest(array){
+function findLongest(array) {
   let res = array[0].toString();
-  for(let i=1; i<array.length; i++){
+  for (let i = 1; i < array.length; i++) {
     let curr = array[i].toString();
-    if(curr.length > res.length) res = curr
+    if (curr.length > res.length) res = curr;
   }
   return Number(res);
 }
