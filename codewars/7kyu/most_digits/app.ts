400494cb2dc567707db697762a891b5314a62066
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-10-04 01:31:47 +0530

    add TS solution

---
+++ codewars/7kyu/most_digits/app.ts
@@ -0,0 +1,10 @@
+export class Kata {
+  static findLongest(array:number[]):number {
+    let res = array[0].toString();
+    for(let i=1; i<array.length; i++){
+      let curr = array[i].toString();
+      if(curr.length > res.length) res = curr
+    }
+    return Number(res);
+  }
+}
