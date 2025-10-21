082f3bef672bc1bdfa5725369cfa84fdf53b5c5b
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-10-22 01:49:02 +0530

    add alternate solution

--- codewars/8kyu/do_you_speak_english/app.ts
+++ codewars/8kyu/do_you_speak_english/app.ts
@@ -1,3 +1,8 @@
 export function spEng(sentence: string): boolean {
   return sentence.toLowerCase().includes("english");
 }
+
+// alternate solution
+export function spEngAlt(sentence: string): boolean{
+  return /english/i.test(sentence);
+}
