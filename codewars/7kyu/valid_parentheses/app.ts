6c277bf14c67b5a8b6b13b5d8549a20d0b31e97b
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-10-28 01:25:38 +0530

    add TS solution

---
+++ codewars/7kyu/valid_parentheses/app.ts
@@ -0,0 +1,14 @@
+export function validParentheses(parenStr: string): boolean {
+  let balance = 0;
+
+  for (const char of parenStr) {
+    if (char === '(') {
+      balance++;
+    } else if (char === ')') {
+      balance--;
+      if (balance < 0) return false;
+    }
+  }
+
+  return balance === 0;
+}
