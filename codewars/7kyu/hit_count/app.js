1a716faeb161dff878762346f02e20b3c6ecfae1
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-11-04 22:10:23 +0530

    add hit_count solution

---
+++ codewars/7kyu/hit_count/app.js
@@ -0,0 +1,11 @@
+function counterEffect(hitCount) {
+  let result  = [];
+  for(let i=0; i<hitCount.length; i++){
+    let tempArr = [];
+    for(let j=0; j<=hitCount[i]; j++){
+      tempArr.push(j)
+    }
+    result.push(tempArr)
+  }
+  return result;
+}
