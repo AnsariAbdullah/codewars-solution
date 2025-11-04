60b8d8ddf966e24f00101065fe1c2ee5fc1487c1
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-11-05 00:29:01 +0530

    format document

--- codewars/7kyu/hit_count/app.js
+++ codewars/7kyu/hit_count/app.js
@@ -1,11 +1,11 @@
 function counterEffect(hitCount) {
-  let result  = [];
-  for(let i=0; i<hitCount.length; i++){
+  let result = [];
+  for (let i = 0; i < hitCount.length; i++) {
     let tempArr = [];
-    for(let j=0; j<=hitCount[i]; j++){
-      tempArr.push(j)
+    for (let j = 0; j <= hitCount[i]; j++) {
+      tempArr.push(j);
     }
-    result.push(tempArr)
+    result.push(tempArr);
   }
   return result;
 }
