92a7bf69cb50f8503c678b5e7a94d813f6795e53
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-11-07 01:22:44 +0530

    format document

--- codewars/7kyu/hit_count/app.ts
+++ codewars/7kyu/hit_count/app.ts
@@ -1,11 +1,11 @@
 export function counterEffect(hitCount: string): number[][] {
-  let result  = [];
-  for(let i=0; i<hitCount.length; i++){
+  let result = [];
+  for (let i = 0; i < hitCount.length; i++) {
     let tempArr = [];
-    for(let j=0; j<=Number(hitCount[i]); j++){
-      tempArr.push(j)
+    for (let j = 0; j <= Number(hitCount[i]); j++) {
+      tempArr.push(j);
     }
-    result.push(tempArr)
+    result.push(tempArr);
   }
   return result;
 }
