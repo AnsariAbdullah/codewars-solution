d5c951e0ea0bbf9bf170a8f6aaf735f3a2ccfd58
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-11-22 17:48:32 +0530

    format document

--- codewars/8kyu/fundamentals_return/app.ts
+++ codewars/8kyu/fundamentals_return/app.ts
@@ -1,23 +1,23 @@
-export function add(a: number,b: number): number{
-    return a+b;
+export function add(a: number, b: number): number {
+  return a + b;
 }
 
-export function divide(a: number,b: number): number{
-    return a/b;
+export function divide(a: number, b: number): number {
+  return a / b;
 }
 
-export function multiply(a: number,b: number): number{
-    return a*b;
+export function multiply(a: number, b: number): number {
+  return a * b;
 }
 
-export function mod(a: number,b: number): number{
-    return a%b;
+export function mod(a: number, b: number): number {
+  return a % b;
 }
-   
-export function exponent(a: number,b: number): number{
-    return a**b;
+
+export function exponent(a: number, b: number): number {
+  return a ** b;
 }
-    
-export function subt(a: number,b: number): number{
-    return a-b;
+
+export function subt(a: number, b: number): number {
+  return a - b;
 }
