51c9bf138df06b07ac9c7c70f9322aa92d9986c2
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-11-24 21:11:40 +0530

    format document

--- codewars/8kyu/be_concise_I_the_ternary_operator/app.ts
+++ codewars/8kyu/be_concise_I_the_ternary_operator/app.ts
@@ -1,3 +1,5 @@
 export function describeAge(a: number): string {
-  return `You're a(n) ${a<13?'kid':a<18?'teenager':a<65?'adult':'elderly'}`
+  return `You're a(n) ${
+    a < 13 ? "kid" : a < 18 ? "teenager" : a < 65 ? "adult" : "elderly"
+  }`;
 }
