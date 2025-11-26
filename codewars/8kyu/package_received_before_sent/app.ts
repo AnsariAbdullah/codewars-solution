79190bdaad387ddfe0214b8d47039f1eca6b37a3
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-11-27 01:59:02 +0530

    format document

--- codewars/8kyu/package_received_before_sent/app.ts
+++ codewars/8kyu/package_received_before_sent/app.ts
@@ -1,4 +1,9 @@
-export function was_package_received_yesterday(tz_from: number, tz_to: number, start: number, duration: number): boolean {
+export function was_package_received_yesterday(
+  tz_from: number,
+  tz_to: number,
+  start: number,
+  duration: number
+): boolean {
   const utc_send = start - tz_from;
   const utc_arrival = utc_send + duration;
   const local_arrival = utc_arrival + tz_to;
