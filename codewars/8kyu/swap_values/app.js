c43ac2e0f93f60aa83fec6bcff83378601786fd8
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-11-29 21:51:44 +0530

    format document

--- codewars/8kyu/swap_values/app.js
+++ codewars/8kyu/swap_values/app.js
@@ -2,8 +2,7 @@ function swapValues() {
   [arguments[0][0], arguments[0][1]] = [arguments[0][1], arguments[0][0]];
 }
 
-
 // alternate solution
 function swapValues(arr) {
-  return arr.reverse()
+  return arr.reverse();
 }
