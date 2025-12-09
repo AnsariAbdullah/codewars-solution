ca793432081dd656ab4c3095db40ba7e3370ff1d
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-12-10 00:56:34 +0530

    format document

--- codewars/6kyu/convert_string_to_camel_case/app.ts
+++ codewars/6kyu/convert_string_to_camel_case/app.ts
@@ -1,3 +1,6 @@
-export function toCamelCase(str: string): string{
-  return str.split(/[-_]/).map((w, i) => i===0 ? w : w[0].toUpperCase() + w.slice(1)).join('')
+export function toCamelCase(str: string): string {
+  return str
+    .split(/[-_]/)
+    .map((w, i) => (i === 0 ? w : w[0].toUpperCase() + w.slice(1)))
+    .join("");
 }
