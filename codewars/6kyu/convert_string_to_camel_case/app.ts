91d8132c8c3d3e4740b6eb92518ff40f7c598995
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-12-11 00:36:47 +0530

    add alternate solution

--- codewars/6kyu/convert_string_to_camel_case/app.ts
+++ codewars/6kyu/convert_string_to_camel_case/app.ts
@@ -4,3 +4,8 @@ export function toCamelCase(str: string): string {
     .map((w, i) => (i === 0 ? w : w[0].toUpperCase() + w.slice(1)))
     .join("");
 }
+
+// alternate solution
+function alToCamelCase(str: string): string{
+  return str.replace(/[-_](.)/g, (_, c) => c.toUpperCase())
+}
