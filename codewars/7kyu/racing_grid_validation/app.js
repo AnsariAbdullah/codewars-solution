51bf01244b40844bf3809fed46735d04a6af0d91
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-12-18 01:37:48 +0530

    add alternate solution

--- codewars/7kyu/racing_grid_validation/app.js
+++ codewars/7kyu/racing_grid_validation/app.js
@@ -13,3 +13,18 @@ function validateRace(moves) {
 
   return true;
 }
+// alternate solution
+let validateRace = (m) =>
+  !m
+    .map((a, b) => a + b)
+    .sort((a, b) => a - b)
+    .some((a, b) => a ^ b);
+
+// alternate solution
+function validateRace(moves) {
+  const final = moves.map((v, i) => i + v);
+  return (
+    final.every((x) => x >= 0 && x < moves.length) &&
+    new Set(final).size === moves.length
+  );
+}
