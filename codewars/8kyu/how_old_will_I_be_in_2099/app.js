d1945919b69b0ebdb697483c6e0ff8ebf391eea7
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-01-20 00:20:39 +0530

    format document

--- codewars/8kyu/how_old_will_I_be_in_2099/app.js
+++ codewars/8kyu/how_old_will_I_be_in_2099/app.js
@@ -13,9 +13,9 @@ function calculateAge(birthYear, targetYear) {
 }
 
 // alternate solution
-function  calculateAge(m, n) {
-	if(m == n) return 'You were born this very year!';
-  var year = Math.abs(m-n) == 1 ? 'year' : 'years';
-  if(m < n) return "You are "+(n-m)+' '+year+' old.';
-  if(m > n) return "You will be born in "+(-n+m)+' '+year+'.';
+function calculateAge(m, n) {
+  if (m == n) return "You were born this very year!";
+  var year = Math.abs(m - n) == 1 ? "year" : "years";
+  if (m < n) return "You are " + (n - m) + " " + year + " old.";
+  if (m > n) return "You will be born in " + (-n + m) + " " + year + ".";
 }
