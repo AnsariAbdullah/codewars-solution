e8342ef57c47e81bf0c74b38163bd00f32d8a7f1
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-01-19 22:59:31 +0530

    format document

--- codewars/8kyu/how_old_will_I_be_in_2099/app.js
+++ codewars/8kyu/how_old_will_I_be_in_2099/app.js
@@ -1,11 +1,13 @@
-function  calculateAge(birthYear, targetYear) {  
+function calculateAge(birthYear, targetYear) {
   let diff = targetYear - birthYear;
-  if(diff === 0) return "You were born this very year!"
-  
-  if(diff > 0){
-    return `You are ${diff} year${diff === 1 ? "" : "s"} old.`
+  if (diff === 0) return "You were born this very year!";
+
+  if (diff > 0) {
+    return `You are ${diff} year${diff === 1 ? "" : "s"} old.`;
   }
-  
+
   const yearsUntilBorn = Math.abs(diff);
-  return `You will be born in ${yearsUntilBorn} year${yearsUntilBorn === 1 ? "" : "s"}.`
+  return `You will be born in ${yearsUntilBorn} year${
+    yearsUntilBorn === 1 ? "" : "s"
+  }.`;
 }
