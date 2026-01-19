ad2546f1f00d75891c1ff64867782e23864783d3
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-01-19 22:58:56 +0530

    add how_old_will_I_be_in_2099 solution

---
+++ codewars/8kyu/how_old_will_I_be_in_2099/app.js
@@ -0,0 +1,11 @@
+function  calculateAge(birthYear, targetYear) {  
+  let diff = targetYear - birthYear;
+  if(diff === 0) return "You were born this very year!"
+  
+  if(diff > 0){
+    return `You are ${diff} year${diff === 1 ? "" : "s"} old.`
+  }
+  
+  const yearsUntilBorn = Math.abs(diff);
+  return `You will be born in ${yearsUntilBorn} year${yearsUntilBorn === 1 ? "" : "s"}.`
+}
