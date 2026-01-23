d17065fc005b46c0358e44e427ce8fecdff2cfff
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-01-23 23:22:01 +0530

    add TS solution

---
+++ codewars/7kyu/going_to_the_cinema/app.ts
@@ -0,0 +1,15 @@
+export function movie(card: number, ticket: number, perc: number): number {
+  let n = 0;
+  let totalA = 0;
+  let totalB = card;
+  let currentTicketPrice = ticket;
+
+  while (Math.ceil(totalB) >= totalA) {
+    n++;
+    totalA += ticket;
+    currentTicketPrice *= perc;
+    totalB += currentTicketPrice;
+  }
+
+  return n;
+}
