baedb336a2d6c50168ffcbb7ba0917b7752f6367
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-01-28 22:36:13 +0530

    format document

--- codewars/7kyu/ordered_count_of_characters/app.ts
+++ codewars/7kyu/ordered_count_of_characters/app.ts
@@ -1,9 +1,7 @@
 export function orderedCount(text: string): [string, number][] {
   const map = new Map();
-
   for (const char of text) {
     map.set(char, (map.get(char) || 0) + 1);
   }
-
   return Array.from(map.entries());
 }
