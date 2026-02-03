6581af3368cb4ecee07ea1da88efc8d5507c7b23
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-02-03 22:22:14 +0530

    format document

--- codewars/6kyu/complementory_dna/app.js
+++ codewars/6kyu/complementory_dna/app.js
@@ -1,22 +1,25 @@
-function DNAStrand(dna){
+function DNAStrand(dna) {
   let newArr = [];
-  dna.split('').map(item => {
-    if(item == 'A'){
-      newArr.push('T')
-    }else if(item == 'T'){
-      newArr.push('A')
-    }else if(item == 'C'){
-      newArr.push('G')
-    }else{
-      newArr.push('C')
+  dna.split("").map((item) => {
+    if (item == "A") {
+      newArr.push("T");
+    } else if (item == "T") {
+      newArr.push("A");
+    } else if (item == "C") {
+      newArr.push("G");
+    } else {
+      newArr.push("C");
     }
-  })
-  return newArr.join('');
+  });
+  return newArr.join("");
 }
 
 // alternate solution
-const pairs = {'A':'T','T':'A','C':'G','G':'C'};
+const pairs = { A: "T", T: "A", C: "G", G: "C" };
 
-function dnaStrand(dna){
-  return dna.split("").map(item => pairs[item]).join("");
+function dnaStrand(dna) {
+  return dna
+    .split("")
+    .map((item) => pairs[item])
+    .join("");
 }
