a6f9d25cc4271dcf85fb3792e9df33ad5cb0c7f6
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-02-13 01:43:48 +0530

    add product_of_largest_pair solution

---
+++ codewars/7kyu/product_of_largest_pair/app.js
@@ -0,0 +1,13 @@
+function maxProduct(a) {
+  let biggest = 0;
+  let secondBiggest = 0;
+  for(const num of a){
+    if(num > biggest){
+      secondBiggest = biggest;
+      biggest = num;
+    }else if( num > secondBiggest){
+      secondBiggest = num;
+    }
+  }
+  return biggest * secondBiggest; 
+}
