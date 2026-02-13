78235361dd6ec8dc11a30d00c7985390cfc5fac2
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-02-14 01:10:22 +0530

    add alternate solution

--- codewars/8kyu/a_strange_trip_to_the_market/app.ts
+++ codewars/8kyu/a_strange_trip_to_the_market/app.ts
@@ -3,3 +3,8 @@ export function isLochNessMonster(s: string): boolean {
     s.includes("3.50") || s.includes("three fifty") || s.includes("tree fiddy")
   );
 }
+
+// alternate solution
+export function isLochNessMonsterAlt(s: string): boolean {
+return /tree fiddy|3\.50|three fifty/i.test(s)
+}
