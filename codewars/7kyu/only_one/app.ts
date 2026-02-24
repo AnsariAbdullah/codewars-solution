be476bc12d02875ab56b9ec2391154cc3572abd4
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-02-25 00:45:21 +0530

    add TS solution

---
+++ codewars/7kyu/only_one/app.ts
@@ -0,0 +1,8 @@
+export function onlyOne(...allParams: boolean[]): boolean {
+  let trueCount = 0;
+  allParams.forEach(item => {
+    if(item === true) trueCount++
+  })
+  return trueCount === 1;
+
+}
