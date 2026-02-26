5cb60ebcd5a28a764c0aee9f3f9d4d9bc5e7ef49
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-02-26 06:47:26 +0530

    format document

--- codewars/7kyu/only_one/app.ts
+++ codewars/7kyu/only_one/app.ts
@@ -1,8 +1,7 @@
 export function onlyOne(...allParams: boolean[]): boolean {
   let trueCount = 0;
-  allParams.forEach(item => {
-    if(item === true) trueCount++
-  })
+  allParams.forEach((item) => {
+    if (item === true) trueCount++;
+  });
   return trueCount === 1;
-
 }
