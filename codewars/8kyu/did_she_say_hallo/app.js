8d92cf6b4976b27c850e18edb25aca492ea1ad73
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-02-27 06:49:43 +0530

    add did_she_say_hallo solution

---
+++ codewars/8kyu/did_she_say_hallo/app.js
@@ -0,0 +1,5 @@
+function validateHello(greetings) {
+  const text = greetings.toLowerCase();
+  const helloWords = ["hello", "ciao", "salut", "hallo", "hola", "ahoj", "czesc"];
+  return helloWords.some(word => text.includes(word))
+}
