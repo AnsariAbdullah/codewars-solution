627a26a5bef149beb535b66262775423160462b7
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-03-10 01:18:45 +0530

    format document

--- codewars/6kyu/IPv4_to_int32/app.ts
+++ codewars/6kyu/IPv4_to_int32/app.ts
@@ -1,6 +1,8 @@
 export function ipToInt32(ip: string): number {
-  return ip
-    .split('.')
-    .map(Number)
-    .reduce((acc, octet) => (acc << 8) | octet, 0) >>> 0;
+  return (
+    ip
+      .split(".")
+      .map(Number)
+      .reduce((acc, octet) => (acc << 8) | octet, 0) >>> 0
+  );
 }
