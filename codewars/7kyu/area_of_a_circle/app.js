6c1d5230eef12df9919782813054e96e4fb111b0
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-03-17 00:59:55 +0530

    format document

--- codewars/7kyu/area_of_a_circle/app.js
+++ codewars/7kyu/area_of_a_circle/app.js
@@ -1,4 +1,4 @@
 function circleArea(radius) {
-  if(radius <=0 ) throw new Error('Incorrect value');
- return Math.PI * radius * radius;
+  if (radius <= 0) throw new Error("Incorrect value");
+  return Math.PI * radius * radius;
 }
