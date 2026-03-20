8b46d660df04855ce2344c2b924bc4256e420634
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-03-20 22:55:46 +0530

    format document

--- codewars/8kyu/job_matching_1/app.ts
+++ codewars/8kyu/job_matching_1/app.ts
@@ -10,6 +10,5 @@ export function match(candidate: Candidate, job: Job): boolean {
   if (candidate.minSalary == null || job.maxSalary == null) {
     throw new Error("Salary missing");
   }
-
   return job.maxSalary >= candidate.minSalary * 0.9;
 }
