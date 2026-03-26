cd0e2dc3233e9aefa12fe9326d3cf6ec947029f6
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-03-27 01:10:55 +0530

    format document

--- codewars/7kyu/job_matching_2/app.js
+++ codewars/7kyu/job_matching_2/app.js
@@ -1,15 +1,15 @@
 function match(job, candidates) {
-  return candidates.filter(candidate => {
+  return candidates.filter((candidate) => {
     if (candidate.desiresEquity && job.equityMax === 0) {
       return false;
     }
-		
+
     const candidateLocations = [
       candidate.currentLocation,
-      ...candidate.desiredLocations
+      ...candidate.desiredLocations,
     ];
 
-    const locationMatch = job.locations.some(location =>
+    const locationMatch = job.locations.some((location) =>
       candidateLocations.includes(location)
     );
 
