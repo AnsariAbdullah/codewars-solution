07d0adaf27b98868e81912c50dfd20acb97ee93c
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-03-31 21:53:00 +0530

    add altERnaTIng_cAsE solution

---
+++ codewars/8kyu/altERnaTIng_cAsE/app.js
@@ -0,0 +1,8 @@
+String.prototype.toAlternatingCase = function () {
+  return this.split('').map(c => {
+    if(c == c.toUpperCase()){
+      return c.toLowerCase()
+    }else{
+      return c.toUpperCase()
+    } }).join('')
+}
