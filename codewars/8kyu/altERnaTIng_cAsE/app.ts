172260e6ab2867e25334ffa54a723e5b2cfe9d00
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-04-04 15:35:47 +0530

    add alternate solution

--- codewars/8kyu/altERnaTIng_cAsE/app.ts
+++ codewars/8kyu/altERnaTIng_cAsE/app.ts
@@ -10,3 +10,17 @@ export function toAlternatingCase(s: string): string {
     })
     .join("");
 }
+
+
+// alternate solution
+export function toAlternatingCaseAlt(s: string): string {
+  let newStr = "";
+  for(let i=0; i<s.length; i++){
+  if(s[i] == s[i].toUpperCase()){
+      newStr += s[i].toLowerCase();
+    }else{
+      newStr += s[i].toUpperCase();
+    }
+  }
+  return newStr;
+}
