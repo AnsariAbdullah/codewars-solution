1801470993cd2cfa7372eef0340cd1cbd3cd3dd2
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-04-09 00:04:41 +0530

    add TS solution

---
+++ codewars/7kyu/odd_ones_out/app.ts
@@ -0,0 +1,7 @@
+export function oddOnesOut(nums: number[]) {
+  let counts: Record<number, number> = {};
+  for(const ele of nums){
+    counts[ele] = (counts[ele] || 0) + 1
+  }
+return nums.filter((num) => counts[num] % 2 === 0)
+}
