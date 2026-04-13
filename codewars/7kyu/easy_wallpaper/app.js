e95caab1e6617f727cc3ccf43201142696c00bd1
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-04-13 09:33:46 +0530

    format document

--- codewars/7kyu/easy_wallpaper/app.js
+++ codewars/7kyu/easy_wallpaper/app.js
@@ -1,7 +1,7 @@
 function wallpaper(l, w, h) {
-  if(l ===0 || w === 0 || h===0)  return "zero";
-  let wallArea = 2 * h * (l+w);
+  if (l === 0 || w === 0 || h === 0) return "zero";
+  let wallArea = 2 * h * (l + w);
   let extraRoll = wallArea * 1.15;
   let numberOfRolls = extraRoll / 5.2;
-  return numbers[Math.ceil(numberOfRolls)]
+  return numbers[Math.ceil(numberOfRolls)];
 }
