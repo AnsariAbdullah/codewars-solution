bb02d2f70211751572008585a6e2e6a489bcc6fa
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-04-18 01:05:47 +0530

    format document

--- codewars/7kyu/chain_me/app.ts
+++ codewars/7kyu/chain_me/app.ts
@@ -1,7 +1,7 @@
 export function chain(input: number, fs: ((num: number) => number)[]): number {
   let val = input;
-  for(let fn of fs){
-    val = fn(val)
+  for (let fn of fs) {
+    val = fn(val);
   }
-return val;
+  return val;
 }
