6a65751e02a54ce0c4e06b9aa23d4b608d3456dd
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-04-18 12:57:51 +0530

    format document

--- codewars/8kyu/finish_guess_the_number_game/app.js
+++ codewars/8kyu/finish_guess_the_number_game/app.js
@@ -3,14 +3,14 @@ class Guesser {
     this.number = number;
     this.lives = lives;
   }
-  
-guess(n) {
-    if(this.lives <= 0) throw new Error("Limit exceeded");
-    if(this.number === n){
-       return true
-    }else{
-      this.lives = this.lives - 1; 
+
+  guess(n) {
+    if (this.lives <= 0) throw new Error("Limit exceeded");
+    if (this.number === n) {
+      return true;
+    } else {
+      this.lives = this.lives - 1;
       return false;
-    };
+    }
   }
 }
