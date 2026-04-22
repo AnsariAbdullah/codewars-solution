d50570e99cae6ffe8280b7afbf08843469d126b7
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-04-23 00:52:40 +0530

    add how_many_lonely_letters solution

---
+++ codewars/7kyu/how_many_lonely_letters/app.js
@@ -0,0 +1,28 @@
+function countLonelyLetters(text) {
+  const letters = text.toLowerCase().replace(/[^a-z]/g, '');
+  
+  const freq = {};
+for (const ch of letters) {
+	freq[ch] = (freq[ch] || 0) + 1;
+}
+
+  let count = 0;
+
+  for (const ch in freq) {
+    if (freq[ch] === 1) {
+      const code = ch.charCodeAt(0);
+
+      const prev = String.fromCharCode(code - 1);
+      const next = String.fromCharCode(code + 1);
+
+      const hasPrev = code > 97 && freq[prev];
+      const hasNext = code < 122 && freq[next];
+
+      if (!hasPrev && !hasNext) {
+        count++;
+      }
+    }
+  }
+
+  return count;
+}
