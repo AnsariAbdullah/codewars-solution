dd0463f30743fbc0fa8768fcafa5656c6f5752fb
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-05-04 01:50:41 +0530

    format document

--- codewars/7kyu/find_the_middle_of_the_product/app.js
+++ codewars/7kyu/find_the_middle_of_the_product/app.js
@@ -1,25 +1,25 @@
-function getMiddleChar(str){
-    let stringLength = str.length;
-    let mid = stringLength / 2;
-    let res = '';
-    if(str.length % 2 === 0){
-        res = str.slice(mid - 1, mid + 1);
-    }else{
-        res = str.charAt(Math.floor(mid));
-    }
-    return Number(res)
+function getMiddleChar(str) {
+  let stringLength = str.length;
+  let mid = stringLength / 2;
+  let res = "";
+  if (str.length % 2 === 0) {
+    res = str.slice(mid - 1, mid + 1);
+  } else {
+    res = str.charAt(Math.floor(mid));
+  }
+  return Number(res);
 }
 
-function findMiddle(str){
-  if (typeof str !== 'string') return -1
-  
+function findMiddle(str) {
+  if (typeof str !== "string") return -1;
+
   const cleaned = str.replace(/\D/g, "");
-  
-  if(cleaned === '') return -1
-  
+
+  if (cleaned === "") return -1;
+
   let product = 1;
-  cleaned.split('').forEach(itm => {
-      product *= itm
+  cleaned.split("").forEach((itm) => {
+    product *= itm;
   });
   return getMiddleChar(String(product));
 }
