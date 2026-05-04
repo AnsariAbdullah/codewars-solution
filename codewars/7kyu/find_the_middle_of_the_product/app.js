7b7c877107537563a45e5faaeb1d7ca1a338fc41
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-05-05 02:26:13 +0530

    format document

--- codewars/7kyu/find_the_middle_of_the_product/app.js
+++ codewars/7kyu/find_the_middle_of_the_product/app.js
@@ -1,27 +1,29 @@
-function extractDigits(str){
-  if(typeof str !== 'string') return null
-  
-  const cleaned = str.replace(/\D/g, '');
+function extractDigits(str) {
+  if (typeof str !== "string") return null;
 
-return cleaned ? cleaned : null;
+  const cleaned = str.replace(/\D/g, "");
+
+  return cleaned ? cleaned : null;
 }
 
-function getMiddleNumber(value){
+function getMiddleNumber(value) {
   const str = String(value);
-const mid = Math.floor(str.length / 2);
-  
-  return str.length % 2 === 0 ? Number(str.slice(mid-1, mid+1)) : Number(str[mid])
+  const mid = Math.floor(str.length / 2);
+
+  return str.length % 2 === 0
+    ? Number(str.slice(mid - 1, mid + 1))
+    : Number(str[mid]);
 }
 
-function findMiddle(str){
+function findMiddle(str) {
   const nums = extractDigits(str);
-  
-if(!nums) return -1
-  
+
+  if (!nums) return -1;
+
   let product = 1;
-  nums.split('').forEach(itm => {
-      product *= itm
+  nums.split("").forEach((itm) => {
+    product *= itm;
   });
-  
+
   return getMiddleNumber(product);
 }
