6fd1ddc6eb049f606cb66cf2787f71ad01d3757a
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-05-06 04:39:13 +0530

    format document

--- codewars/7kyu/find_the_middle_of_the_product/app.ts
+++ codewars/7kyu/find_the_middle_of_the_product/app.ts
@@ -1,26 +1,28 @@
 export function extractDigits(str: string): string | null {
-  if (typeof str !== 'string') return null;
+  if (typeof str !== "string") return null;
 
-const cleaned = str.replace(/\D/g, '');
+  const cleaned = str.replace(/\D/g, "");
 
- return cleaned ? cleaned : null;
+  return cleaned ? cleaned : null;
 }
 
 export function getMiddleNumber(value: number): number {
   const str = String(value);
- const mid = Math.floor(str.length / 2);
+  const mid = Math.floor(str.length / 2);
 
- return str.length % 2 === 0 ? Number(str.slice(mid - 1, mid + 1)) : Number(str[mid]);
+  return str.length % 2 === 0
+    ? Number(str.slice(mid - 1, mid + 1))
+    : Number(str[mid]);
 }
 
 export function findMiddle(str: string): number {
   const nums = extractDigits(str);
 
- if (!nums) return -1;
+  if (!nums) return -1;
 
- let product = 1;
+  let product = 1;
 
-  nums.split('').forEach((itm: string) => {
+  nums.split("").forEach((itm: string) => {
     product *= Number(itm);
   });
 
