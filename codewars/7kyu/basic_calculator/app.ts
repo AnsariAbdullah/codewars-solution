9e1fbc7dda69b4123b572c4e845ae7944cadc2a8
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-05-08 01:50:45 +0530

    add TS solution

---
+++ codewars/7kyu/basic_calculator/app.ts
@@ -0,0 +1,18 @@
+export function calculate(a:number, operator: string, b: number): number | null {
+ switch (operator){
+   case "+":
+    return a+b
+   case "-":
+    return a-b
+   case "/":
+     if(b==0){
+       return null
+     }else{
+       return a/b
+     }
+  case "*":
+    return a*b
+   default:
+    return null;  
+ }
+}
