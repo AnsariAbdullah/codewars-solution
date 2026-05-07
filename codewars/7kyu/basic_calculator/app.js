25cd67b9a8f3b074f06767c9e982112ecf96724c
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-05-08 01:49:47 +0530

    format document

--- codewars/7kyu/basic_calculator/app.js
+++ codewars/7kyu/basic_calculator/app.js
@@ -1,18 +1,18 @@
 function calculate(a, operator, b) {
- switch (operator){
-   case "+":
-    return a+b
-   case "-":
-    return a-b
-   case "/":
-     if(b==0){
-       return null
-     }else{
-       return a/b
-     }
-  case "*":
-    return a*b
-   default:
-    return null;  
- }
+  switch (operator) {
+    case "+":
+      return a + b;
+    case "-":
+      return a - b;
+    case "/":
+      if (b == 0) {
+        return null;
+      } else {
+        return a / b;
+      }
+    case "*":
+      return a * b;
+    default:
+      return null;
+  }
 }
