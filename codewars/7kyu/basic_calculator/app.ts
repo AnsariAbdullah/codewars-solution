14c22ed7a4151570a3c95211e766854fc90eb063
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-05-12 00:36:57 +0530

    format document

--- codewars/7kyu/basic_calculator/app.ts
+++ codewars/7kyu/basic_calculator/app.ts
@@ -1,12 +1,16 @@
-export function calculate(a: number, operator: string, b: number): number | null {
+export function calculate(
+  a: number,
+  operator: string,
+  b: number
+): number | null {
   switch (operator) {
     case "+":
       return a + b;
     case "-":
       return a - b;
     case "/":
-      return b===0 ? null : a/b
-   case "*":
+      return b === 0 ? null : a / b;
+    case "*":
       return a * b;
     default:
       return null;
