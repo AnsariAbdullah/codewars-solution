56793192cf02c5b5450ae0d44f6f23efa1186e91
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-05-11 22:28:22 +0530

    refactor solution

--- codewars/7kyu/basic_calculator/app.ts
+++ codewars/7kyu/basic_calculator/app.ts
@@ -1,20 +1,12 @@
-export function calculate(
-  a: number,
-  operator: string,
-  b: number
-): number | null {
+export function calculate(a: number, operator: string, b: number): number | null {
   switch (operator) {
     case "+":
       return a + b;
     case "-":
       return a - b;
     case "/":
-      if (b == 0) {
-        return null;
-      } else {
-        return a / b;
-      }
-    case "*":
+      return b===0 ? null : a/b
+   case "*":
       return a * b;
     default:
       return null;
