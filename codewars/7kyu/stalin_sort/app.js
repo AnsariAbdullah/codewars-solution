b23f76a64d9a45e82c260892383bc0382c75242a
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-05-22 01:50:10 +0530

    format document

--- codewars/7kyu/stalin_sort/app.js
+++ codewars/7kyu/stalin_sort/app.js
@@ -1,14 +1,14 @@
 function stalinSort(array) {
   if (array.length === 0) return;
-  
+
   let survivor = array[0];
   let writeIndex = 1;
-  for(let i=1;i<array.length; i++){
-    if(array[i]>= survivor){
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] >= survivor) {
       survivor = array[i];
       array[writeIndex] = array[i];
-      writeIndex++
+      writeIndex++;
     }
   }
- array.length = writeIndex;
+  array.length = writeIndex;
 }
