593925d188ae6e894dfcd6a3b9c14483a7719c9b
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-05-25 02:05:17 +0530

    add TS solution

---
+++ codewars/7kyu/stalin_sort/app.ts
@@ -0,0 +1,14 @@
+export function stalinSort(array: number[]): void {
+  if (array.length === 0) return;
+  
+  let survivor = array[0];
+  let writeIndex = 1;
+  for(let i=1; i<array.length; i++){
+    if(array[i]>= survivor){
+       survivor = array[i];
+      array[writeIndex] = array[i];
+      writeIndex++
+    }
+  }
+ array.length = writeIndex;
+}
