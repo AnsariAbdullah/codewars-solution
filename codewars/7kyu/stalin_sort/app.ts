2f54ba11d57c58eecba13e4e781169df7efa6069
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-05-27 13:27:15 +0530

    format document

--- codewars/7kyu/stalin_sort/app.ts
+++ codewars/7kyu/stalin_sort/app.ts
@@ -1,14 +1,14 @@
 export function stalinSort(array: number[]): void {
   if (array.length === 0) return;
-  
+
   let survivor = array[0];
   let writeIndex = 1;
-  for(let i=1; i<array.length; i++){
-    if(array[i]>= survivor){
-       survivor = array[i];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] >= survivor) {
+      survivor = array[i];
       array[writeIndex] = array[i];
-      writeIndex++
+      writeIndex++;
     }
   }
- array.length = writeIndex;
+  array.length = writeIndex;
 }
