2e64c831ced4cbd51151d281b30311159ad5daed
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-06-26 01:23:04 +0530

    format document

--- codewars/7kyu/maximum_length_difference/app.js
+++ codewars/7kyu/maximum_length_difference/app.js
@@ -1,15 +1,15 @@
 function mxdiflg(a1, a2) {
-  if(a1.length === 0 || a2.length === 0){
-    return -1
+  if (a1.length === 0 || a2.length === 0) {
+    return -1;
   }
   let maxDiff = 0;
 
-  for(let str1 of a1){
-    for(let str2 of a2){
-     let diff = Math.abs(str1.length - str2.length);
-     if(diff > maxDiff) maxDiff = diff
+  for (let str1 of a1) {
+    for (let str2 of a2) {
+      let diff = Math.abs(str1.length - str2.length);
+      if (diff > maxDiff) maxDiff = diff;
     }
   }
-  
+
   return maxDiff;
 }
