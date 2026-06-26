fce0e2dbb7713f326ffd7444165e865c41217da5
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-06-27 01:28:33 +0530

    format document

--- codewars/7kyu/maximum_length_difference/app.js
+++ codewars/7kyu/maximum_length_difference/app.js
@@ -17,21 +17,18 @@ function mxdiflg(a1, a2) {
 // alternate solution
 
 function mxdiflg(a1, a2) {
-  if(a1.length === 0 || a2.length === 0){
-    return -1
+  if (a1.length === 0 || a2.length === 0) {
+    return -1;
   }
-  
-  const length1 = a1.map(s => s.length)
-  const length2 = a2.map(s => s.length)
-  
-  const max1 = Math.max(...length1)
-  const min1 = Math.min(...length1)
-  
-  const max2 = Math.max(...length2)
-  const min2 = Math.min(...length2)
-  
-  return Math.max(
-    Math.abs( max1 - min2 ),
-    Math.abs( max2 - min1 ),
-  )
+
+  const length1 = a1.map((s) => s.length);
+  const length2 = a2.map((s) => s.length);
+
+  const max1 = Math.max(...length1);
+  const min1 = Math.min(...length1);
+
+  const max2 = Math.max(...length2);
+  const min2 = Math.min(...length2);
+
+  return Math.max(Math.abs(max1 - min2), Math.abs(max2 - min1));
 }
