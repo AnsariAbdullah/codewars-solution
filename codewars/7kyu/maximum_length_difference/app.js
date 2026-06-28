3c5cddf1ef41228c2aa4d99b7cf4b8b9337cb0c5
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-06-29 00:37:04 +0530

    refactor changes

--- codewars/7kyu/maximum_length_difference/app.js
+++ codewars/7kyu/maximum_length_difference/app.js
@@ -15,7 +15,6 @@ function mxdiflg(a1, a2) {
 }
 
 // alternate solution
-
 function mxdiflg(a1, a2) {
   if (a1.length === 0 || a2.length === 0) {
     return -1;
@@ -24,11 +23,8 @@ function mxdiflg(a1, a2) {
   const length1 = a1.map((s) => s.length);
   const length2 = a2.map((s) => s.length);
 
-  const max1 = Math.max(...length1);
-  const min1 = Math.min(...length1);
-
-  const max2 = Math.max(...length2);
-  const min2 = Math.min(...length2);
-
-  return Math.max(Math.abs(max1 - min2), Math.abs(max2 - min1));
+  return Math.max(
+    Math.abs( Math.max(...length1) - Math.min(...length2) ),
+    Math.abs( Math.max(...length2) - Math.min(...length1) ),
+  )
 }
