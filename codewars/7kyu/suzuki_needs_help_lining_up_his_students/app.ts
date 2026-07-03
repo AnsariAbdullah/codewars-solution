f0a126145c503a256fcc5f442083698a19022095
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-07-03 23:42:21 +0530

    format document

--- codewars/7kyu/suzuki_needs_help_lining_up_his_students/app.ts
+++ codewars/7kyu/suzuki_needs_help_lining_up_his_students/app.ts
@@ -1,6 +1,7 @@
 export function lineupStudents(students: string): string[] {
-  return students.split(' ')
+  return students
+    .split(" ")
     .sort()
     .reverse()
-    .sort( (a, b ) => b.length - a.length );
+    .sort((a, b) => b.length - a.length);
 }
