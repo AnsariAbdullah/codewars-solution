02556ed5df70e186536b642c94c9c4c8aac74886
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-07-06 23:56:11 +0530

    add TS solution

---
+++ codewars/7kyu/maximum_length_difference/app.ts
@@ -0,0 +1,15 @@
+export const mxdiflg = (a1:string[], a2:string[]): number => {
+  if(a1.length === 0 || a2.length === 0){
+   return -1
+  }
+  let maxDiff = 0;
+
+  for(let str1 of a1){
+    for(let str2 of a2){
+      let diff = Math.abs( str1.length - str2.length );
+      if( diff > maxDiff ) maxDiff = diff
+    }
+  }
+  
+  return maxDiff;
+}
