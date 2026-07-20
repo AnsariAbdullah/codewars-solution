c82c04f69b6f4b2ae1cff2bd337fe736318941c4
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-07-21 00:36:21 +0530

    format document

--- codewars/8kyu/name_on_billboard/app.js
+++ codewars/8kyu/name_on_billboard/app.js
@@ -1,8 +1,8 @@
-function billboard(name, price = 30){
+function billboard(name, price = 30) {
   const str = name.split("");
-  let finalPrice = 0;  
+  let finalPrice = 0;
   str.forEach((item) => {
-      finalPrice += price;
-  })
+    finalPrice += price;
+  });
   return finalPrice;
 }
