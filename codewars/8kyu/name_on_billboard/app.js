03c07319d8fb37c6f225ba641984668f1bd66c0a
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-07-24 01:07:52 +0530

    add alternate solution

--- codewars/8kyu/name_on_billboard/app.js
+++ codewars/8kyu/name_on_billboard/app.js
@@ -5,3 +5,8 @@ function billboard(name, price = 30){
   }
   return totalPrice;
 }
+
+// alternate solution
+function billboard(name, price = 30){
+  return name.split('').reduce( (sum, letter) => sum+ price, 0)
+}
