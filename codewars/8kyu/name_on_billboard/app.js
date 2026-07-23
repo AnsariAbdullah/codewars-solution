cd6260c970c6d246a2ebe3b09bb19dae0de8dc18
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-07-24 01:08:44 +0530

    format document

--- codewars/8kyu/name_on_billboard/app.js
+++ codewars/8kyu/name_on_billboard/app.js
@@ -1,12 +1,12 @@
-function billboard(name, price = 30){
+function billboard(name, price = 30) {
   let totalPrice = 0;
-  for(let i=0; i<name.length; i++){
-     totalPrice += price;
+  for (let i = 0; i < name.length; i++) {
+    totalPrice += price;
   }
   return totalPrice;
 }
 
 // alternate solution
-function billboard(name, price = 30){
-  return name.split('').reduce( (sum, letter) => sum+ price, 0)
+function billboard(name, price = 30) {
+  return name.split("").reduce((sum, letter) => sum + price, 0);
 }
