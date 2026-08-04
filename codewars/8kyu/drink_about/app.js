a6fce20a97602b044cdfc57d597ed56c7f8d1112
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-08-05 01:32:58 +0530

    format document

--- codewars/8kyu/drink_about/app.js
+++ codewars/8kyu/drink_about/app.js
@@ -1,3 +1,5 @@
 function peopleWithAgeDrink(old) {
-	return `drink ${old < 14 ? 'toddy' : old < 18 ? 'coke' : old < 21 ? 'beer' : 'whisky'}`
-};
+  return `drink ${
+    old < 14 ? "toddy" : old < 18 ? "coke" : old < 21 ? "beer" : "whisky"
+  }`;
+}
