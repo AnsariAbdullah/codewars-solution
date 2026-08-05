9a5801fd947d48402c5fdfc512c99d60e850b56f
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-08-06 01:02:32 +0530

    format document

--- codewars/7kyu/make_acronym/app.js
+++ codewars/7kyu/make_acronym/app.js
@@ -1,8 +1,8 @@
-function toAcronym(inp){
+function toAcronym(inp) {
   let arr = inp.split(" ");
-  let res = ''
-  for(let i=0; i<arr.length; i++){
-   res += arr[i].charAt(0)
+  let res = "";
+  for (let i = 0; i < arr.length; i++) {
+    res += arr[i].charAt(0);
   }
   return res.toUpperCase();
 }
