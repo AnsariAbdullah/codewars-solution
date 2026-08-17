aa7455d1877401f5849392cebf674bbe00c45b39
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-08-17 23:11:47 +0530

    add neutralisation solution

---
+++ codewars/8kyu/neutralisation/app.js
@@ -0,0 +1,17 @@
+function neutralise(s1, s2) {
+  let neutralizedValue = ""
+  let lgth = s1.length;
+  for(let i=0; i<lgth; i++){
+    let str1 =   s1[i];
+    let str2 =   s2[i];
+    if(str1 === "+" && str2 === "+"){
+      neutralizedValue += "+"
+    }else if(str1 === "-" && str2 === "-"){
+      neutralizedValue += "-"
+    }else{
+      neutralizedValue += "0"
+    }
+  }
+  
+ return neutralizedValue;
+}
