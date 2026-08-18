6c2e83cd82549743c18ca7990443e1b95e08fcc1
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-08-18 23:33:49 +0530

    refactor

--- codewars/8kyu/neutralisation/app.js
+++ codewars/8kyu/neutralisation/app.js
@@ -1,17 +1,7 @@
 function neutralise(s1, s2) {
-  let neutralizedValue = "";
-  let lgth = s1.length;
-  for (let i = 0; i < lgth; i++) {
-    let str1 = s1[i];
-    let str2 = s2[i];
-    if (str1 === "+" && str2 === "+") {
-      neutralizedValue += "+";
-    } else if (str1 === "-" && str2 === "-") {
-      neutralizedValue += "-";
-    } else {
-      neutralizedValue += "0";
-    }
+  let neutralizedValue = ''
+  for(let i=0; i<s1.length; i++){
+    neutralizedValue += s1[i] === s2[i] ? s1[i] : "0"
   }
-
-  return neutralizedValue;
+ return neutralizedValue;
 }
