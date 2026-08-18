698ef0e5372543d359894f93f0298c0d9edd2070
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-08-19 00:10:05 +0530

    format document

--- codewars/8kyu/neutralisation/app.js
+++ codewars/8kyu/neutralisation/app.js
@@ -1,7 +1,7 @@
 function neutralise(s1, s2) {
-  let neutralizedValue = ''
-  for(let i=0; i<s1.length; i++){
-    neutralizedValue += s1[i] === s2[i] ? s1[i] : "0"
+  let neutralizedValue = "";
+  for (let i = 0; i < s1.length; i++) {
+    neutralizedValue += s1[i] === s2[i] ? s1[i] : "0";
   }
- return neutralizedValue;
+  return neutralizedValue;
 }
