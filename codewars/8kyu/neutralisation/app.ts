2aec5b21b2c72b282cec211f7534ddcc1dd8366e
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-08-24 22:31:04 +0530

    format document

--- codewars/8kyu/neutralisation/app.ts
+++ codewars/8kyu/neutralisation/app.ts
@@ -1,7 +1,7 @@
 export function neutraliseOpt(s1: string, s2: string): string {
-  let neutralizedValue = ''
-  for(let i=0; i<s1.length; i++){
-   neutralizedValue += s1[i] === s2[i] ? s1[i] : "0"
+  let neutralizedValue = "";
+  for (let i = 0; i < s1.length; i++) {
+    neutralizedValue += s1[i] === s2[i] ? s1[i] : "0";
   }
- return neutralizedValue;
+  return neutralizedValue;
 }
