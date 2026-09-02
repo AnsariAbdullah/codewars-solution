1fa73fc6c6f941c8ef2cb0e6e1e7620360fd6b15
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-09-03 00:40:29 +0530

    format document

--- codewars/8kyu/jennys_secret_message/app.js
+++ codewars/8kyu/jennys_secret_message/app.js
@@ -1,5 +1,4 @@
 function greet(name) {
-	if (name === "Johnny")
-		return "Hello, my love!";
-	return "Hello, " + name + "!";
+  if (name === "Johnny") return "Hello, my love!";
+  return "Hello, " + name + "!";
 }
