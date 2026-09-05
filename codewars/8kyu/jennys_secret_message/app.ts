28df74b07fa8e693250fc669f1c13d88023c8cf9
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-09-05 19:43:55 +0530

    add alternate solution

--- codewars/8kyu/jennys_secret_message/app.ts
+++ codewars/8kyu/jennys_secret_message/app.ts
@@ -2,3 +2,9 @@ export function greet(name: string): string {
   if (name === "Johnny") return "Hello, my love!";
   return "Hello, " + name + "!";
 }
+
+
+// alternate solution
+export function greetAlt(name:string): string {
+ return name === "Johnny" ? "Hello, my love!" : "Hello, " + name + "!";
+}
