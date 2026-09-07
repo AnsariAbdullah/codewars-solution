94dc7c772b2a7e427799f115d62c8c80cdf495dd
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-09-07 23:07:23 +0530

    add alternate solution

--- codewars/8kyu/jennys_secret_message/app.js
+++ codewars/8kyu/jennys_secret_message/app.js
@@ -2,3 +2,8 @@ function greet(name) {
   if (name === "Johnny") return "Hello, my love!";
   return "Hello, " + name + "!";
 }
+
+// alternate solution
+function greet(name){
+ return name === "Johnny" ? "Hello, my love!" : "Hello, " + name + "!";
+}
