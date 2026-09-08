759c515c0e11893644b59917b9e013c1f87f4ce0
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-09-09 01:52:46 +0530

    format document

--- codewars/7kyu/reducing_problems/app.js
+++ codewars/7kyu/reducing_problems/app.js
@@ -1,5 +1,5 @@
 function calculateTotal(team1, team2) {
- let t1s = team1.reduce((a, c) => a+c, 0);
- let t2s = team2.reduce((a, c) => a+c, 0);
- return t1s > t2s;
+  let t1s = team1.reduce((a, c) => a + c, 0);
+  let t2s = team2.reduce((a, c) => a + c, 0);
+  return t1s > t2s;
 }
