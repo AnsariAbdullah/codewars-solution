63fd1f9e48bee84c5dbf3aaee74b0fbefe24ee13
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-09-11 00:53:47 +0530

    format document

--- codewars/7kyu/reducing_problems/app.ts
+++ codewars/7kyu/reducing_problems/app.ts
@@ -1,5 +1,5 @@
 export function calculateTotal(team1: number[], team2: number[]): boolean {
- let t1s = team1.reduce((a, c) => a+c, 0);
- let t2s = team2.reduce((a, c) => a+c, 0);
- return t1s > t2s;
+  let t1s = team1.reduce((a, c) => a + c, 0);
+  let t2s = team2.reduce((a, c) => a + c, 0);
+  return t1s > t2s;
 }
