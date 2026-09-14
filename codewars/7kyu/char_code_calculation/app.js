daef8acdd2933313c3501d38a5960af88511f864
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-09-14 20:11:29 +0530

    format document

--- codewars/7kyu/char_code_calculation/app.js
+++ codewars/7kyu/char_code_calculation/app.js
@@ -1,11 +1,14 @@
-function sum(arg){
- return arg.split('').reduce((accumulator, current) => {
-  return accumulator + Number(current);
- }, 0);
+function sum(arg) {
+  return arg.split("").reduce((accumulator, current) => {
+    return accumulator + Number(current);
+  }, 0);
 }
 
-function calc(x){
-  let total1 = x.split('').map(char => char.charCodeAt(0)).join(''); 
-  let total2 = total1.replaceAll("7", "1")
-  return sum(total1) - sum(total2)
+function calc(x) {
+  let total1 = x
+    .split("")
+    .map((char) => char.charCodeAt(0))
+    .join("");
+  let total2 = total1.replaceAll("7", "1");
+  return sum(total1) - sum(total2);
 }
