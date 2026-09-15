101e2bc78c85be9a3a8b2bc80ad8ebdc0a6cd8ae
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-09-16 01:17:04 +0530

    format document

--- codewars/7kyu/char_code_calculation/app.ts
+++ codewars/7kyu/char_code_calculation/app.ts
@@ -1,12 +1,17 @@
 export function sum(arg: string): number {
- return arg.split('').reduce((accumulator: number, current: string): number => {
-  return accumulator + Number(current);
- }, 0);
+  return arg
+    .split("")
+    .reduce((accumulator: number, current: string): number => {
+      return accumulator + Number(current);
+    }, 0);
 }
 
 export const calc = (str: string): number => {
- const total1: string = str.split('').map((char: string) => char.charCodeAt(0)).join(''); 
- const total2: string = total1.replaceAll("7", "1");
-  
- return sum(total1) - sum(total2);
+  const total1: string = str
+    .split("")
+    .map((char: string) => char.charCodeAt(0))
+    .join("");
+  const total2: string = total1.replaceAll("7", "1");
+
+  return sum(total1) - sum(total2);
 };
