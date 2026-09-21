62e3b87447eb54fb05df6b88d8aa65e1ff7fed1a
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-09-22 00:27:04 +0530

    format document

--- codewars/7kyu/swap_the_head_and_the_tail/app.ts
+++ codewars/7kyu/swap_the_head_and_the_tail/app.ts
@@ -1,12 +1,12 @@
 export function swapHeadAndTail(arr: number[]): number[] {
- const mid = Math.floor(arr.length/2)
- if(arr.length%2===0){
+  const mid = Math.floor(arr.length / 2);
+  if (arr.length % 2 === 0) {
+    const heads = arr.slice(0, mid);
+    const tails = arr.slice(mid);
+    return [...tails, ...heads];
+  }
   const heads = arr.slice(0, mid);
-  const tails = arr.slice(mid);  
-  return [...tails, ...heads];
- }
- const heads = arr.slice(0, mid);
- const middle = arr[mid];
- const tails = arr.slice(mid+1);  
- return [...tails, middle, ...heads];
+  const middle = arr[mid];
+  const tails = arr.slice(mid + 1);
+  return [...tails, middle, ...heads];
 }
