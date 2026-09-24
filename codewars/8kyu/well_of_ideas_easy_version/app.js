49ec0a747c7d7ca09cb5ad1d3254690e66a2a4e3
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-09-25 00:43:33 +0530

    format document

--- codewars/8kyu/well_of_ideas_easy_version/app.js
+++ codewars/8kyu/well_of_ideas_easy_version/app.js
@@ -1,4 +1,8 @@
-function well(x){
+function well(x) {
   let goodIdeasLength = x.filter((item) => item == "good").length;
-  return goodIdeasLength > 2 ? 'I smell a series!' : goodIdeasLength > 0 ? 'Publish!' : 'Fail!'
+  return goodIdeasLength > 2
+    ? "I smell a series!"
+    : goodIdeasLength > 0
+    ? "Publish!"
+    : "Fail!";
 }
